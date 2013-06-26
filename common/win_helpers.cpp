9c9b832fa52b721455b7f083932f9fa34fbb348c
Travis Collins <travis@tec27.com>
2013-06-26 03:42:41 -0700

    Add WindowsError type for dealing with GetLastError more easily.

--- common/win_helpers.cpp
+++ common/win_helpers.cpp
@@ -1,7 +1,10 @@
 #include "common/win_helpers.h"
 
 #include <Windows.h>
+#include <string>
 #include "common/types.h"
+
+using std::wstring;
 
 namespace sbat {
 ScopedVirtualProtect::ScopedVirtualProtect(void* address, size_t size, uint32 new_protection)
@@ -21,5 +24,37 @@ ScopedVirtualProtect::~ScopedVirtualProtect() {
 
 bool ScopedVirtualProtect::has_errors() const {
   return has_errors_;
+}
+
+WindowsError::WindowsError()
+    : error_code_(0),
+      error_message_() {
+}
+
+WindowsError::WindowsError(uint32 error_code) 
+    : error_code_(error_code),
+      error_message_() {
+  if (error_code_ == 0) {
+    return;
+  }
+
+  wchar_t* message_buffer;
+  FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |
+      FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, error_code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+      reinterpret_cast<wchar_t*>(&message_buffer), 0, nullptr);
+  error_message_ = message_buffer;
+  LocalFree(message_buffer);
+}
+
+bool WindowsError::is_error() const {
+  return error_code_ != 0;
+}
+
+uint32 WindowsError::error_code() const {
+  return error_code_;
+}
+
+wstring WindowsError::error_message() const {
+  return error_message_;
 }
 }  // namespace sbat
--- common/win_helpers.h
+++ common/win_helpers.h
@@ -2,6 +2,7 @@
 #define COMMON_WIN_HELPERS_H_
 
 #include <Windows.h>
+#include <string>
 #include "common/types.h"
 
 // Helper Types/Functions for Windows things, to make them easier/safer to use
@@ -18,6 +19,20 @@ private:
   size_t size_;
   uint32 old_protection_;
   bool has_errors_;
+};
+
+class WindowsError {
+public:
+  WindowsError();
+  explicit WindowsError(uint32 error_code);
+
+  bool is_error() const;
+  uint32 error_code() const;
+  std::wstring error_message() const;
+
+private:
+  uint32 error_code_;
+  std::wstring error_message_;
 };
 }  // namespace sbat
 #endif  // COMMON_WIN_HELPERS_H_
