15ef544c07ee2b8e3ebeae67ed405e028ee84ae4
Travis Collins <travis@tec27.com>
2013-08-25 04:56:27 -0700

    Update websocket events to match manner-pylon style.

--- js/psi-entry.js
+++ js/psi-entry.js
@@ -7,6 +7,7 @@ httpServer.listen(33198, '127.0.0.1')
 
 io.configure(function() {
   io.set('transports', ['websocket'])
+    .set('log level', 2)
 })
 
 var siteSockets = io.of('/site')
@@ -18,15 +19,15 @@ siteSockets.on('connection', function(socket) {
     doLaunch(cb)
   }).on('disconnect', function() {
     console.log('site client disconnected.')
-  }).on('game:load', function(plugins, cb) {
+  }).on('game/load', function(plugins, cb) {
     if (gameSocket) {
       gameSocket.emit('load', plugins, function(errors) { cb(errors) })
     }
-  }).on('game:start', function(params) {
+  }).on('game/start', function(params) {
     if (gameSocket) {
       gameSocket.emit('start', params)
     }
-  }).on('game:join', function(params) {
+  }).on('game/join', function(params) {
     if (gameSocket) {
       gameSocket.emit('join', params)
     }
@@ -35,16 +36,16 @@ siteSockets.on('connection', function(socket) {
 
 io.of('/game').on('connection', function(socket) {
   console.log('game client connected.')
-  siteSockets.emit('game:connected')
+  siteSockets.emit('game/connected')
   gameSocket = socket
   socket.on('disconnect', function() {
     console.log('game client disconnected.')
-    siteSockets.emit('game:disconnected')
+    siteSockets.emit('game/disconnected')
     gameSocket = null
   }).on('error', function(err) {
     console.log('game client error: ' + err)
   }).on('status', function(status) {
-    siteSockets.emit('game:status', status)
+    siteSockets.emit('game/status', status)
   })
 })
 
