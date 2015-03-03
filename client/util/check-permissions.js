a39ec486394ceb7e60913c42a35e043cc622d7c2
Travis Collins <travis@tec27.com>
2015-03-03 00:48:56 -0800

    Delete angular client files to make it more clear what's going on now.

--- client/util/check-permissions.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(permissions) {
-  if (!angular.isArray(permissions)) {
-    permissions = [ permissions ]
-  }
-
-  return function(authService) {
-    return authService.checkPermissions(permissions)
-  }
-}
