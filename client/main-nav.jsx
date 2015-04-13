4d17af4d6641170fe702f34db360ddeb1647b6f6
Travis Collins <travis@tec27.com>
2015-04-13 00:41:15 -0700

    Add a bottom toolbar to the site layout.

--- client/main-nav.jsx
+++ client/main-nav.jsx
@@ -5,17 +5,22 @@ import Card from './material/card.jsx'
 
 class MainNav extends React.Component {
   render() {
-    return (<div className='page-with-docked-left-nav'>
-      <Card className='docked-left-nav' zDepth={1}>
-        <ul className="tabs">
-          <Tab label="Games" route="games" />
-          <Tab label="Replays" route="replays" />
-        </ul>
-      </Card>
+    return (<div>
+      <div className='page-with-docked-left-nav'>
+        <Card className='docked-left-nav' zDepth={1}>
+          <ul className="tabs">
+            <Tab label="Games" route="games" />
+            <Tab label="Replays" route="replays" />
+          </ul>
+        </Card>
 
-      <div className="mui-app-page-content">
-        <RouteHandler />
+        <div className="mui-app-page-content">
+          <RouteHandler />
+        </div>
       </div>
+      <Card className='docked-bottom-toolbar' zDepth={2}>
+        <span>Hello bottom toolbar world!</span>
+      </Card>
     </div>)
   }
 }
