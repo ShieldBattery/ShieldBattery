b39393705ea6713271c13ca519d80bfeb6ed28fa
Travis Collins <travis@tec27.com>
2015-07-12 00:09:59 -0700

    Fix lint in all the api files and a few other misceallaneous files.

--- server/api/sessions.js
+++ server/api/sessions.js
@@ -1,10 +1,10 @@
-var bcrypt = require('bcrypt')
-  , thenify = require('thenify')
-  , users = require('../models/users')
-  , permissions = require('../models/permissions')
-  , httpErrors = require('../http/errors')
-  , initSession = require('../session/init')
-  , setReturningCookie = require('../session/set-returning-cookie')
+import bcrypt from 'bcrypt'
+import thenify from 'thenify'
+import users from '../models/users'
+import permissions from '../models/permissions'
+import httpErrors from '../http/errors'
+import initSession from '../session/init'
+import setReturningCookie from '../session/set-returning-cookie'
 
 module.exports = function(router) {
   router
@@ -15,13 +15,13 @@ module.exports = function(router) {
 
 function* getCurrentSession(next) {
   if (!this.session.userId) throw new httpErrors.GoneError('Session expired')
-  let userId = this.session.userId
+  const userId = this.session.userId
 
   let user
   try {
     user = yield* users.find(userId)
   } catch (err) {
-    this.log.error({ err: err }, 'error finding user')
+    this.log.error({ err }, 'error finding user')
     throw err
   }
 
@@ -30,13 +30,13 @@ function* getCurrentSession(next) {
     throw new httpErrors.GoneError('Session expired')
   }
 
-  this.body = { user: user, permissions: this.session.permissions }
+  this.body = { user, permissions: this.session.permissions }
 }
 
-let bcryptCompare = thenify(bcrypt.compare)
+const bcryptCompare = thenify(bcrypt.compare)
 function* startNewSession(next) {
-  if (!!this.session.userId) throw new httpErrors.ConflictError('Session already active')
-  let { username, password, remember } = this.request.body
+  if (this.session.userId) throw new httpErrors.ConflictError('Session already active')
+  const { username, password, remember } = this.request.body
   remember = !!remember
   if (!username || !password) {
     throw new httpErrors.BadRequestError('Username and password required')
@@ -46,7 +46,7 @@ function* startNewSession(next) {
   try {
     user = yield* users.find(username)
   } catch (err) {
-    this.log.error({ err: err }, 'error finding user')
+    this.log.error({ err }, 'error finding user')
     throw err
   }
   if (!user) {
@@ -57,7 +57,7 @@ function* startNewSession(next) {
   try {
     same = yield bcryptCompare(password, user.password)
   } catch (err) {
-    this.log.error({ err: err }, 'error comparing passwords')
+    this.log.error({ err }, 'error comparing passwords')
     throw err
   }
   if (!same) {
@@ -66,13 +66,13 @@ function* startNewSession(next) {
 
   try {
     yield this.regenerateSession()
-    let perms = yield* permissions.get(user.id)
+    const perms = yield* permissions.get(user.id)
     initSession(this, user, perms)
     setReturningCookie(this)
 
-    this.body = { user: user, permissions: perms }
+    this.body = { user, permissions: perms }
   } catch (err) {
-    this.log.error({ err: err }, 'error regenerating session')
+    this.log.error({ err }, 'error regenerating session')
     throw err
   }
 }
