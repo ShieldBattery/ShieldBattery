0410f169918e5bbb66f24fa946c21b01af9f20e3
Travis Collins <travis@tec27.com>
2015-10-04 22:01:28 -0700

    Update react/redux/react-router deps and make things work again (moving to React 0.14-rc1 to ease context stuff).

--- client/routes.jsx
+++ client/routes.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Router, Route } from 'react-router'
-import { reduxRouteComponent } from 'redux-react-router'
+import { Route } from 'react-router'
 import AppNotFound from './app-not-found.jsx'
 import MainLayout from './main-layout.jsx'
 import Home from './home.jsx'
@@ -11,21 +10,19 @@ import LoginLayout from './auth/login-layout.jsx'
 import Login from './auth/login.jsx'
 import Signup from './auth/signup.jsx'
 
-export default function getRoutes(history, store) {
-  return (<Router history={history}>
-    <Route component={reduxRouteComponent(store)}>
-      <Route component={LoginRequired}>
-        <Route component={MainLayout}>
-          <Route path='/' component={Home} />
-          <Route path='games' component={Games} />
-          <Route path='replays' component={Replays} />
-        </Route>
-      </Route>
-      <Route component={LoginLayout}>
-        <Route path='/login' component={Login} />
-        <Route path='/signup' component={Signup} />
-      </Route>
-      <Route path='*' component={AppNotFound} />
+const routes = <Route>
+  <Route component={LoginRequired}>
+    <Route component={MainLayout}>
+      <Route path='/' component={Home} />
+      <Route path='games' component={Games} />
+      <Route path='replays' component={Replays} />
     </Route>
-  </Router>)
-}
+  </Route>
+  <Route component={LoginLayout}>
+    <Route path='/login' component={Login} />
+    <Route path='/signup' component={Signup} />
+  </Route>
+  <Route path='*' component={AppNotFound} />
+</Route>
+
+export default routes
