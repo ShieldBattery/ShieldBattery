a852b21bfc92674db8e6003b6605624ddf9c94cf
Travis Collins <travis@tec27.com>
2015-12-18 19:23:12 -0800

    Refactor the dummy data out of LeftNav and MainLayout.

--- client/main-layout.jsx
+++ client/main-layout.jsx
@@ -3,9 +3,29 @@ import { connect } from 'react-redux'
 import siteSocket from './network/site-socket'
 
 import AppBar from './material/app-bar.jsx'
+import Divider from './material/left-nav/divider.jsx'
+import Entry from './material/left-nav/entry.jsx'
+import FontIcon from './material/font-icon.jsx'
 import LeftNav from './material/left-nav/left-nav.jsx'
+import Section from './material/left-nav/section.jsx'
+import Subheader from './material/left-nav/subheader.jsx'
 
-@connect(state => ({ auth: state.auth }))
+function stateToProps(state) {
+  return {
+    auth: state.auth,
+    chatChannels: [
+      { name: '#doyoureallywantthem' },
+      { name: '#teamliquid', active: true },
+      { name: '#x17' },
+      { name: '#nohunters' },
+    ],
+    whispers: [
+      { from: 'Pachi' },
+    ],
+  }
+}
+
+@connect(stateToProps)
 class MainLayout extends React.Component {
   componentDidMount() {
     siteSocket.connect()
@@ -16,8 +36,26 @@ class MainLayout extends React.Component {
   }
 
   render() {
+    const channels = this.props.chatChannels.map(
+        channel => <Entry key={channel.name} active={channel.active}>{channel.name}</Entry>)
+    const whispers = this.props.whispers.map(
+        whisper => <Entry key={whisper.from} active={whisper.active}>{whisper.from}</Entry>)
     return (<div className='flex-row'>
-      <LeftNav />
+      <LeftNav>
+        <Subheader>Chat channels</Subheader>
+        <Section>
+          {channels}
+          <Entry>
+            <FontIcon>add_circle</FontIcon>
+            <span>Join another</span>
+          </Entry>
+        </Section>
+        <Divider/>
+        <Subheader>Whispers</Subheader>
+        <Section>
+          {whispers}
+        </Section>
+      </LeftNav>
       <div className='flex-fit'>
         <AppBar title='#teamliquid'/>
         { this.props.children }
--- client/material/left-nav/left-nav.jsx
+++ client/material/left-nav/left-nav.jsx
@@ -1,35 +1,11 @@
 import React from 'react'
-import Divider from './divider.jsx'
-import Entry from './entry.jsx'
-import FontIcon from '../font-icon.jsx'
-import Section from './section.jsx'
-import Subheader from './subheader.jsx'
 
-class LeftNav extends React.Component {
-  render() {
-    return (<nav className='leftnav'>
-      <div className='logotext'>
-        <h3 className='logotext-light'>Shield</h3>
-        <h3 className='logotext-medium'>Battery</h3>
-      </div>
-      <Subheader>Chat channels</Subheader>
-      <Section>
-        <Entry>#doyoureallywantthem</Entry>
-        <Entry active={true}>#teamliquid</Entry>
-        <Entry>#x17</Entry>
-        <Entry>#nohunters</Entry>
-        <Entry>
-          <FontIcon>add_circle</FontIcon>
-          <span>Join another</span>
-        </Entry>
-      </Section>
-      <Divider/>
-      <Subheader>Whispers</Subheader>
-      <Section>
-        <Entry>Pachi</Entry>
-      </Section>
-    </nav>)
-  }
+export default function LeftNav(props) {
+  return (<nav className='leftnav'>
+    <div className='logotext'>
+      <h3 className='logotext-light'>Shield</h3>
+      <h3 className='logotext-medium'>Battery</h3>
+    </div>
+    {props.children}
+  </nav>)
 }
-
-export default LeftNav
