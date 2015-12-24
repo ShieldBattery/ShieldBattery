155af9a09990f2781693d3498d839dfac13bd025
Travis Collins <travis@tec27.com>
2015-12-23 17:05:58 -0800

    Add handling for escape keypresses to close dialogs.

--- client/material/dialog.jsx
+++ client/material/dialog.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import TransitionGroup from 'react-addons-css-transition-group'
+import keycode from 'keycode'
 
 class Dialog extends React.Component {
   static propTypes = {
     onCancel: React.PropTypes.func,
+  }
+
+  constructor(props) {
+    super(props)
+    this._onKeyUp = ::this.onKeyUp
+  }
+
+  componentDidMount() {
+    window.addEventListener('keyup', this._onKeyUp)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keyup', this._onKeyUp)
   }
 
   render() {
@@ -25,6 +39,14 @@ class Dialog extends React.Component {
   onCancel() {
     if (this.props.onCancel) {
       this.props.onCancel()
+    }
+  }
+
+  onKeyUp(event) {
+    if (!this.props.children) return
+
+    if (event.keyCode === keycode('esc')) {
+      this.onCancel()
     }
   }
 }
