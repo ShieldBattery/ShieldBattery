71dbf382bb83e7ec766287729c2bd9590a68eb5c
Travis Collins <travis@tec27.com>
2015-12-28 17:50:45 -0800

    Use redux-batched-subscribe instead of our own batching middleware.

--- client/create-store.js
+++ client/create-store.js
@@ -1,12 +1,16 @@
 import { applyMiddleware, createStore, compose } from 'redux'
 import thunk from 'redux-thunk'
 import promise from 'redux-promise'
-import { batchedUpdatesMiddleware } from './dom/batched-updates'
+import { batchedSubscribe } from 'redux-batched-subscribe'
+/*eslint-disable camelcase*/
+import { unstable_batchedUpdates as batchedUpdates } from 'react-dom'
+/* eslint-enable camelcase */
 import rootReducer from './root-reducer'
 
 const isDev = (process.env.NODE_ENV || 'development') === 'development'
 const createMiddlewaredStore = compose(
-    applyMiddleware(thunk, promise, batchedUpdatesMiddleware),
+    applyMiddleware(thunk, promise),
+    batchedSubscribe(batchedUpdates),
     // Support for https://github.com/zalmoxisus/redux-devtools-extension
     isDev && window.devToolsExtension ? window.devToolsExtension() : f => f
 )(createStore)
