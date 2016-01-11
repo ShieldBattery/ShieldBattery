7748204b9427771ee2c48c985870677cf72b90b7
Travis Collins <travis@tec27.com>
2016-01-10 23:31:13 -0800

    Replace JSHint with ESLint, no attempt yet made to make things pass lint.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-var modName = process.argv[process.argv.length - 1]
+const modName = process.argv[process.argv.length - 1]
 
 // requiring files explicitly so that browserify picks up on them easily
-if (modName == 'shieldbattery') {
+if (modName === 'shieldbattery') {
   require('./shieldbattery.js')
 } else {
   require('./psi.js')
