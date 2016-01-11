e6689efb9ef1594ec8aab1e78f28bea4ada1137d
Travis Collins <travis@tec27.com>
2016-01-11 01:46:02 -0800

    Fix lint, formatting, use imports in psi.js. (Still horribly broken.)

--- js/psi.js
+++ js/psi.js
@@ -1,4 +1,4 @@
-var log = require('./psi/logger')
+import log from './psi/logger'
 process.on('uncaughtException', function(err) {
   log.error(err.stack)
   // give the log time to write out
@@ -7,27 +7,31 @@ process.on('uncaughtException', function(err) {
   }, 100)
 })
 
-var psi = require('./psi/natives/index')
-  , path = require('path')
-  , EventEmitter = require('events').EventEmitter
-  , httpServer = require('./psi/http-server')(33198, '127.0.0.1')
-  , nydus = require('nydus')(httpServer, { authorize: authorize })
-  , shieldbatteryRoot = path.dirname(process.execPath)
-  , localSettings = require('./psi/local-settings')(path.join(shieldbatteryRoot, 'settings.json'))
-  , fs = require('fs')
-  , path = require('path')
+import path from 'path'
+import fs from 'fs'
+import { EventEmitter } from 'events'
+import nydus from 'nydus'
+import psi from './psi/natives/index'
+import createHttpServer from './psi/http-server'
+import createLocalSettings from './psi/local-settings'
 
-var environment = {
+const httpServer = createHttpServer(33198, '127.0.0.1')
+const nydusServer = nydus(httpServer, { allowRequest: authorize })
+const shieldbatteryRoot = path.dirname(process.execPath)
+const localSettings = createLocalSettings(path.join(shieldbatteryRoot, 'settings.json'))
+const socketTypes = new WeakMap()
+
+const environment = {
   allowedHosts: [
     'https://shieldbattery.net',
     'https://www.shieldbattery.net',
     'https://dev.shieldbattery.net'
   ],
   updateUrl: 'https://shieldbattery.net/update',
-  autoUpdate: true
+  autoUpdate: true,
 }
 if (fs.existsSync(path.join(shieldbatteryRoot, 'dev.json'))) {
-  var devEnv = require(path.join(shieldbatteryRoot, 'dev.json'))
+  const devEnv = require(path.join(shieldbatteryRoot, 'dev.json'))
   environment.allowedHosts = environment.allowedHosts.concat(devEnv.extraAllowedHosts || [])
   environment.updateUrl = devEnv.updateUrl || environment.updateUrl
   if (devEnv.autoUpdate !== undefined) {
@@ -36,16 +40,18 @@ if (fs.existsSync(path.join(shieldbatteryRoot, 'dev.json'))) {
 }
 log.verbose('environment:\n' + JSON.stringify(environment))
 
-function authorize(info, cb) {
-  var clientType = info.origin == 'BROODWARS' ? 'game' : 'site'
-  if (clientType == 'site') {
+function authorize(req, cb) {
+  const clientType = req.origin === 'BROODWARS' ? 'game' : 'site'
+  if (clientType === 'site') {
     // ensure that this connection is coming from a site we trust
-    if (environment.allowedHosts.indexOf(info.origin) == -1) {
-      log.warning('Blocked a connection from an untrusted origin: ' + info.origin)
-      return cb(false)
+    if (!environment.allowedHosts.includes(req.origin)) {
+      log.warning('Blocked a connection from an untrusted origin: ' + req.origin)
+      return cb(null, false)
     }
   }
-  cb(true, { clientType: clientType })
+  // TODO(tec27): store clientType somewhere for this socket
+  socketTypes.set(req, clientType)
+  cb(null, true)
 }
 
 psi.on('shutdown', function() {
@@ -55,26 +61,27 @@ psi.on('shutdown', function() {
   log.verbose('localSettings stopped watching')
 })
 
-var gameSocket = null
-  , gameConnectedEmitter = new EventEmitter()
+let gameSocket = null
+const gameConnectedEmitter = new EventEmitter()
 
 nydus.on('connection', function(socket) {
-  log.verbose('websocket (' + socket.handshake.clientType + ') connected.')
-  if (socket.handshake.clientType == 'game') {
+  const clientType = socketTypes.get(socket.conn.request)
+  log.verbose('websocket (' + clientType + ') connected.')
+  if (clientType === 'game') {
     gameSocket = socket
     gameConnectedEmitter.emit('connected')
   }
 
   socket.on('disconnect', function() {
-    log.verbose('websocket (' + socket.handshake.clientType + ') disconnected.')
-    if (socket.handshake.clientType == 'game') {
+    log.verbose('websocket (' + clientType + ') disconnected.')
+    if (gameSocket === socket) {
       nydus.publish('/game/disconnected')
       gameSocket = null
     }
   })
 })
 
-nydus.router.call('/launch', function(req, res) {
+nydusServer.router.call('/launch', function(req, res) {
   doLaunch(req, res)
 }).call('/getResolution', function(req, res) {
   detectResolution(req, res)
@@ -88,26 +95,27 @@ nydus.router.call('/launch', function(req, res) {
   res.complete(localSettings.getSettings())
 })
 
-;[ 'setSettings'
-, 'createLobby'
-, 'joinLobby'
-, 'setRace'
-, 'addComputer'
-, 'startGame'
-, 'quit'
-].forEach(function(command) {
-  nydus.router.call('/game/' + command, function(/*req, res, params...*/) {
+;[
+  'setSettings',
+  'createLobby',
+  'joinLobby',
+  'setRace',
+  'addComputer',
+  'startGame',
+  'quit',
+].forEach(command => {
+  nydusServer.router.call('/game/' + command, function(/* req, res, params... */) {
     // pass through the calls directly to the game, and return responses back to the site
-    var res = arguments[1]
+    const res = arguments[1]
     if (!gameSocket) {
       return res.fail(502, 'bad gateway', { msg: 'no game client connected.' })
     }
 
-    var callArgs = [ '/' + command ].concat(Array.prototype.slice.call(arguments, 2))
+    const callArgs = [ '/' + command ].concat(Array.prototype.slice.call(arguments, 2))
     callArgs.push(function() {
-      var err = arguments[0]
+      const err = arguments[0]
       if (err) {
-        res.fail(err.code, err.desc, err.details);
+        res.fail(err.code, err.desc, err.details)
       } else {
         res.complete.apply(res, Array.prototype.slice.call(arguments, 1))
       }
@@ -116,11 +124,12 @@ nydus.router.call('/launch', function(req, res) {
   })
 })
 
-;[ 'playerJoined'
-, 'gameStarted'
-, 'gameFinished'
-].forEach(function(command) {
-  nydus.router.publish('/' + command, function(req, event, complete) {
+;[
+  'playerJoined',
+  'gameStarted',
+  'gameFinished',
+].forEach(command => {
+  nydusServer.router.publish('/' + command, function(req, event, complete) {
     complete(event)
   }).subscribe('/' + command, function(req, res) {
     res.complete()
@@ -133,7 +142,7 @@ function awaitGameConnection(timeout, cb) {
     return cb(false)
   }
   gameConnectedEmitter.once('connected', onConnection)
-  var timeoutId = setTimeout(function() {
+  const timeoutId = setTimeout(function() {
     gameConnectedEmitter.removeListener('connected', onConnection)
     cb(true)
   }, timeout)
@@ -147,50 +156,50 @@ function awaitGameConnection(timeout, cb) {
 function doLaunch(req, res) {
   // TODO(tec27): we should also try to guess the install path as %ProgramFiles(x86)%/Starcraft and
   // %ProgramFiles%/Starcraft, and allow this to be set through the web interface as well
-  var installPath = psi.getInstallPathFromRegistry()
+  const installPath = psi.getInstallPathFromRegistry()
   installPath = installPath || 'C:\\Program Files (x86)\\Starcraft'
-  var appPath = installPath +
-      (installPath.charAt(installPath.length - 1) == '\\' ? '' : '\\') +
+  const appPath = installPath +
+      (installPath.charAt(installPath.length - 1) === '\\' ? '' : '\\') +
       'Starcraft.exe'
 
-  psi.launchProcess(
-      { appPath: appPath
-      , launchSuspended: true
-      , currentDir: installPath
-      }, function(err, proc) {
-        if (err) {
-          return res.fail(500, 'internal server error',
-              { when: 'launching process', msg: err.message })
+  psi.launchProcess({
+    appPath,
+    launchSuspended: true,
+    currentDir: installPath,
+  }, (err, proc) => {
+    if (err) {
+      return res.fail(500, 'internal server error',
+          { when: 'launching process', msg: err.message })
+    }
+
+    log.verbose('Process launched')
+    const shieldbatteryDll = path.join(shieldbatteryRoot, 'shieldbattery.dll')
+
+    proc.injectDll(shieldbatteryDll, 'OnInject', function(err) {
+      if (err) {
+        return res.fail(500, 'internal server error',
+            { when: 'injecting dll', msg: err.message })
+      }
+
+      log.verbose('Dll injected. Attempting to resume process...')
+
+      const resumeErr = proc.resume()
+      log.verbose('Process resumed')
+      if (resumeErr) {
+        return res.fail(500, 'internal server error',
+            { when: 'resuming process', msg: resumeErr.message })
+      }
+
+      awaitGameConnection(100000, function(timedOut) {
+        if (timedOut) {
+          res.fail(504, 'gateway timeout',
+              { when: 'resuming process', msg: 'waiting for game connection timed out' })
+        } else {
+          res.complete()
         }
-
-        log.verbose('Process launched')
-        var shieldbatteryDll = path.join(shieldbatteryRoot, 'shieldbattery.dll')
-
-        proc.injectDll(shieldbatteryDll, 'OnInject', function(err) {
-          if (err) {
-            return res.fail(500, 'internal server error',
-                { when: 'injecting dll', msg: err.message })
-          }
-
-          log.verbose('Dll injected. Attempting to resume process...')
-
-          var resumeErr = proc.resume()
-          log.verbose('Process resumed')
-          if (resumeErr) {
-            return res.fail(500, 'internal server error',
-                { when: 'resuming process', msg: resumeErr.message })
-          }
-
-          awaitGameConnection(100000, function(timedOut) {
-            if (timedOut) {
-              res.fail(504, 'gateway timeout',
-                  { when: 'resuming process', msg: 'waiting for game connection timed out' })
-            } else {
-              res.complete()
-            }
-          })
-        })
       })
+    })
+  })
 }
 
 function detectResolution(req, res) {
