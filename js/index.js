3b088f70640560e517398f8ef594b3e7c1d5821e
Travis Collins <travis@tec27.com>
2016-01-11 00:27:53 -0800

    Add babel so we can use all the coooool stuff.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+require('babel-register')
+require('babel-polyfill')
+
 const modName = process.argv[process.argv.length - 1]
 
 // requiring files explicitly so that browserify picks up on them easily
