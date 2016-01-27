470737eafd0fa0fbc6112fe2e7423d67f57a92f8
2Pacalypse- <marko@kastela.com>
2016-01-27 23:07:04 +0100

    Use two files for babel caching.
    This avoids weirdness with two processes writing to the same file.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
-require('babel-register')
-require('babel-polyfill')
+const path = require('path')
 
 // $ <executable> <script> <moduleName> ...
 const modName = process.argv[2]
+const shieldbatteryDir = path.dirname(path.dirname(path.resolve(process.argv[1])))
+
+// use two files for babel caching to avoid weirdness with two processes writing to the same file
+process.env.BABEL_CACHE_PATH = path.join(shieldbatteryDir, `.${modName}.babel.json`)
+
+require('babel-register')
+require('babel-polyfill')
 
 // requiring files explicitly so that browserify picks up on them easily
 if (modName === 'shieldbattery') {
