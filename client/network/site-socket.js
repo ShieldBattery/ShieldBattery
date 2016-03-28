18caf3dea7cc1f67e34782abe08d02b3fadc6f67
Travis Collins <travis@tec27.com>
2016-03-28 11:39:16 -0700

    Fix protocol check for site socket so that it actually works on https.

--- client/network/site-socket.js
+++ client/network/site-socket.js
@@ -1,7 +1,7 @@
 import createNydus from 'nydus-client'
 
 const location = window.location
-const protocol = location.protocol === 'https' ? 'wss' : 'ws'
+const protocol = location.protocol === 'https:' ? 'wss' : 'ws'
 
 const siteSocket = createNydus(`${protocol}://${location.hostname}:${location.port}`)
 export default siteSocket
