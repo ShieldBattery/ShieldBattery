7908e8d15a98476c2949706ead8d968019d1cafe
Travis Collins <travis@tec27.com>
2016-04-13 02:00:02 -0700

    Simplify admin-utils.

--- client/admin/admin-utils.js
+++ client/admin/admin-utils.js
@@ -1,27 +1,11 @@
 export function isAdmin(authState) {
-  const perms = authState.permissions.toArray()
-  if (!perms) return false
-
-  for (let i = 0; i < perms.length; i++) {
-    if (perms[i]) return true
+  for (const perm of Object.keys(authState.permissions)) {
+    if (authState.permissions[perm]) return true
   }
-
   return false
 }
 
-export function checkPermissions(authState, permissionsToCheck) {
+export function checkPermissions(authState, ...permissionsToCheck) {
   const perms = authState.permissions
-  if (typeof permissionsToCheck === 'string') {
-    return perms[permissionsToCheck]
-  }
-
-  if (Array.isArray(permissionsToCheck)) {
-    for (let i = 0; i < permissionsToCheck.length; i++) {
-      if (!perms[permissionsToCheck[i]]) return false
-    }
-
-    return true
-  }
-
-  throw new Error('Invalid argument of permissions to check')
+  return permissionsToCheck.every(p => perms[p])
 }
