32bea76c87efba671232ab0bd7278b59156974a0
Travis Collins <travis@tec27.com>
2016-05-02 01:17:55 -0700

    Report starcraft path validity when settings change (and on connect).

--- js/psi/site-routes.js
+++ js/psi/site-routes.js
@@ -1,8 +1,33 @@
+import fs from 'fs'
+import path from 'path'
+import thenify from 'thenify'
 import { detectResolution } from './natives/index'
 import log from './logger'
 import packageJson from '../package.json'
 
+let lastHadValidPath = false
+
+const accessAsync = thenify(fs.access)
+async function hasValidPath(settings) {
+  if (!settings.starcraftPath) {
+    lastHadValidPath = false
+  } else {
+    try {
+      await accessAsync(path.join(settings.starcraftPath, 'starcraft.exe'))
+      lastHadValidPath = true
+    } catch (e) {
+      lastHadValidPath = false
+    }
+  }
+
+  return lastHadValidPath
+}
+
+
 export function register(nydus, localSettings, activeGameManager, mapStore) {
+  // init our cache
+  hasValidPath(localSettings.settings)
+
   async function setSettings(data, next) {
     // Will cause a publish via the settings change handler below
     localSettings.settings = data.get('body').settings
@@ -29,13 +54,18 @@ export function register(nydus, localSettings, activeGameManager, mapStore) {
   nydus.registerRoute('/site/activateMap', activateMap)
   nydus.registerRoute('/site/getVersion', getVersion)
 
-  localSettings.on('change', () => nydus.publish('/settings', localSettings.settings))
+  localSettings.on('change', async function() {
+    const validPath = await hasValidPath(localSettings.settings)
+    nydus.publish('/settings', localSettings.settings)
+    nydus.publish('/starcraftPathValidity', validPath)
+  })
 }
 
 export function subscribe(nydus, client, activeGameManager, localSettings) {
   nydus.subscribeClient(client, '/game/status', activeGameManager.getStatusForSite())
   nydus.subscribeClient(client, '/game/results')
   nydus.subscribeClient(client, '/settings', localSettings.settings)
+  nydus.subscribeClient(client, '/starcraftPathValidity', lastHadValidPath)
 }
 
 async function getResolution(data, next) {
