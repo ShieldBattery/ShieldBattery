8711bdf2d5032f0a08b9aa7f03d34d0a48242d90
2Pacalypse- <marko@kastela.com>
2016-05-29 20:18:02 +0200

    Fix suggested changes and edit a couple of questions a bit.

--- client/beta/faq.jsx
+++ client/beta/faq.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router'
 import styles from './beta.css'
 
 import LogoText from '../logos/logotext-640x100.svg'
-import QuestionIcon from '../icons/material/ic_help_outline_black_36px.svg'
+import QuestionIcon from '../icons/material/ic_help_outline_black_48px.svg'
 
 const QuestionSection = ({ question, answer }) => {
   return (<div className={styles.faqFeature}>
     <div className={styles.faqText}>
       <QuestionIcon className={styles.faqQuestionIcon} />
       <h3 className={styles.faqQuestion}>{question}</h3>
-      <p className={styles.faqAnswer}>{answer}</p>
+      <div className={styles.faqAnswer}>{answer}</div>
     </div>
   </div>)
 }
@@ -20,38 +20,38 @@ export default class Faq extends React.Component {
     const questions = [
       {
         question: 'When will the open beta start?',
-        answer: <span>
+        answer: [<p key='p1'>
           Open beta should start once we feel the project is ready for public, which means that the
           majority of the crucial bugs that we are aware of are fixed and the major features are all
           implemented and working. Exact ETA on this is unfortunately impossible to give, since
           we're only working on this project in our free time, but expect to receive regular updates
-          from us as we make progress.
-          <br /><br />
-          It's important to note that until the open beta starts, we'll keep inviting new people to
-          the closed beta to increase the variety of people using it and to help us find most of the
-          bugs.
-        </span>,
-      },
-      {
-        question: 'Will the ShieldBattery cost anything? What about donation links?',
-        answer: <span>
-          Of course not. This is a project made purely out of our love for BW; by community members,
-          for community.
-          <br /><br />
-          As for the donation link, there likely won't be one as well. The server costs are not that
-          high for now and we can handle it fine.
-        </span>,
+          from us as we make progress.</p>,
+          <p key='p2'>It's important to note that until the open beta starts, we'll keep inviting
+          new people to the closed beta to increase the variety of people using it and to help us
+          find most of the bugs.
+        </p>],
       },
       {
         question: 'What\'s the status of the closed beta?',
-        answer: <span>
-          It's hard to quantify how much exactly the project is done, since we have so much ideas
-          that we'd like to implement. However, for now we're focusing on our major features and we
-          can proudly say that the most of them are done, which makes ShieldBattery usable today.
-          The current two major features that are under active development are our improved
-          networking code, as well as an auto-matchmaking service. Both of them should be
-          implemented to a certain level fairly soon.
-        </span>,
+        answer: [<p key='p1'>
+          As mentioned in the previous answer, closed beta will last until all the features we
+          initially announced are implemented and working, at least the basic version of them. Two
+          features that are in active development to make that happen are improved networking
+          solution and auto-matchmaking. The former is very close to being done and the latter is
+          about half way through.</p>,
+          <p key='p2'>Besides those two major features, there is a lot of work still needed to be
+          done in fleshing out the UI elements and adding additional functionality to our existing
+          features (eg. support for multiple chat channels).
+        </p>],
+      },
+      {
+        question: 'Will the ShieldBattery cost anything? What about donation links?',
+        answer: [<p key='p1'>
+          Of course not. This is a project made purely out of our love for BW; by community members,
+          for community.</p>,
+          <p key='p2'>As for the donation link, there likely won't be one as well. The server costs
+          are not that high for now and we can handle it fine.
+        </p>],
       },
       {
         question: 'Will the project be open source?',
@@ -86,11 +86,11 @@ export default class Faq extends React.Component {
       },
       {
         question: 'What are system requirements to play on ShieldBattery?',
-        answer: <span>
-          <ul><li>A computer running Windows Vista or later</li>
+        answer: <ul>
+          <li>A computer running Windows Vista or later</li>
           <li>A graphics card that supports either DirectX 10+ or OpenGL 3.1+</li>
-          <li>A modern browser (Chrome, Firefox, Opera, or IE10+ should all work)</li></ul>
-        </span>,
+          <li>A modern browser (Chrome, Firefox, Opera, or IE10+ should all work)</li>
+        </ul>,
       }
     ]
 
