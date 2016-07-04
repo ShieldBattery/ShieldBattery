545e5e9f1f11b979db9468c338b7840c098c16ef
2Pacalypse- <marko@kastela.com>
2016-07-04 14:36:31 +0200

    Move message related components into its own folder.

--- client/chat/channel.jsx
+++ client/chat/channel.jsx
@@ -1,99 +1,12 @@
 import React, { PropTypes } from 'react'
-import ReactDom from 'react-dom'
 import { connect } from 'react-redux'
 import { routeActions } from 'redux-simple-router'
 import { sendMessage, retrieveInitialMessageHistory } from './action-creators'
 import styles from './channel.css'
 
 import ContentLayout from '../content/content-layout.jsx'
-import { ChatMessage, ChatMessageLayout } from './message.jsx'
+import MessageList from '../messaging/message-list.jsx'
 import TextField from '../material/text-field.jsx'
-
-class BaseMessage extends React.Component {
-  static propTypes = {
-    record: PropTypes.object.isRequired,
-  };
-
-  shouldComponentUpdate(nextProps) {
-    return this.record !== nextProps.record
-  }
-}
-
-class UserOnlineMessage extends BaseMessage {
-  render() {
-    const { time, user } = this.props.record
-    return (<ChatMessageLayout time={time} className={styles.systemMessage}>
-      <span>
-        &gt;&gt; <span className={styles.important}>{user}</span> has come online
-      </span>
-    </ChatMessageLayout>)
-  }
-}
-
-class UserOfflineMessage extends BaseMessage {
-  render() {
-    const { time, user } = this.props.record
-    return (<ChatMessageLayout time={time} className={styles.systemMessage}>
-      <span>
-        &lt;&lt; <span className={styles.important}>{user}</span> has gone offline
-      </span>
-    </ChatMessageLayout>)
-  }
-}
-
-class MessageList extends React.Component {
-  static propTypes = {
-    messages: PropTypes.object.isRequired,
-    loading: PropTypes.bool,
-  };
-
-  constructor(props) {
-    super(props)
-    this._shouldAutoScroll = true
-  }
-
-  shouldComponentUpdate(nextProps) {
-    return this.props.messages !== nextProps.messages
-  }
-
-  componentWillUpdate() {
-    const node = ReactDom.findDOMNode(this)
-    this._shouldAutoScroll = (node.scrollTop + node.offsetHeight) >= node.scrollHeight
-  }
-
-  componentDidMount() {
-    this.maybeScrollToBottom()
-  }
-
-  componentDidUpdate() {
-    this.maybeScrollToBottom()
-  }
-
-  maybeScrollToBottom() {
-    if (this._shouldAutoScroll) {
-      const node = ReactDom.findDOMNode(this)
-      node.scrollTop = node.scrollHeight
-    }
-  }
-
-  renderMessage(msg) {
-    const { id, type } = msg
-    switch (type) {
-      case 'message':
-        return <ChatMessage key={id} user={msg.from} time={msg.time} text={msg.text} />
-      case 'userOnline': return <UserOnlineMessage key={id} record={msg} />
-      case 'userOffline': return <UserOfflineMessage key={id} record={msg} />
-      default: return null
-    }
-  }
-
-  render() {
-    return (<div className={styles.messages}>
-      { this.props.loading ? <div>Loading&hellip;</div> : null }
-      { this.props.messages.map(msg => this.renderMessage(msg)) }
-    </div>)
-  }
-}
 
 class UserListEntry extends React.Component {
   static propTypes = {
