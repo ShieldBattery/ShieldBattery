d13997f7f6c274a526c2f5e42cf0b410f35a4b1f
Travis Collins <travis@tec27.com>
2016-08-31 03:13:12 -0700

    Make dialogs scroll properly, add scroll dividers as well.

--- client/material/dialog.jsx
+++ client/material/dialog.jsx
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react'
 import keycode from 'keycode'
 import KeyListener from '../keyboard/key-listener.jsx'
+import { ScrollableContent } from '../material/scroll-bar.jsx'
 import styles from './dialog.css'
 
 const ESCAPE = keycode('esc')
@@ -14,17 +15,28 @@ class Dialog extends React.Component {
     buttons: PropTypes.arrayOf(PropTypes.element),
   };
 
+  state = {
+    scrolledUp: false,
+    scrolledDwon: false,
+  };
+
   render() {
     const { title, showCloseButton, buttons } = this.props
+    const { scrolledUp, scrolledDown } = this.state
     return (<KeyListener onKeyDown={this.onKeyDown}>
       <div key='dialog' className={styles.container}>
         <div className={styles.scrim} onClick={this.onCancel} />
           <div role='dialog' className={styles.contents}>
             <h3 className={styles.title}>{title}</h3>
             { showCloseButton ? <span>X</span> : null }
-            <div className={styles.body}>
-              { this.props.children }
-            </div>
+            { scrolledDown ? <div className={styles.titleDivider} /> : null }
+            <ScrollableContent autoHeight={true} autoHeightMin={'100px'}
+                autoHeightMax={'calc(80vh - 132px)'} onUpdate={this.onScrollUpdate}>
+              <div className={styles.body}>
+                { this.props.children }
+              </div>
+            </ScrollableContent>
+            { scrolledUp ? <div className={styles.actionsDivider} /> : null }
             <div className={styles.actions}>
               {buttons}
             </div>
@@ -46,6 +58,16 @@ class Dialog extends React.Component {
     }
 
     return false
+  };
+
+  onScrollUpdate = values => {
+    const { scrollTop, scrollHeight, clientHeight } = values
+    const scrolledUp = scrollTop + clientHeight < scrollHeight
+    const scrolledDown = scrollTop > 0
+
+    if (scrolledUp !== this.state.scrolledUp || scrolledDown !== this.state.scrolledDown) {
+      this.setState({ scrolledUp, scrolledDown })
+    }
   };
 }
 
