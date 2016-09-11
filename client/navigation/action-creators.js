17b624e01e52e7967bacadb6069cdab6128936d7
2Pacalypse- <marko@kastela.com>
2016-09-12 01:40:36 +0200

    Fix a bug in our goToIndex function when checking the whispers size. (#190)

--- client/navigation/action-creators.js
+++ client/navigation/action-creators.js
@@ -4,13 +4,13 @@ import { routerActions } from 'react-router-redux'
 // don't really have a root content page
 export function goToIndex(transitionFn = routerActions.push) {
   return (dispatch, getState) => {
-    const { lobby, whispers, chat: { channels } } = getState()
+    const { lobby, whispers: { sessions }, chat: { channels } } = getState()
     if (lobby.inLobby) {
       dispatch(transitionFn(`/lobbies/${encodeURIComponent(lobby.info.name)}`))
     } else if (channels.size) {
       dispatch(transitionFn(`/chat/${encodeURIComponent(channels.first())}`))
-    } else if (whispers.size) {
-      dispatch(transitionFn(`/whispers/${encodeURIComponent(whispers.get(0).from)}`))
+    } else if (sessions.size) {
+      dispatch(transitionFn(`/whispers/${encodeURIComponent(sessions.get(0).from)}`))
     } else {
       dispatch(transitionFn('/chat/'))
     }
