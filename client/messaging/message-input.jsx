572ede10dc2ed78347d1459431ec8659d1ce3a3e
2Pacalypse- <marko@kastela.com>
2016-09-19 03:31:13 +0200

    Add a KeyListener around MessageInput so you can always type in chat. (#194)

--- client/messaging/message-input.jsx
+++ client/messaging/message-input.jsx
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react'
 import TextField from '../material/text-field.jsx'
+import KeyListener from '../keyboard/key-listener.jsx'
 
 export default class MessageInput extends React.Component {
   static propTypes = {
@@ -13,10 +14,12 @@ export default class MessageInput extends React.Component {
   render() {
     const { className } = this.props
     const { message } = this.state
-    return (<TextField className={className} label='Send a message' value={message}
-        maxLength={500} floatingLabel={false} allowErrors={false}
-        inputProps={{ autoComplete: 'off' }}
-        onEnterKeyDown={this.onEnterKeyDown} onChange={this.onChange}/>)
+    return (<KeyListener onKeyPress={this.onKeyPress}>
+        <TextField ref='messageInput' className={className} label='Send a message' value={message}
+            maxLength={500} floatingLabel={false} allowErrors={false}
+            inputProps={{ autoComplete: 'off' }}
+            onEnterKeyDown={this.onEnterKeyDown} onChange={this.onChange}/>
+    </KeyListener>)
   }
 
   onChange = e => {
@@ -31,5 +34,20 @@ export default class MessageInput extends React.Component {
       this.props.onSend(this.state.message)
       this.setState({ message: '' })
     }
+  };
+
+  onKeyPress = event => {
+    if (event.ctrlKey || event.altKey) {
+      return false
+    }
+
+    const key = event.key ? event.key : String.fromCharCode(event.charCode)
+    if (key && key.length === 1) {
+      this.refs.messageInput.focus()
+      this.setState({ message: this.state.message + key })
+      return true
+    }
+
+    return false
   };
 }
