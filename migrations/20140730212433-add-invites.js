232b8b65a4b69202762553a80320d228a9ac3b7c
2Pacalypse- <marko@kastela.com>
2016-12-01 10:39:59 +0100

    Add user_ips table which will track user IP addresses over time. (#201)
    Also fixes some lint issues in the migration files that were somehow
    missed.

--- migrations/20140730212433-add-invites.js
+++ migrations/20140730212433-add-invites.js
@@ -1,13 +1,13 @@
 exports.up = function(db, callback) {
   db.createTable('invites',
-      { email: { type: 'string', length: 100, primaryKey: true },
-        token: { type: 'string', length: 50 },
-        teamliquid_name: { type: 'string', length: 20 },
-        os: { type: 'string', length: 50 },
-        browser: { type: 'string', length: 50 },
-        graphics: { type: 'string', length: 50 },
-        can_host: { type: 'boolean' }
-      }, callback)
+    { email: { type: 'string', length: 100, primaryKey: true },
+      token: { type: 'string', length: 50 },
+      teamliquid_name: { type: 'string', length: 20 },
+      os: { type: 'string', length: 50 },
+      browser: { type: 'string', length: 50 },
+      graphics: { type: 'string', length: 50 },
+      can_host: { type: 'boolean' }
+    }, callback)
 }
 
 exports.down = function(db, callback) {
---
+++ migrations/20161130220937-user-ips.js
@@ -0,0 +1,33 @@
+exports.up = function(db, cb) {
+  db.runSql(`
+      CREATE TABLE user_ips (
+        user_id integer NOT NULL,
+        ip_address inet NOT NULL,
+        first_used timestamp without time zone NOT NULL,
+        last_used timestamp without time zone NOT NULL
+      );`, createUserIdIndex)
+
+  function createUserIdIndex(err) {
+    if (err) {
+      cb(err)
+      return
+    }
+
+    db.addIndex('user_ips', 'user_ips_user_id_index', ['user_id'],
+        false /* unique */, createIpAddressIndex)
+  }
+
+  function createIpAddressIndex(err) {
+    if (err) {
+      cb(err)
+      return
+    }
+
+    db.addIndex('user_ips', 'user_ips_ip_address_index', ['ip_address'],
+        false /* unique */, cb)
+  }
+}
+
+exports.down = function(db, cb) {
+  db.dropTable('user_ips', cb)
+}
