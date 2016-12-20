ebcd44771227334151ee261cd0f2e97b3f526d91
2Pacalypse- <marko@kastela.com>
2016-12-20 17:17:56 +0100

    Add a redis migration which saves session IDs of existing sessions.

---
+++ redis_migrations/not-sure-what-to-call-this.js
@@ -0,0 +1,14 @@
+import redis from '../server/redis'
+import config from '../config'
+
+(async () => {
+  const sessionKeys = await redis.keys('koa:sess:*')
+  for (const sessionId of sessionKeys) {
+    const session = JSON.parse(await redis.get(sessionId))
+    const userSessionsKey = 'user_sessions:' + session.userId
+    redis.sadd(userSessionsKey, sessionId)
+    redis.expire(userSessionsKey, config.sessionTtl)
+  }
+
+  redis.quit()
+})()
