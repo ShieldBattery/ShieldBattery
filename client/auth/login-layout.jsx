f64bcd79052151cc96a1e2b80647dda3880a96bb
Travis Collins <travis@tec27.com>
2017-02-07 01:43:21 -0800

    Add window controls to login screens, fix sizing on web browser when no window controls are present.

--- client/auth/login-layout.jsx
+++ client/auth/login-layout.jsx
@@ -3,14 +3,18 @@ import { makeServerUrl } from '../network/server-url'
 import styles from './login.css'
 
 import LogoText from '../logos/logotext-640x100.svg'
+import WindowControls from '../window-controls.jsx'
 
 class MainLayout extends React.Component {
   render() {
-    return (<div className={styles.wrapper}>
-      <img className={styles.logo} src={makeServerUrl('/images/logo.svg')} />
-      <div className={styles.logoText}><LogoText /></div>
-      <div>
-        { this.props.children }
+    return (<div className={styles.background}>
+      <WindowControls className={styles.windowControls}/>
+      <div className={styles.wrapper}>
+        <img className={styles.logo} src={makeServerUrl('/images/logo.svg')} />
+        <div className={styles.logoText}><LogoText /></div>
+        <div>
+          { this.props.children }
+        </div>
       </div>
     </div>)
   }
