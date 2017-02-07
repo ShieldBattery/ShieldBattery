5b436b95feca16e8ad5906caf12af5ab6ea7d2d8
Travis Collins <travis@tec27.com>
2017-02-07 01:47:23 -0800

    Don't redirect electron users to the beta splash page.

--- client/beta/is-returning-user.js
+++ client/beta/is-returning-user.js
@@ -2,5 +2,9 @@ import readCookies from '../network/read-cookies'
 import { isLoggedIn } from '../auth/auth-utils'
 
 export default function isReturningUser(authState) {
-  return isLoggedIn(authState) || readCookies().returning
+  return (
+      process.webpackEnv.SB_ENV === 'electron' ||
+      isLoggedIn(authState) ||
+      readCookies().returning
+  )
 }
