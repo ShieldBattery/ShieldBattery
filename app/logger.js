c46a53c7581ef827c2d75d9428de21d758d32738
Travis Collins <travis@tec27.com>
2017-02-16 14:48:55 -0800

    Make electron logger not eat autoUpdater logs.

--- app/logger.js
+++ app/logger.js
@@ -4,4 +4,4 @@ import { getUserDataPath } from './user-data-path'
 
 const logFile = path.join(getUserDataPath(), 'logs', 'app')
 // TODO(tec27): configure log levels based on build type
-export default logger(logFile, { logLevels: [ 'verbose', 'debug', 'warning', 'error' ] })
+export default logger(logFile, { logLevels: [ 'verbose', 'info', 'debug', 'warning', 'error' ] })
