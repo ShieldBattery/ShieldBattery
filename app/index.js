35efe565be2b105741cd3f22a62758775b52c188
Marko Žarković <marko@kastela.com>
2017-03-26 09:16:35 +0200

    Force the electron app to only open a single instance in non-dev mode. (#261)
    If someone tries to open two instances, we just focus the main window.

--- app/index.js
+++ app/index.js
@@ -2,4 +2,28 @@ process.env.BABEL_ENV = 'app'
 
 require('babel-register')
 require('babel-polyfill')
-require('./app.js')
+
+const isDev = require('electron-is-dev')
+const app = require('electron').app
+
+let getMainWindow
+
+// Ensure that it's only possible to open a single instance of the application in non-dev mode. If
+// someone tries to open two instances, we just focus the main window
+if (!isDev) {
+  const shouldQuit = app.makeSingleInstance(() => {
+    const mainWindow = getMainWindow()
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+
+    return true
+  })
+
+  if (shouldQuit) {
+    app.quit()
+  } else {
+    getMainWindow = require('./app.js').getMainWindow
+  }
+}
