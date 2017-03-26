1a1004af39ab3aa355248f39a59801f0bd14297f
2Pacalypse- <marko@kastela.com>
2017-03-26 23:59:19 +0200

    Use the PNG file instead of ICO for the system tray icon.
    Apparently there is some weirdness/bugs with using .ico files in
    electron, so we should be using the pngs from now on.
    
    See this issue: https://github.com/electron/electron/issues/5844?ts=2

--- app/system-tray.js
+++ app/system-tray.js
@@ -1,11 +1,12 @@
 import { app, Menu, Tray } from 'electron'
+import path from 'path'
 
 export default class SystemTray {
   constructor(mainWindow, quitAppFn) {
     this.mainWindow = mainWindow
     this.onQuitClick = quitAppFn
 
-    this.systemTray = new Tray('icon.ico')
+    this.systemTray = new Tray(path.join(__dirname, 'assets', 'shieldbattery-16.png'))
     this.systemTray.setToolTip('ShieldBattery')
     this.systemTray.setContextMenu(this.buildContextMenu())
     this.systemTray.on('click', this.onTrayClick)
@@ -30,7 +31,8 @@ export default class SystemTray {
     const message =
         'ShieldBattery is running in the background. Right click the system tray icon to quit.'
     this.systemTray.displayBalloon({
-      title: 'Shieldbattery',
+      icon: path.join(__dirname, 'assets', 'shieldbattery-64.png'),
+      title: 'ShieldBattery',
       content: message,
     })
   }
