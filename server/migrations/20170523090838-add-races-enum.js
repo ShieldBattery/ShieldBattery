13a2ac6c745ee0a0761ea720fab651cd150a4725
Marko Žarković <marko@kastela.com>
2017-05-23 11:34:27 +0200

    Add race enum to the database. (#345)

---
+++ server/migrations/20170523090838-add-races-enum.js
@@ -0,0 +1,13 @@
+exports.up = async function(db) {
+  await db.runSql(`
+    CREATE TYPE race AS ENUM ('zerg', 'terran', 'protoss', 'random');
+  `)
+}
+
+exports.down = async function(db) {
+  await db.runSql('DROP TYPE race;')
+}
+
+exports._meta = {
+  version: 1
+}
