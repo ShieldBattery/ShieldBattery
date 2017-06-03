f7404fd66c93dc1fa6c5f4afa2fae3c12325797b
2Pacalypse- <marko@kastela.com>
2017-06-03 18:56:09 +0200

    Fix suggested changes.

--- server/lib/api/matchmakingMapPools.js
+++ server/lib/api/matchmakingMapPools.js
@@ -11,18 +11,29 @@ import {
 
 export default function(router) {
   router.get('/:matchmakingType', ensureLoggedIn, getHistory)
-    .get('/current/:matchmakingType', ensureLoggedIn, getCurrent)
+    .get('/:matchmakingType/current', ensureLoggedIn, getCurrent)
     .post('/:matchmakingType', ensureLoggedIn, checkAllPermissions('manageMapPools'), setNewMapPool)
 }
 
 async function getHistory(ctx, next) {
   const { matchmakingType } = ctx.params
+  let { limit, page: pageNumber } = ctx.query
 
   if (!isValidMatchmakingType(matchmakingType)) {
     throw new httpErrors.BadRequest('invalid matchmaking type')
   }
 
-  const mapPoolHistory = await getMapPoolHistory(matchmakingType)
+  limit = parseInt(limit, 10)
+  if (!limit || isNaN(limit) || limit < 0 || limit > 100) {
+    limit = 10
+  }
+
+  pageNumber = parseInt(pageNumber, 10)
+  if (!pageNumber || isNaN(pageNumber) || pageNumber < 0) {
+    pageNumber = 0
+  }
+
+  const mapPoolHistory = await getMapPoolHistory(matchmakingType, limit, pageNumber)
   if (!mapPoolHistory) {
     throw new httpErrors.NotFound('no matchmaking map pool history for this type')
   }
@@ -35,16 +46,22 @@ async function getHistory(ctx, next) {
 async function setNewMapPool(ctx, next) {
   const { matchmakingType } = ctx.params
   const { maps } = ctx.request.body
+  let { startDate } = ctx.request.body
+
+  if (startDate) {
+    startDate = new Date(startDate)
+  }
 
   if (!isValidMatchmakingType(matchmakingType)) {
     throw new httpErrors.BadRequest('invalid matchmaking type')
-  } else if (!maps || maps instanceof Array && maps.length < 1) {
+  } else if (!Array.isArray(maps) || Array.isArray(maps) && maps.length < 1) {
     throw new httpErrors.BadRequest('maps must be specified')
+  } else if (startDate && startDate.toString() === 'Invalid Date') {
+    throw new httpErrors.BadRequest('startDate must be a valid date')
   }
 
-  await addMapPool(matchmakingType, maps)
-  ctx.status = 201
-  ctx.body = {}
+  await addMapPool(matchmakingType, maps, startDate)
+  ctx.status = 204
 }
 
 async function getCurrent(ctx, next) {
--- server/lib/models/matchmaking-map-pools.js
+++ server/lib/models/matchmaking-map-pools.js
@@ -1,13 +1,15 @@
 import db from '../db'
 
-export async function getMapPoolHistory(matchmakingType) {
+export async function getMapPoolHistory(matchmakingType, limit, pageNumber) {
   const query = `
     SELECT start_date, maps
     FROM matchmaking_map_pools
     WHERE matchmaking_type = $1
     ORDER BY start_date
+    LIMIT $2
+    OFFSET $3
   `
-  const params = [matchmakingType]
+  const params = [matchmakingType, limit, pageNumber * limit]
 
   const { client, done } = await db()
   try {
@@ -23,13 +25,13 @@ export async function getMapPoolHistory(matchmakingType) {
   }
 }
 
-export async function addMapPool(matchmakingType, maps) {
+export async function addMapPool(matchmakingType, maps, startDate = new Date()) {
   const query = `
     INSERT INTO matchmaking_map_pools (matchmaking_type, start_date, maps)
     VALUES ($1, $2, $3)
   `
   const mapsHashes = maps.map(m => Buffer.from(m, 'hex'))
-  const params = [matchmakingType, new Date(), mapsHashes]
+  const params = [matchmakingType, startDate, mapsHashes]
 
   const { client, done } = await db()
   try {
@@ -45,6 +47,7 @@ export async function getCurrentMapPool(matchmakingType) {
     FROM matchmaking_map_pools
     WHERE matchmaking_type = $1
     ORDER BY start_date DESC
+    LIMIT 1
   `
   const params = [matchmakingType]
 
