df3145f5e7c199844b70bb9970fbc0742c5b7e80
Marko Žarković <marko@kastela.com>
2018-02-03 21:42:42 +0100

    Move ActivityOverlay to styled components. (#390)
    * Move ActivityOverlay to styled components.
    
    This was mostly a test to see how styled components played with React's
    CSS Transition Group. The answer is very nice!
    
    This commit also adds a JS version of few CSS-related common features,
    mainly:
    - curve constants
    - shadows
    - z-index
    
    * Use `css` helper in styled-components instead of bare text.

---
+++ client/material/curve-constants.js
@@ -0,0 +1,5 @@
+import { css } from 'styled-components'
+
+export const fastOutSlowIn = css`cubic-bezier(.4, 0, .2, 1)`
+export const linearOutSlowIn = css`cubic-bezier(0, 0, .2, 1)`
+export const fastOutLinearIn = css`cubic-bezier(.4, 0, 1, 1)`
