e367e8ff9338d20c8d22a7c1c7f3cd73ff1054ae
Markus Heikkinen <ittevien@gmail.com>
2019-05-16 14:34:38 +0300

    Add render skipping from the c++ code

--- game/src/forge/renderer.rs
+++ game/src/forge/renderer.rs
@@ -1,3 +1,7 @@
+use std::mem;
+use std::time::{Duration, Instant};
+use std::ptr::null_mut;
+
 use winapi::shared::windef::{HWND};
 use winapi::um::wingdi::PALETTEENTRY;
 
@@ -8,6 +12,8 @@ use super::Settings;
 pub struct Renderer {
     renderer: Option<Box<dyn RenderApi>>,
     indirect_draw: Option<*mut IndirectDraw>,
+    last_render: Instant,
+    min_frame_delay: Duration,
 }
 
 pub trait RenderApi {
@@ -15,15 +21,51 @@ pub trait RenderApi {
     fn render(&mut self, pixels: &[u8]);
 }
 
+fn device_display_frequency() -> Option<u32> {
+    use winapi::um::wingdi::DEVMODEW;
+    use winapi::um::winuser::{ENUM_CURRENT_SETTINGS, EnumDisplaySettingsW};
+    unsafe {
+        let mut devmode = DEVMODEW {
+            dmSize: mem::size_of::<DEVMODEW>() as u16,
+            ..mem::zeroed()
+        };
+        let success = EnumDisplaySettingsW(null_mut(), ENUM_CURRENT_SETTINGS, &mut devmode);
+        if success != 0 {
+            Some(devmode.dmDisplayFrequency).filter(|&x| x > 1)
+        } else {
+            warn!("Couldn't get display settings: {}", std::io::Error::last_os_error());
+            None
+        }
+    }
+}
+
 impl Renderer {
     pub fn new() -> Renderer {
+        // If we can't get display frequency, or it is below 60hz for some reason,
+        // still assume 60hz.
+        let display_frequency = device_display_frequency()
+            .unwrap_or(60)
+            .max(60);
         Renderer {
             renderer: None,
             indirect_draw: None,
+            last_render: Instant::now(),
+            // We allow updates 4x as fast, as BW makes no attempt to sync to vblank.
+            // This means that if we hit a two or more frames in the same vblank range,
+            // one at the beginning and one at the end, we would skip the second one and
+            // be stuck waiting until BW comes back around and calls us
+            // (which might be longer than the *next* vblank window, thus skipping a frame).
+            min_frame_delay: Duration::from_secs(1) / display_frequency / 4,
         }
     }
 
     pub fn render(&mut self) {
+        let now = Instant::now();
+        if now.duration_since(self.last_render) < self.min_frame_delay {
+            return;
+        }
+        self.last_render = now;
+
         let renderer = match self.renderer {
             Some(ref mut s) => s,
             None => return,
