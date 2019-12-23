fbfb3552e7a5688195171979685972a6df60518a
2Pacalypse- <marko@kastela.com>
2019-12-23 19:06:41 +0100

    Fix an annoying issue where some libraries might require `fs` in browsers
    I honestly have no idea which library is even doing this, but since
    it started to be an issue from the last commit where
    `react-transition-group` was added, I will assume it is that one. Or one
    of *its* dependencies.
    
    In any case, it's really annoying and this "fix" doesn't seem optimal to
    me; I'm not even sure if it didn't break anything else (from my limited
    testing, it didn't). See this github issue for a bit more info:
    https://github.com/tailwindcss/tailwindcss/issues/1129

--- common.webpack.config.js
+++ common.webpack.config.js
@@ -50,6 +50,9 @@ export default function({
   const config = {
     ...webpackOpts,
     mode: isProd ? 'production' : 'development',
+    // NOTE(2Pac): This is required because some libraries might not use their `browser` field
+    // properly.
+    node: { fs: 'empty' },
     context: __dirname,
     module: {
       rules: [
@@ -119,7 +122,7 @@ export default function({
 
   if (!isProd) {
     // Allow __filename usage in our files in dev
-    config.node = { __filename: true, __dirname: true }
+    config.node = { ...config.node, __filename: true, __dirname: true }
     config.devtool = 'cheap-module-eval-source-map'
 
     if (hotUrl) {
