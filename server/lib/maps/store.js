a4d30bc1b53b9a32f79bed7089332feb4282ff59
Marko Žarković <marko@kastela.com>
2020-08-20 19:33:00 +0200

    Implement a promise-based queue and use it for map parsing. (#497)
    * Implement a promise-based queue and use it for map parsing.
    
    * Throw an error if SB_MAP_PARSER_MAX_CONCURRENT is incorrectly set.

--- server/lib/maps/store.js
+++ server/lib/maps/store.js
@@ -1,16 +1,22 @@
 import childProcess from 'child_process'
 import fs from 'fs'
+import bl from 'bl'
 import { addMap } from '../models/maps'
 import { writeFile } from '../file-upload'
-import bl from 'bl'
+import Queue from '../../../common/async/promise-queue'
 
 const BW_DATA_PATH = process.env.SB_SPRITE_DATA || ''
+const MAX_CONCURRENT = Number(process.env.SB_MAP_PARSER_MAX_CONCURRENT)
+if (Number.isNaN(MAX_CONCURRENT)) {
+  throw new Error('SB_MAP_PARSER_MAX_CONCURRENT must be a number')
+}
+const mapQueue = new Queue(MAX_CONCURRENT)
 
 // Takes both a parsed chk which it pulls metadata from,
 // and the temppath of compressed mpq, which will be needed
 // when the map is actually stored somewhere.
 export async function storeMap(path, extension, uploadedBy, visibility) {
-  const { mapData, imageStream } = await mapParseWorker(path, extension)
+  const { mapData, imageStream } = await mapQueue.addToQueue(() => mapParseWorker(path, extension))
   const { hash } = mapData
 
   const mapParams = { mapData, extension, uploadedBy, visibility }
