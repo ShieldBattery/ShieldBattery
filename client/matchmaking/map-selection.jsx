2d459be38d31f1a446a20456808e252d0e98cca0
Travis Collins <travis@tec27.com>
2020-12-23 04:09:36 -0800

    Remove an unused import.

--- client/matchmaking/map-selection.jsx
+++ client/matchmaking/map-selection.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled, { css, keyframes } from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { rgba } from 'polished'
 
 import MapThumbnail from '../maps/map-thumbnail.jsx'
