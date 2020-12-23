6dea9b25b57abea0b78ffc3ef3bf1061627e39d4
Travis Collins <travis@tec27.com>
2020-12-23 03:57:19 -0800

    Clean up styling on matchmaking map pool screen.
    - Add a glow effect that doesn't look completely terrible (now animated
    kind of haphazardly!)
    - Use updated text styles (these also apply to the map browser UIs and
    seem decent there)
    - Make the Random Map thing vertically centered (this was already done
    elsewhere, I just copied that here)

--- client/maps/map-thumbnail.jsx
+++ client/maps/map-thumbnail.jsx
@@ -10,7 +10,7 @@ import Menu from '../material/menu/menu.jsx'
 import MenuItem from '../material/menu/item.jsx'
 
 import { colorTextPrimary, colorTextSecondary, amberA100, grey800 } from '../styles/colors'
-import { SubheadingOld, singleLine } from '../styles/typography'
+import { subtitle2, singleLine } from '../styles/typography'
 
 import FavoritedIcon from '../icons/material/baseline-star-24px.svg'
 import ImageIcon from '../icons/material/baseline-image-24px.svg'
@@ -115,9 +115,10 @@ const TextProtection = styled.div`
   background-color: rgba(0, 0, 0, 0.7);
 `
 
-const MapName = styled(SubheadingOld)`
-  color: ${colorTextPrimary};
+const MapName = styled.div`
+  ${subtitle2};
   ${singleLine};
+  color: ${colorTextPrimary};
 `
 
 const MapActionButton = styled(IconButton)`
--- client/matchmaking/map-selection.jsx
+++ client/matchmaking/map-selection.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
 import { rgba } from 'polished'
 
 import MapThumbnail from '../maps/map-thumbnail.jsx'
 
 import RandomIcon from '../icons/material/ic_casino_black_24px.svg'
 
+import { fastOutLinearIn } from '../material/curve-constants'
 import { shadowDef2dp } from '../material/shadow-constants'
-import { blue200, grey800 } from '../styles/colors'
-import { Display3Old, SubheadingOld } from '../styles/typography'
+import { amberA400, grey800 } from '../styles/colors'
+import { Headline2, Subtitle2 } from '../styles/typography'
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  padding-top: 40px;
 `
 
 const MapsContainer = styled.div`
@@ -22,40 +25,64 @@ const MapsContainer = styled.div`
   grid-template-columns: auto auto;
   grid-template-rows: auto auto;
   grid-gap: 32px;
-  margin-top: 40px;
+  margin-top: 80px;
+`
+
+const pulse = keyframes`
+  0%, 20%, 80%, 100% {
+    opacity: 1;
+  }
+
+  50% {
+    opacity: 0;
+  }
 `
 
 const MapContainer = styled.div`
+  position: relative;
   width: 256px;
   height: 256px;
+  background-color: ${grey800};
   border-radius: 2px;
   box-shadow: ${shadowDef2dp};
 
-  ${props => {
-    if (props.glowing) {
-      return `
-        box-shadow: 0px 0px 32px 8px ${rgba(blue200, 0.5)};
-      `
-    }
+  &::after {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    content: '';
 
-    return ''
-  }}
+    border-radius: 2px;
+    border: 2px solid ${props => rgba(amberA400, props.selected ? 0.7 : 0)};
+    box-shadow: ${props =>
+      props.selected
+        ? `inset 0px 0px 16px -10px ${rgba(amberA400, 0.7)},
+           inset 0px 0px 14px 0px ${rgba(amberA400, 0.24)};`
+        : 'none'};
+
+    animation: ${pulse} 3s ${fastOutLinearIn} infinite;
+    transition: opacity;
+  }
 `
 
 const RandomThumbnail = styled.div`
+  position: absolute;
+  left: 0;
+  top: 44px;
+  width: 100%;
+
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-  background-color: ${grey800};
 `
 
 const StyledRandomIcon = styled(RandomIcon)`
   width: 128px;
   height: 128px;
   opacity: 0.5;
+  margin-bottom: 24px;
 `
 
 export default class MapSelection extends React.Component {
@@ -69,17 +96,17 @@ export default class MapSelection extends React.Component {
     const { preferredMaps, randomMaps, chosenMap } = this.props
     const preferredMapsItems = preferredMaps.map(m => {
       return (
-        <MapContainer key={m.id} glowing={m.id === chosenMap.id}>
+        <MapContainer key={m.id} selected={m.id === chosenMap.id}>
           <MapThumbnail map={m} showMapName={true} />
         </MapContainer>
       )
     })
     const randomMapsItems = randomMaps.map(m => {
       return (
-        <MapContainer key={m.id} glowing={m.id === chosenMap.id}>
+        <MapContainer key={m.id} selected={m.id === chosenMap.id}>
           <RandomThumbnail>
             <StyledRandomIcon />
-            <SubheadingOld>Random map</SubheadingOld>
+            <Subtitle2>Random map</Subtitle2>
           </RandomThumbnail>
         </MapContainer>
       )
@@ -87,7 +114,7 @@ export default class MapSelection extends React.Component {
 
     return (
       <Container>
-        <Display3Old>Map pool</Display3Old>
+        <Headline2>Map pool</Headline2>
         <MapsContainer>
           {preferredMapsItems}
           {randomMapsItems}
