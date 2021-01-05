49d5a5e1cceea7e7e76a8d2968cc1922cbdc1b84
Travis Collins <travis@tec27.com>
2021-01-05 00:25:15 -0800

    Use process.env more safely with Webpack 5.

--- client/network/server-url.js
+++ client/network/server-url.js
@@ -1,4 +1,4 @@
-const baseUrl = process.env.SB_SERVER ? process.env.SB_SERVER : process.webpackEnv.SB_SERVER
+const baseUrl = process?.env?.SB_SERVER ? process.env.SB_SERVER : process.webpackEnv.SB_SERVER
 
 // Returns an absolute server URL for a path, if necessary (if running in Electron). If it's not
 // necessary (in the browser), the path will be returned unmodified
