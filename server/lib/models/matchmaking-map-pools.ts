ff2a1193f2d0845ae95c1f7da803ab4b0d01e9da
Travis Collins <travis@tec27.com>
2021-01-04 22:10:35 -0800

    Convert matchmaking-map-pools to typescript.

--- server/lib/models/matchmaking-map-pools.ts
+++ server/lib/models/matchmaking-map-pools.ts
@@ -1,16 +1,35 @@
 import db from '../db'
 import sql from 'sql-template-strings'
+import { MatchmakingType } from '../../../common/matchmaking'
 
-class MapPool {
-  constructor(props) {
-    this.id = props.id
-    this.type = props.matchmaking_type
-    this.startDate = props.start_date
-    this.maps = props.maps || []
-  }
+interface DbMapPool {
+  /* eslint-disable camelcase */
+  id: string
+  matchmaking_type: MatchmakingType
+  start_date: Date
+  maps: string[]
+  /* eslint-enable camelcase */
 }
 
-async function getMapPoolsCount(type) {
+export interface MapPool {
+  id: string
+  type: MatchmakingType
+  startDate: Date
+  maps: string[]
+}
+
+function convertFromDb(dbMapPool: DbMapPool): MapPool {
+  /* eslint-disable camelcase */
+  return {
+    id: dbMapPool.id,
+    type: dbMapPool.matchmaking_type,
+    startDate: dbMapPool.start_date,
+    maps: dbMapPool.maps,
+  }
+  /* eslint-enable camelcase */
+}
+
+async function getMapPoolsCount(type: MatchmakingType): Promise<number> {
   const query = sql`
     SELECT COUNT(*)
     FROM matchmaking_map_pools
@@ -26,7 +45,11 @@ async function getMapPoolsCount(type) {
   }
 }
 
-export async function getMapPoolHistory(matchmakingType, limit, pageNumber) {
+export async function getMapPoolHistory(
+  matchmakingType: MatchmakingType,
+  limit: number,
+  pageNumber: number,
+): Promise<{ mapPools: MapPool[]; total: number }> {
   const query = sql`
     SELECT *
     FROM matchmaking_map_pools
@@ -42,14 +65,18 @@ export async function getMapPoolHistory(matchmakingType, limit, pageNumber) {
       getMapPoolsCount(matchmakingType),
       client.query(query),
     ])
-    const mapPools = result.rows.length > 0 ? result.rows.map(row => new MapPool(row)) : []
+    const mapPools = result.rows.length > 0 ? result.rows.map(row => convertFromDb(row)) : []
     return { mapPools, total }
   } finally {
     done()
   }
 }
 
-export async function addMapPool(matchmakingType, mapIds, startDate) {
+export async function addMapPool(
+  matchmakingType: MatchmakingType,
+  mapIds: string[],
+  startDate: Date,
+): Promise<MapPool> {
   const query = sql`
     INSERT INTO matchmaking_map_pools (matchmaking_type, start_date, maps)
     VALUES (${matchmakingType}, ${startDate}, ${mapIds})
@@ -59,13 +86,13 @@ export async function addMapPool(matchmakingType, mapIds, startDate) {
   const { client, done } = await db()
   try {
     const result = await client.query(query)
-    return new MapPool(result.rows[0])
+    return convertFromDb(result.rows[0])
   } finally {
     done()
   }
 }
 
-export async function getCurrentMapPool(matchmakingType) {
+export async function getCurrentMapPool(matchmakingType: MatchmakingType): Promise<MapPool | null> {
   const query = sql`
     SELECT *
     FROM matchmaking_map_pools
@@ -77,13 +104,13 @@ export async function getCurrentMapPool(matchmakingType) {
   const { client, done } = await db()
   try {
     const result = await client.query(query)
-    return result.rows.length > 0 ? new MapPool(result.rows[0]) : null
+    return result.rows.length > 0 ? convertFromDb(result.rows[0]) : null
   } finally {
     done()
   }
 }
 
-export async function getMapPoolById(mapPoolId) {
+export async function getMapPoolById(mapPoolId: number): Promise<MapPool | null> {
   const query = sql`
     SELECT *
     FROM matchmaking_map_pools
@@ -93,13 +120,13 @@ export async function getMapPoolById(mapPoolId) {
   const { client, done } = await db()
   try {
     const result = await client.query(query)
-    return result.rows.length > 0 ? new MapPool(result.rows[0]) : null
+    return result.rows.length > 0 ? convertFromDb(result.rows[0]) : null
   } finally {
     done()
   }
 }
 
-export async function removeMapPool(mapPoolId) {
+export async function removeMapPool(mapPoolId: number): Promise<void> {
   const query = sql`
     DELETE FROM matchmaking_map_pools
     WHERE id = ${mapPoolId};
