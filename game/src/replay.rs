ba27dc0a11e676941ba8cd98b4abf6ffe8084ad5
Markus Heikkinen <ittevien@gmail.com>
2021-01-07 18:41:38 +0200

    Changing the replay metadata not have a variable-length version string
    So that it doesn't have to be handled if reading unrelated things.

--- game/src/replay.rs
+++ game/src/replay.rs
@@ -51,14 +51,15 @@ pub unsafe fn add_shieldbattery_data(
     // Current format: (The first two u32s are required by SC:R, after that we can have anything)
     // u32 section_id
     // u32 data_length (Not counting these first 8 bytes)
-    // u16 format_version (0)
-    // u32 starcraft_exe_build
-    // u8 shieldbattery_version_length
-    // u8 shieldbattery_version_string[..]
-    // u8 team_game_main_players[4]
+    // 0x0      u16 format_version (0)
+    // 0x2      u32 starcraft_exe_build
+    //      This is somewhat redundant as GCFG section that SC:R writes by default has it too,
+    //      but may as well have a copy we control.
+    // 0x6      u8 shieldbattery_version_string[0x10]
+    // 0x16     u8 team_game_main_players[4]
     //      Think that relying on it being [0, 0, 0, 0] for non-team games is ok?
     //      BW shouldn't care at least.
-    // u8 starting_races[0xc]
+    // 0x1a     u8 starting_races[0xc]
     //      This is also needed for team game replays.
     let game = bw.game();
     let mut buffer = Vec::with_capacity(32);
@@ -67,9 +68,11 @@ pub unsafe fn add_shieldbattery_data(
     buffer.write_u16::<LE>(0)?;
     buffer.write_u32::<LE>(exe_build)?;
     let version = env!("SHIELDBATTERY_VERSION").as_bytes();
-    let length = version.len().min(255) as u8;
-    buffer.push(length);
-    buffer.extend_from_slice(&version[..length as usize]);
+    let mut version_buf = [0u8; 16];
+    for (out, val) in version_buf.iter_mut().zip(version.iter()) {
+        *out = *val;
+    }
+    buffer.extend_from_slice(&version_buf[..]);
     let team_game_main_players = (*game).team_game_main_player;
     buffer.extend_from_slice(&team_game_main_players);
     let starting_races = (*game).starting_races;
@@ -85,11 +88,8 @@ pub fn parse_shieldbattery_data(data: &[u8]) -> Option<SbatReplayData> {
     if format != 0 {
         return None;
     }
-    let &version_string_length = data.get(6)?;
-    let mut offset = 7 + version_string_length as usize;
-    let team_game_main_players = data.get(offset..)?.get(..4)?;
-    offset += 4;
-    let starting_races = data.get(offset..)?.get(..0xc)?;
+    let team_game_main_players = data.get(0x16..)?.get(..4)?;
+    let starting_races = data.get(0x1a..)?.get(..0xc)?;
     Some(SbatReplayData {
         team_game_main_players: team_game_main_players.try_into().ok()?,
         starting_races: starting_races.try_into().ok()?,
