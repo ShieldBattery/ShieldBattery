f21066e992f1978fefa36c8d6a5f7f1b1fabeebf
GrantJenk <43507279+GrantJenk@users.noreply.github.com>
2021-01-26 23:16:57 -0600

    Change opacity of offline users (#587)

--- client/chat/channel.jsx
+++ client/chat/channel.jsx
@@ -21,7 +21,7 @@ import MessageList from '../messaging/message-list'
 import MenuItem from '../material/menu/item'
 import UserProfileOverlay from '../profile/user-profile-overlay'
 import { ScrollableContent } from '../material/scroll-bar'
-import { colorDividers, colorTextSecondary } from '../styles/colors'
+import { colorDividers, colorTextSecondary, alphaDisabled } from '../styles/colors'
 import { body2, overline, singleLine } from '../styles/typography'
 
 import { MULTI_CHANNEL } from '../../common/flags'
@@ -71,6 +71,18 @@ const UserSublist = styled.ul`
   list-style-type: none;
   margin: 0;
   padding: 0;
+
+  ${props => {
+    if (props.faded) {
+      return `
+        color: ${colorTextSecondary};
+        ${StyledAvatar} {
+          opacity: ${alphaDisabled};
+        }
+      `
+    }
+    return ''
+  }}
 `
 
 const UserListEntryItem = styled.li`
@@ -174,12 +186,14 @@ class UserList extends React.Component {
       return null
     }
 
+    const faded = title === 'Offline'
+
     return (
       <UserListSection>
         <UserListOverline>
           {title} ({users.size})
         </UserListOverline>
-        <UserSublist>
+        <UserSublist faded={faded}>
           {users.map(u => (
             <UserListEntry user={u} key={u} onWhisperClick={this.props.onWhisperClick} />
           ))}
