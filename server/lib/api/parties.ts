e4048e766b60aa1c989be8064f2d5ff1d9b43713
2Pacalypse- <marko@kastela.com>
2021-02-07 22:01:45 +0100

    Add the beginnings of a party system.
    This PR adds the server-side party service (and API handlers) for our
    party system. Currently, only the following three actions are
    implemented:
      - Send an invite to someone
      - Decline an invite (or cancel an invite you sent)
      - Accept an invite
    
    Not sure how many more actions will be added to this PR, before we
    decide it's ok to merge it. But I'm guessing at least the "Leave the
    party" action as well.
    
    NOTE: Some things will most likely change since we still haven't defined
    a clear flowchart for all the possible scenarios, nor did we settle on
    a UI/UX yet.

---
+++ server/lib/api/parties.ts
@@ -0,0 +1,163 @@
+import Router, { RouterContext } from '@koa/router'
+import httpErrors from 'http-errors'
+import Joi from 'joi'
+import { container } from 'tsyringe'
+import { USERNAME_MAXLENGTH, USERNAME_MINLENGTH, USERNAME_PATTERN } from '../../../common/constants'
+import users from '../models/users'
+import PartyService, { PartyServiceError, PartyUser } from '../parties/party-service'
+import ensureLoggedIn from '../session/ensure-logged-in'
+import createThrottle from '../throttle/create-throttle'
+import throttleMiddleware from '../throttle/middleware'
+import { joiValidator } from '../validation/joi-validator'
+
+const partyService = container.resolve(PartyService)
+
+const invitesThrottle = createThrottle('partyInvites', {
+  rate: 40,
+  burst: 60,
+  window: 60000,
+})
+
+const partyThrottle = createThrottle('parties', {
+  rate: 20,
+  burst: 40,
+  window: 60000,
+})
+
+const clientIdSchema = Joi.object().keys({
+  clientId: Joi.string().required(),
+})
+
+const invitesPostchema = clientIdSchema.keys({
+  targets: Joi.array()
+    .items(
+      Joi.string()
+        .min(USERNAME_MINLENGTH)
+        .max(USERNAME_MAXLENGTH)
+        .pattern(USERNAME_PATTERN)
+        .required(),
+    )
+    .min(1)
+    .required(),
+})
+
+const partyIdSchema = Joi.object().keys({
+  partyId: Joi.string().required(),
+})
+
+const declineParamsSchema = partyIdSchema.keys({
+  targetId: Joi.number(),
+})
+
+export default function (router: Router) {
+  router
+    .use(ensureLoggedIn)
+    .post(
+      '/invites',
+      throttleMiddleware(invitesThrottle, ctx => ctx.session!.userId),
+      joiValidator({ body: invitesPostchema }),
+      invite,
+    )
+    .delete(
+      '/invites/:partyId/:targetId',
+      throttleMiddleware(invitesThrottle, ctx => ctx.session!.userId),
+      joiValidator({ params: declineParamsSchema }),
+      decline,
+    )
+    .post(
+      '/:partyId',
+      throttleMiddleware(partyThrottle, ctx => ctx.session!.userId),
+      joiValidator({ params: partyIdSchema }),
+      accept,
+    )
+}
+
+function isPartyServiceError(error: Error): error is PartyServiceError {
+  return error.hasOwnProperty('code')
+}
+
+async function invite(ctx: RouterContext) {
+  const { clientId, targets } = ctx.request.body
+
+  try {
+    const invites = await Promise.all(
+      targets.map(
+        async (target: string): Promise<PartyUser> => {
+          const foundTarget = await users.find(target)
+          if (!foundTarget) {
+            throw new httpErrors.NotFound('Target user not found')
+          }
+
+          // TODO(2Pac): Check if the target user has blocked invitations from the user issuing the
+          // request. Or potentially use friends list when implemented.
+
+          return new PartyUser({ id: foundTarget.id, name: foundTarget.name })
+        },
+      ),
+    )
+
+    const leader = new PartyUser({
+      id: ctx.session!.userId,
+      name: ctx.session!.userName,
+    })
+
+    partyService.invite(leader, clientId, invites as PartyUser[])
+
+    ctx.status = 204
+  } catch (err) {
+    if (isPartyServiceError(err)) {
+      throw new httpErrors[err.code](err.message)
+    } else {
+      throw err
+    }
+  }
+}
+
+async function decline(ctx: RouterContext) {
+  const { partyId, targetId } = ctx.params
+
+  try {
+    let leader
+    let target = new PartyUser({ id: ctx.session!.userId, name: ctx.session!.userName })
+
+    // If the `targetId` is provided, that means the user issuing the request wants to cancel an
+    // invite to someone else.
+    if (targetId) {
+      const foundTarget = await users.find(targetId)
+      if (!foundTarget) {
+        throw new httpErrors.NotFound('Target user not found')
+      }
+
+      leader = target
+      target = new PartyUser({ id: foundTarget.id, name: foundTarget.name })
+    }
+
+    partyService.removeInvite(partyId, target, leader)
+
+    ctx.status = 204
+  } catch (err) {
+    if (isPartyServiceError(err)) {
+      throw new httpErrors[err.code](err.message)
+    } else {
+      throw err
+    }
+  }
+}
+
+async function accept(ctx: RouterContext) {
+  const { partyId } = ctx.params
+  const { clientId } = ctx.request.body
+
+  try {
+    const user = new PartyUser({ id: ctx.session!.userId, name: ctx.session!.userName })
+    partyService.acceptInvite(partyId, user, clientId)
+
+    ctx.status = 204
+  } catch (err) {
+    if (isPartyServiceError(err)) {
+      throw new httpErrors[err.code](err.message)
+    } else {
+      throw err
+    }
+  }
+}
---
+++ server/lib/parties/party-service.ts
@@ -0,0 +1,217 @@
+import cuid from 'cuid'
+import { Map, Record } from 'immutable'
+import { NydusServer } from 'nydus'
+import { singleton } from 'tsyringe'
+import {
+  ClientSocketsGroup,
+  ClientSocketsManager,
+  UserSocketsGroup,
+  UserSocketsManager,
+} from '../websockets/socket-groups'
+
+const MAX_PARTY_SIZE = 8
+
+export class PartyUser extends Record({
+  id: null as number | null,
+  name: null as string | null,
+}) {}
+
+export class PartyRecord extends Record({
+  id: null as string | null,
+  invites: Map<number | null, PartyUser>(),
+  members: Map<number | null, PartyUser>(),
+  leader: null as number | null,
+}) {}
+
+enum ErrorEnum {
+  Unauthorized = 'Unauthorized',
+  Forbidden = 'Forbidden',
+  NotFound = 'NotFound',
+  Conflict = 'Conflict',
+}
+
+export class PartyServiceError extends Error {
+  public code: ErrorEnum
+
+  constructor(code: ErrorEnum, message: string) {
+    super(message)
+    this.code = code
+  }
+}
+
+function getInvitesPath(partyId: string): string {
+  return `/parties/invites/${partyId}`
+}
+
+function getPartyPath(partyId: string): string {
+  return `/parties/invites/${partyId}`
+}
+
+@singleton()
+export default class PartyService {
+  private parties = Map<string, PartyRecord>()
+  private clientToPartyId = Map<ClientSocketsGroup, string>()
+
+  constructor(
+    private nydus: NydusServer,
+    private clientSockets: ClientSocketsManager,
+    private userSockets: UserSocketsManager,
+  ) {}
+
+  invite(leader: PartyUser, leaderClientId: string, invites: PartyUser[]) {
+    const leaderClient = this.getClient(leader.id as number, leaderClientId)
+
+    let party = this.getClientParty(leaderClient)
+    if (party) {
+      if (party.leader !== leader.id) {
+        throw new PartyServiceError(ErrorEnum.Forbidden, 'Only party leader can invite people')
+      }
+
+      const updatedParty = party.mergeIn('invites', Map(invites.map(i => [i.id, i])))
+      this.parties = this.parties.set(party.id as string, updatedParty)
+
+      this.publishToParty(party.id as string, {
+        type: 'invite',
+        invites,
+      })
+    } else {
+      const partyId = cuid()
+      party = new PartyRecord({
+        id: partyId,
+        invites: Map(invites.map(i => [i.id, i])),
+        members: Map([[leader.id, leader]]),
+        leader: leader.id,
+      })
+
+      this.parties = this.parties.set(partyId, party)
+      this.clientToPartyId = this.clientToPartyId.set(leaderClient, partyId)
+      this.subscribeToParty(leaderClient, party)
+    }
+
+    const inviteUsers = invites.map(i => this.getUser(i.name as string))
+    inviteUsers.forEach(user => {
+      user.subscribe(
+        getInvitesPath(party!.id as string),
+        () => ({
+          action: 'invite',
+          from: party!.leader,
+        }),
+        () => {
+          // TODO(2Pac): Handle user quitting; need to keep a map of user -> invites?
+        },
+      )
+    })
+  }
+
+  removeInvite(partyId: string, target: PartyUser, leader?: PartyUser) {
+    const party = this.parties.get(partyId)
+    if (!party) {
+      throw new PartyServiceError(ErrorEnum.NotFound, 'Party not found')
+    }
+
+    if (leader && leader.id !== party.leader) {
+      throw new PartyServiceError(ErrorEnum.Forbidden, 'Must be party leader')
+    }
+
+    const updatedParty = party.deleteIn(['invites', target.id])
+    this.parties = this.parties.set(party.id as string, updatedParty)
+
+    const targetUser = this.getUser(target.name as string)
+    targetUser.unsubscribe(getInvitesPath(partyId))
+
+    this.publishToParty(partyId, {
+      type: 'decline',
+      target,
+    })
+
+    // TODO(2Pac): Do we want to remove the party record if there's only one member left in a party
+    // and no outstanding invites?
+  }
+
+  acceptInvite(partyId: string, user: PartyUser, clientId: string) {
+    const client = this.getClient(user.id as number, clientId)
+
+    const party = this.parties.get(partyId)
+    if (!party) {
+      throw new PartyServiceError(ErrorEnum.NotFound, 'Party not found')
+    }
+
+    if (party.members.size > MAX_PARTY_SIZE) {
+      throw new PartyServiceError(ErrorEnum.Conflict, 'Party is full')
+    }
+
+    const oldParty = this.getClientParty(client)
+    if (oldParty) {
+      // TODO(2Pac): Handle switching parties
+    }
+
+    const updatedParty = party.deleteIn(['invites', user.id]).setIn(['members', user.id], user)
+    this.parties = this.parties.set(party.id as string, updatedParty)
+    this.clientToPartyId = this.clientToPartyId.set(client, partyId)
+
+    this.publishToParty(partyId, {
+      type: 'join',
+      user,
+    })
+
+    const userSockets = this.getUser(user.name as string)
+    userSockets.unsubscribe(getInvitesPath(partyId))
+    this.subscribeToParty(client, updatedParty)
+  }
+
+  private subscribeToParty(client: ClientSocketsGroup, party: PartyRecord) {
+    client.subscribe(
+      getPartyPath(party.id as string),
+      () => ({
+        action: 'init',
+        party,
+      }),
+      () => this.handleClientQuit(client),
+    )
+  }
+
+  private handleClientQuit(client: ClientSocketsGroup) {
+    const party = this.getClientParty(client)
+    if (!party) {
+      throw new PartyServiceError(ErrorEnum.NotFound, 'Party not found')
+    }
+
+    this.clientToPartyId.delete(client)
+    this.parties = this.parties.deleteIn([party.id, 'members', client.userId])
+    this.publishToParty(party.id as string, {
+      type: 'leave',
+      user: client.userId,
+    })
+
+    // TODO(2Pac): Handle party leader leaving
+
+    // TODO(2Pac): Handle last person in a party leaving
+  }
+
+  private publishToParty(partyId: string, data: any) {
+    this.nydus.publish(getPartyPath(partyId as string), data)
+  }
+
+  private getUser(username: string): UserSocketsGroup {
+    const user = this.userSockets.getByName(username)
+    if (!user) {
+      throw new PartyServiceError(ErrorEnum.Unauthorized, 'Authorization required')
+    }
+
+    return user
+  }
+
+  private getClient(userId: number, clientId: string): ClientSocketsGroup {
+    const client = this.clientSockets.getById(userId, clientId)
+    if (!client) {
+      throw new PartyServiceError(ErrorEnum.Unauthorized, 'Authorization required')
+    }
+
+    return client
+  }
+
+  private getClientParty(client: ClientSocketsGroup): PartyRecord | undefined {
+    const partyId = this.clientToPartyId.get(client)
+    return this.parties.get(partyId as string)
+  }
+}
