2ad93555250a4d2f2ea74ef589b6584a255e2cd4
Travis Collins <travis@tec27.com>
2021-02-13 18:35:36 -0800

    Use real map images for the dev matchmaking test (from the staging server's cloud storage).

--- client/matchmaking/devonly/match-test.jsx
+++ client/matchmaking/devonly/match-test.jsx
@@ -92,6 +92,19 @@ export default class MapSelectionTest extends React.Component {
     const map = new MapRecord({
       id: 1,
       name: 'Fighting Spirit',
+      hash: '0924d3cbab0061cdbcc1dc2e20586cf514df8c5391126dae71a280616afdc03c',
+      image256Url:
+        'https://staging.shieldbattery.net/files/map_images/' +
+        '09/24/0924d3cbab0061cdbcc1dc2e20586cf514df8c5391126dae71a280616afdc03c-256.jpg',
+      image512Url:
+        'https://staging.shieldbattery.net/files/map_images/' +
+        '09/24/0924d3cbab0061cdbcc1dc2e20586cf514df8c5391126dae71a280616afdc03c-512.jpg',
+      image1024Url:
+        'https://staging.shieldbattery.net/files/map_images/' +
+        '09/24/0924d3cbab0061cdbcc1dc2e20586cf514df8c5391126dae71a280616afdc03c-1024.jpg',
+      image2048Url:
+        'https://staging.shieldbattery.net/files/map_images/' +
+        '09/24/0924d3cbab0061cdbcc1dc2e20586cf514df8c5391126dae71a280616afdc03c-2048.jpg',
     })
     const players = [
       new Player({ id: 1, name: 'tec27', race: 'p' }),
