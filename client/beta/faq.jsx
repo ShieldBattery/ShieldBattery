2d56d6f07e1f3ad44e11b85bf6bd1e185b736b02
Travis Collins <travis@tec27.com>
2021-03-03 03:00:54 -0800

    Do a quick pass on FAQ content.

--- client/beta/faq.jsx
+++ client/beta/faq.jsx
@@ -11,58 +11,53 @@ import { STARCRAFT_DOWNLOAD_URL } from '../../common/constants'
 import LogoText from '../logos/logotext-640x100.svg'
 import QuestionIcon from '../icons/material/ic_help_outline_black_48px.svg'
 import { colorDividers, colorTextSecondary, blue400, grey850, grey900 } from '../styles/colors'
-import { HeadlineOld, SubheadingOld, Display4Old } from '../styles/typography'
+import { headline1, headline5 } from '../styles/typography'
 import { shadowDef4dp } from '../material/shadow-constants'
 
 const questions = [
   {
-    question: 'What version of BW does ShieldBattery support?',
+    question: 'What version of StarCraft does ShieldBattery support?',
     answer: [
       <p key='p1'>
-        ShieldBattery supports all versions of BW from 1.16.1 and up. The version that is actually
-        run when playing on ShieldBattery is 1.16.1. This means that you can give our application an
-        install path to any version of BW 1.16.1 and above, and things will just work.
-      </p>,
-      <p key='p2'>
-        You can download the free version of StarCraft: Brood War from the offical{' '}
-        <a href={STARCRAFT_DOWNLOAD_URL} target='_blank' rel='nofollow noreferrer'>
+        ShieldBattery supports the latest version of StarCraft: Remastered. You can download the
+        free version of StarCraft: Remastered from the offical{' '}
+        <a href={STARCRAFT_DOWNLOAD_URL} target='_blank' rel='nofollow noreferrer noopener'>
           Blizzard site
         </a>
-        .
+        , or install it through the Blizzard launcher. Any purchased addons (such as HD graphics)
+        will be usable on ShieldBattery.
       </p>,
     ],
   },
   {
-    question: 'What is the status of the open beta?',
+    question: 'Is ShieldBattery developed with the support of Blizzard?',
     answer: [
       <p key='p1'>
-        The open beta is currently running, and will last until we feel that the feature set is
-        relatively complete, polished, and bug-free. We've ironed out most of the significant bugs
-        in our base feature set during our closed beta process, and are now ready to test things
-        with a more significant playerbase, as well as build new features that require a larger
-        group.
-      </p>,
-
-      <p key='p2'>
-        Please keep in mind that this is still a <i>beta</i>, so there will be features missing that
-        you want to see, and you might encounter issues that need fixing. You shouldn't expect a
-        perfect, completely polished experience, but we'll do our best to keep the quality bar high.
-        The ShieldBattery developers will continue working relentlessly on features that are
-        missing, with priorities based on community feedback.
+        No, ShieldBattery is a project developed by passionate community members, and has no
+        official support or acknowledgement from Blizzard.
       </p>,
     ],
   },
   {
     question: 'Does ShieldBattery cost anything? Is there a way to donate?',
     answer: [
-      <p key='p1'>
-        Of course not. This is a project made purely out of our love for BW; by community members,
-        for the community.
-      </p>,
+      <p key='p1'>ShieldBattery is totally free to use!</p>,
       <p key='p2'>
-        As for the donation link, there isn't one at this time. The server costs are currently very
-        manageable, and we'd prefer the community directed their funds to more needy causes.
+        It does, however, cost us time and money to host and develop ShieldBattery. If you would
+        like to help us cover those costs, we'd greatly appreciate it. We have set up a number of
+        ways to contribute:
       </p>,
+      <ul>
+        <li>
+          <a href='https://github.com/sponsors/ShieldBattery'>Github Sponsors</a>
+        </li>
+        <li>
+          <a href='https://ko-fi.com/tec27'>Ko-fi</a>
+        </li>
+        <li>
+          <a href='https://patreon.com/tec27'>Patreon</a>
+        </li>
+      </ul>,
     ],
   },
   {
@@ -71,26 +66,49 @@ const questions = [
       <p key='p1'>
         Yes. You can access our main repository as well as the various additional projects we've
         written and separated into their own repositories at our{' '}
-        <a href='https://github.com/ShieldBattery' target='_blank'>
+        <a href='https://github.com/ShieldBattery' target='_blank' rel='noopener'>
           GitHub page
         </a>
         .
       </p>,
       <p key='p2'>
-        We could always use more contributors, so if you find technically challenging projects to
-        your liking and also enjoy the awesome game that is BW, we then look forward to seeing you
-        on our GitHub repos.
+        We could always use more contributors, so if you think you can help, check it out!
       </p>,
     ],
   },
   {
     question: 'What are the system requirements to play on ShieldBattery?',
+    answer:
+      ((
+        <p key='p1'>
+          Our system requirements are mainly driven by those of
+          <a
+            href='https://us.battle.net/support/en/article/28438'
+            target='_blank'
+            rel='nofollow noopener'>
+            StarCraft: Remastered
+          </a>
+          , but in brief:
+        </p>
+      ),
+      (
+        <ul key='u1'>
+          <li>A computer running Windows 7 or later</li>
+          <li>2GB RAM</li>
+          <li>NVIDIA Gefore 6800 (256MB) or ATI Radeon X1600 Pro (256MB) or better</li>
+          <li>A StarCraft: Remastered installation, patched to the latest version</li>
+        </ul>
+      )),
+  },
+  {
+    question: 'Is hotkey customization allowed or provided?',
     answer: (
-      <ul>
-        <li>A computer running Windows 7 or later</li>
-        <li>A graphics card that supports either DirectX 10+ or OpenGL 3.1+</li>
-        <li>A Brood War installation, patched to version 1.16.1 or greater</li>
-      </ul>
+      <span>
+        Yes, we support customized hotkeys, but we do not currently have a hotkey editor. If you
+        want to use customized hotkeys, launch StarCraft: Remastered through the Blizzard launcher
+        and customize them there first. After doing so, future launches through ShieldBattery will
+        use those hotkeys.
+      </span>
     ),
   },
   {
@@ -99,36 +117,6 @@ const questions = [
       <span>
         This is not currently something we're focusing on, but we do have plans to ensure that
         ShieldBattery is usable via Wine. For now, however, it is Windows-only.
-      </span>
-    ),
-  },
-  {
-    question: 'Will there be matchmaking for non-1v1 match types, such as 2v2 or UMS?',
-    answer: (
-      <span>
-        Our initial focus for matchmaking is on 1v1 matches, but we are open to adding other sorts
-        of matchmaking in the future, if player interest is sufficient to sustain it. Our platform
-        is definitely flexible enough to support all of these things.
-      </span>
-    ),
-  },
-  {
-    question: 'Does the windowed mode allow widescreen resolutions or larger viewports?',
-    answer: (
-      <span>
-        No, our windowed mode only allows 4:3 aspect ratios, and always scales the sprites to the
-        new resolution (meaning that even at larger resolutions, the same portion of the map is
-        visible as at the original, 640x480 resolution). We're not opposed to such things for
-        observers, but our focus right now is largely on the playing experience.
-      </span>
-    ),
-  },
-  {
-    question: 'Is hotkey customization allowed or provided?',
-    answer: (
-      <span>
-        Currently no, but we plan to implement built-in hotkey customization and presets at some
-        point in the future.
       </span>
     ),
   },
@@ -172,7 +160,8 @@ const StyledQuestionIcon = styled(QuestionIcon)`
   }
 `
 
-const QuestionText = styled(HeadlineOld)`
+const QuestionText = styled.div`
+  ${headline5};
   margin: 0;
   display: inline-block;
 
@@ -185,11 +174,12 @@ const QuestionText = styled(HeadlineOld)`
   }
 `
 
-const AnswerText = styled(SubheadingOld)`
+const AnswerText = styled.div`
+  ${headline5};
   margin: 8px 0 0 80px;
+
   color: ${colorTextSecondary};
-  font-size: 20px;
-  line-height: 1.5;
+  font-weight: 300;
 
   & > p {
     line-height: inherit;
@@ -310,7 +300,8 @@ const FaqHeaderContainer = styled.div`
   }
 `
 
-const FaqHeader = styled(Display4Old)`
+const FaqHeader = styled.div`
+  ${headline1};
   width: 100%;
   max-width: 464px;
   margin: 0;
