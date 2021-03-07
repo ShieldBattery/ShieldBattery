f4834db528c86f1062d7b4b6289b1a914c096266
Travis Collins <travis@tec27.com>
2021-03-06 18:14:12 -0800

    Fix uploading maps larger than 1MB.

--- server/lib/file-upload/handle-multipart-files.js
+++ server/lib/file-upload/handle-multipart-files.js
@@ -1,7 +1,16 @@
 import koaBody from 'koa-body'
 import fs from 'fs'
 
-const bodyMiddleware = koaBody({ multipart: true })
+const bodyMiddleware = koaBody({
+  multipart: true,
+  formidable: {
+    /**
+     * NOTE(tec27): if you change this, ensure our nginx config has a larger size for
+     * client_max_body_size
+     */
+    maxFileSize: 50 * 1024 * 1024,
+  },
+})
 
 // A Koa middleware function that sets up multipart file handling and will clean up the files
 // once the request is completed
