30a0bcaa327844d0cb82f6e619fa710e5654d36d
hubert <gsj285@umsystem.edu>
2021-03-19 00:08:09 -0500

    Move HiddenRaceIcon to race-picker

--- client/lobbies/selected-race.jsx
+++ client/lobbies/selected-race.jsx
@@ -1,35 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
 
 import RacePicker from './race-picker'
-
-import { colorDividers } from '../styles/colors'
-
-export const HiddenRaceIcon = styled.span`
-  position: relative;
-  display: inline-block;
-  vertical-align: middle;
-  width: 36px;
-  height: 36px;
-  min-height: 32px;
-  padding: 2px;
-
-  &:not(:first-child) {
-    margin-left: 4px;
-  }
-
-  &::after {
-    content: '';
-    position: absolute;
-    left: calc(50% - 6px);
-    top: calc(50% - 6px);
-    width: 12px;
-    height: 12px;
-    border-radius: 50%;
-    background-color: ${colorDividers};
-  }
-`
 
 // Like a RacePicker, but for uncontrollable slots
 export default class SelectedRace extends React.Component {
