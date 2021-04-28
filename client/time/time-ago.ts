8a52fe8d1de68904043af37394b2614fc5af0942
2Pacalypse- <marko@kastela.com>
2021-04-28 02:00:04 +0200

    Rework the API a bit.

--- client/time/time-ago.ts
+++ client/time/time-ago.ts
@@ -1,4 +1,3 @@
-const NOW = Date.now()
 const MINUTE = 60 // in seconds
 const HOUR = MINUTE * 60
 const DAY = HOUR * 24
@@ -7,45 +6,43 @@ const MONTH = DAY * 30.42
 const YEAR = MONTH * 12
 
 /**
- * A function which takes the amount of time, in miliseconds, and returns a string representing how
- * much time has passsed since that time, in "Xy ago" syntax. Where X equals amount of time, and y
- * specifies the time unit.
+ * A function which takes the number representing a difference between two dates, and returns a
+ * string representation of that difference, in "Xy ago" syntax. Where X equals amount of time, and
+ * y specifies the time unit.
  *
  * Note that this is not a *perfect* measurement of time. Some values are approximated, e.g. number
  * of days in a month, and leap days/seconds/whatever are not taken into account.
  */
-export function timeAgo(date: number): string {
-  const diffInSeconds = Math.floor((NOW - date) / 1000)
-
-  let interval = diffInSeconds / YEAR
+export function timeAgo(diff: number): string {
+  let interval = diff / YEAR
   if (interval >= 1) {
     return `${Math.floor(interval)}y ago`
   }
 
-  interval = diffInSeconds / MONTH
+  interval = diff / MONTH
   if (interval >= 1) {
     return `${Math.floor(interval)}mo ago`
   }
 
-  interval = diffInSeconds / WEEK
+  interval = diff / WEEK
   if (interval >= 1) {
     return `${Math.floor(interval)}w ago`
   }
 
-  interval = diffInSeconds / DAY
+  interval = diff / DAY
   if (interval >= 1) {
     return `${Math.floor(interval)}d ago`
   }
 
-  interval = diffInSeconds / HOUR
+  interval = diff / HOUR
   if (interval >= 1) {
     return `${Math.floor(interval)}h ago`
   }
 
-  interval = diffInSeconds / MINUTE
+  interval = diff / MINUTE
   if (interval >= 1) {
     return `${Math.floor(interval)}m ago`
   }
 
-  return `${Math.floor(diffInSeconds)}s ago`
+  return `${Math.floor(diff)}s ago`
 }
