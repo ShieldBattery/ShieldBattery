88182e03719c669a42490a46a44acc0ef5a2f94a
Marko Žarković <marko@kastela.com>
2021-05-20 05:14:21 +0200

    Refactor the profile components so they're easier to extend. (#674)
    This PR creates a ConnectedUserProfileOverlay component which allows the
    usage of profile overlay without needing to send pre-made actions to it.
    Threading required state/callbacks through the chat-channel components
    just to create the user's profile overlay actions was a bit of a pain,
    and was especially annoying once we started adding more actions to that
    menu that depend on other application state, e.g. (the party invite actions
    change depending on whether the user is in your party).
    
    This makes it possible to use the user profile overlay at many different
    places (something that we might start doing soon, e.g. in chat
    messages), without a lot of boiler plate. However, this is done at the
    expense of the flexibility in terms of which actions are displayed in
    the overlay. If more flexibility is needed in certain scenarios, it is
    still possible to use the un-connected user profile overlay component
    and provide the actions manually to it.
    
    Another thing that is left as a TODO in this PR is to be able to
    configure the position of the user profile overlay through props,
    something that will probably be required once we start showing this
    overlay at other places.
    
    Finally, this PR moves all the profile components to Typescript, so
    that's nice.

---
+++ client/profile/profile-overlay-content.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+import Avatar from '../avatars/avatar'
+import { headline6, singleLine } from '../styles/typography'
+
+export const PopoverContents = styled.div`
+  min-width: 256px;
+`
+
+export const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 24px;
+`
+
+export const StyledAvatar = styled(Avatar)`
+  width: 64px;
+  height: 64px;
+  margin-bottom: 16px;
+`
+
+export const Username = styled.div`
+  ${headline6};
+  ${singleLine};
+`
+
+export const Actions = styled.div`
+  padding-top: 8px;
+  padding-bottom: 8px;
+`
---
+++ client/profile/self-profile-overlay.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { UseTransitionProps } from 'react-spring'
+import { Popover, useAnchorPosition } from '../material/popover'
+import { defaultSpring } from '../material/springs'
+import { Actions, Header, PopoverContents, StyledAvatar, Username } from './profile-overlay-content'
+
+interface SelfProfileOverlayProps {
+  open: boolean
+  username: string
+  anchor: HTMLElement | null
+  children: React.ReactNode
+  onDismiss: () => void
+}
+
+const VERTICAL_TRANSITION: UseTransitionProps<boolean> = {
+  from: { opacity: 0, scaleY: 0.5 },
+  enter: { opacity: 1, scaleY: 1 },
+  leave: { opacity: 0, scaleY: 0 },
+  config: (item, index, phase) => key =>
+    phase === 'leave' || key === 'opacity' ? { ...defaultSpring, clamp: true } : defaultSpring,
+}
+
+export function SelfProfileOverlay(props: SelfProfileOverlayProps) {
+  const { username, children, open, onDismiss, anchor } = props
+  const [, anchorX, anchorY] = useAnchorPosition('left', 'top', anchor ?? null)
+
+  return (
+    <Popover
+      open={open}
+      onDismiss={onDismiss}
+      anchorX={anchorX ?? 0}
+      anchorY={anchorY ?? 0}
+      originX='left'
+      originY='bottom'
+      transitionProps={VERTICAL_TRANSITION}>
+      <PopoverContents>
+        <Header>
+          <StyledAvatar user={username} />
+          <Username>{username}</Username>
+        </Header>
+        <Actions>{children}</Actions>
+      </PopoverContents>
+    </Popover>
+  )
+}
