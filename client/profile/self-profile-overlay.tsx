1437bc079ae8906583a29893923170cc307b6bc9
2Pacalypse- <marko@kastela.com>
2021-05-20 03:00:53 +0200

    Use a vertical transition for self-profile overlay.

--- client/profile/self-profile-overlay.tsx
+++ client/profile/self-profile-overlay.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { UseTransitionProps } from 'react-spring'
 import { Popover, useAnchorPosition } from '../material/popover'
+import { defaultSpring } from '../material/springs'
 import { Actions, Header, PopoverContents, StyledAvatar, Username } from './profile-overlay-content'
 
 interface SelfProfileOverlayProps {
@@ -8,6 +10,14 @@ interface SelfProfileOverlayProps {
   anchor: HTMLElement | null
   children: React.ReactNode
   onDismiss: () => void
+}
+
+const VERTICAL_TRANSITION: UseTransitionProps<boolean> = {
+  from: { opacity: 0, scaleY: 0.5 },
+  enter: { opacity: 1, scaleY: 1 },
+  leave: { opacity: 0, scaleY: 0 },
+  config: (item, index, phase) => key =>
+    phase === 'leave' || key === 'opacity' ? { ...defaultSpring, clamp: true } : defaultSpring,
 }
 
 export function SelfProfileOverlay(props: SelfProfileOverlayProps) {
@@ -21,7 +31,8 @@ export function SelfProfileOverlay(props: SelfProfileOverlayProps) {
       anchorX={anchorX ?? 0}
       anchorY={anchorY ?? 0}
       originX='left'
-      originY='bottom'>
+      originY='bottom'
+      transitionProps={VERTICAL_TRANSITION}>
       <PopoverContents>
         <Header>
           <StyledAvatar user={username} />
