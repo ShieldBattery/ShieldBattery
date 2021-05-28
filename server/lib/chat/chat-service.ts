7dc298cb71bcc6688510ff1bfe90e2aa139c8cec
Marko Žarković <marko@kastela.com>
2021-05-29 01:45:08 +0200

    Move the chat logic from WS API to a dedicated chat service. (#678)

---
+++ server/lib/chat/chat-service.ts
@@ -0,0 +1,260 @@
+import { Map, Record, Set } from 'immutable'
+import { NydusServer } from 'nydus'
+import { singleton } from 'tsyringe'
+import filterChatMessage from '../messaging/filter-chat-message'
+import {
+  addMessageToChannel,
+  addUserToChannel,
+  findChannel,
+  getChannelsForUser,
+  getMessagesForChannel,
+  getUsersForChannel,
+  leaveChannel,
+} from '../models/chat-channels'
+import { UserSocketsGroup, UserSocketsManager } from '../websockets/socket-groups'
+
+class ChatState extends Record({
+  /** Maps channel name -> Set of users in that channel (as names). */
+  channels: Map<string, Set<string>>(),
+  /** Maps username -> Set of channels they're in (as names). */
+  users: Map<string, Set<string>>(),
+}) {}
+
+export enum ChatServiceErrorCode {
+  UserOffline,
+  InvalidJoinAction,
+  LeaveShieldBattery,
+  InvalidLeaveAction,
+  InvalidSendAction,
+  InvalidGetHistoryAction,
+  InvalidGetUsersAction,
+}
+
+export class ChatServiceError extends Error {
+  constructor(readonly code: ChatServiceErrorCode, message: string) {
+    super(message)
+  }
+}
+
+export function getChannelPath(channelName: string): string {
+  return `/chat/${encodeURIComponent(channelName)}`
+}
+
+@singleton()
+export default class ChatService {
+  private state = new ChatState()
+
+  constructor(private nydus: NydusServer, private userSocketsManager: UserSocketsManager) {
+    userSocketsManager
+      .on('newUser', (userSockets: UserSocketsGroup) => this.handleNewUser(userSockets))
+      .on('userQuit', (userName: string) => this.handleUserQuit(userName))
+  }
+
+  async joinChannel(channelName: string, userName: string) {
+    const userSockets = this.getUserSockets(userName)
+    const originalChannelName = await this.getOriginalChannelName(channelName)
+    if (
+      this.state.users.has(userSockets.name) &&
+      this.state.users.get(userSockets.name)!.has(originalChannelName)
+    ) {
+      throw new ChatServiceError(ChatServiceErrorCode.InvalidJoinAction, 'Already in this channel')
+    }
+
+    await addUserToChannel(userSockets.session.userId, originalChannelName)
+
+    this.state = this.state
+      .updateIn(['channels', originalChannelName], (s = Set()) => s.add(userName))
+      .updateIn(['users', userName], (s = Set()) => s.add(originalChannelName))
+    this.publishToChannel(originalChannelName, { action: 'join', user: userName })
+    this.subscribeUserToChannel(userSockets, originalChannelName)
+  }
+
+  async leaveChannel(channelName: string, userName: string) {
+    const userSockets = this.getUserSockets(userName)
+    const originalChannelName = await this.getOriginalChannelName(channelName)
+    if (originalChannelName === 'ShieldBattery') {
+      throw new ChatServiceError(
+        ChatServiceErrorCode.LeaveShieldBattery,
+        "Can't leave ShieldBattery channel",
+      )
+    }
+    if (
+      !this.state.users.has(userSockets.name) ||
+      !this.state.users.get(userSockets.name)!.has(originalChannelName)
+    ) {
+      throw new ChatServiceError(
+        ChatServiceErrorCode.InvalidLeaveAction,
+        'Must be in channel to leave it',
+      )
+    }
+
+    const result = await leaveChannel(userSockets.session.userId, originalChannelName)
+    const updated = this.state.channels.get(originalChannelName)!.delete(userSockets.name)
+    this.state = updated.size
+      ? this.state.setIn(['channels', originalChannelName], updated)
+      : this.state.deleteIn(['channels', originalChannelName])
+    this.state = this.state.updateIn(['users', userSockets.name], u =>
+      u.delete(originalChannelName),
+    )
+    this.publishToChannel(originalChannelName, {
+      action: 'leave',
+      user: userSockets.name,
+      newOwner: result.newOwner,
+    })
+    this.unsubscribeUserFromChannel(userSockets, originalChannelName)
+  }
+
+  async sendChatMessage(channelName: string, userName: string, message: string) {
+    const userSockets = this.getUserSockets(userName)
+    const originalChannelName = await this.getOriginalChannelName(channelName)
+    // TODO(tec27): lookup channel keys case insensitively?
+    if (
+      !this.state.users.has(userSockets.name) ||
+      !this.state.users.get(userSockets.name)!.has(originalChannelName)
+    ) {
+      throw new ChatServiceError(
+        ChatServiceErrorCode.InvalidSendAction,
+        'Must be in a channel to send a message to it',
+      )
+    }
+
+    const text = filterChatMessage(message)
+    const result = await addMessageToChannel(userSockets.session.userId, originalChannelName, {
+      type: 'message',
+      text,
+    })
+
+    this.publishToChannel(originalChannelName, {
+      id: result.msgId,
+      action: 'message',
+      user: result.userName,
+      sent: +result.sent,
+      data: result.data,
+    })
+  }
+
+  async getChannelHistory(
+    channelName: string,
+    userName: string,
+    limit?: number,
+    beforeTime?: number,
+  ) {
+    const userSockets = this.getUserSockets(userName)
+    const originalChannelName = await this.getOriginalChannelName(channelName)
+    // TODO(tec27): lookup channel keys case insensitively?
+    if (
+      !this.state.users.has(userSockets.name) ||
+      !this.state.users.get(userSockets.name)!.has(originalChannelName)
+    ) {
+      throw new ChatServiceError(
+        ChatServiceErrorCode.InvalidGetHistoryAction,
+        'Must be in a channel to retrieve message history',
+      )
+    }
+
+    const messages = await getMessagesForChannel(
+      originalChannelName,
+      userSockets.session.userId,
+      limit,
+      beforeTime && beforeTime > -1 ? new Date(beforeTime) : undefined,
+    )
+    return messages.map(m => ({
+      id: m.msgId,
+      user: m.userName,
+      sent: +m.sent,
+      data: m.data,
+    }))
+  }
+
+  async getChannelUsers(channelName: string, userName: string) {
+    const userSockets = this.getUserSockets(userName)
+    const originalChannelName = await this.getOriginalChannelName(channelName)
+    if (
+      !this.state.users.has(userSockets.name) ||
+      !this.state.users.get(userSockets.name)!.has(originalChannelName)
+    ) {
+      throw new ChatServiceError(
+        ChatServiceErrorCode.InvalidGetUsersAction,
+        'Must be in a channel to retrieve user list',
+      )
+    }
+
+    const users = await getUsersForChannel(originalChannelName)
+    return users.map(u => u.userName)
+  }
+
+  async getOriginalChannelName(channelName: string) {
+    const foundChannel = await findChannel(channelName)
+
+    // If the channel already exists in database, return its name with original casing; otherwise
+    // return it as is
+    return foundChannel ? foundChannel.name : channelName
+  }
+
+  private getUserSockets(userName: string): UserSocketsGroup {
+    const userSockets = this.userSocketsManager.getByName(userName)
+    if (!userSockets) {
+      throw new ChatServiceError(ChatServiceErrorCode.UserOffline, 'User is offline')
+    }
+
+    return userSockets
+  }
+
+  // TODO(tec27): type event properly
+  private publishToChannel(channelName: string, data: any) {
+    this.nydus.publish(getChannelPath(channelName), data)
+  }
+
+  private subscribeUserToChannel(userSockets: UserSocketsGroup, channelName: string) {
+    userSockets.subscribe(getChannelPath(channelName), () => {
+      return {
+        action: 'init',
+        activeUsers: this.state.channels.get(channelName),
+      }
+    })
+  }
+
+  unsubscribeUserFromChannel(user: UserSocketsGroup, channelName: string) {
+    user.unsubscribe(getChannelPath(channelName))
+  }
+
+  private async handleNewUser(userSockets: UserSocketsGroup) {
+    const channelsForUser = await getChannelsForUser(userSockets.session.userId)
+    if (!userSockets.sockets.size) {
+      // The user disconnected while we were waiting for their channel list
+      return
+    }
+
+    const channelSet = Set(channelsForUser.map(c => c.channelName))
+    const userSet = Set([userSockets.name])
+    const inChannels = Map(channelsForUser.map(c => [c.channelName, userSet]))
+
+    this.state = this.state
+      .mergeDeepIn(['channels'], inChannels)
+      .setIn(['users', userSockets.name], channelSet)
+    for (const { channelName: chan } of channelsForUser) {
+      this.publishToChannel(chan, { action: 'userActive', user: userSockets.name })
+      this.subscribeUserToChannel(userSockets, chan)
+    }
+    userSockets.subscribe(`${userSockets.getPath()}/chat`, () => ({ type: 'chatReady' }))
+  }
+
+  private async handleUserQuit(userName: string) {
+    if (!this.state.users.has(userName)) {
+      // This can happen if a user disconnects before we get their channel list back from the DB
+      return
+    }
+    const channels = this.state.users.get(userName)!
+    for (const channel of channels.values()) {
+      const updated = this.state.channels.get(channel)?.delete(userName)
+      this.state = updated?.size
+        ? this.state.setIn(['channels', channel], updated)
+        : this.state.deleteIn(['channels', channel])
+    }
+    this.state = this.state.deleteIn(['users', userName])
+
+    for (const c of channels.values()) {
+      this.publishToChannel(c, { action: 'userOffline', user: userName })
+    }
+  }
+}
--- server/lib/wsapi/chat.ts
+++ server/lib/wsapi/chat.ts
@@ -1,31 +1,16 @@
 import errors from 'http-errors'
-import { Map, Record, Set } from 'immutable'
+import { Map } from 'immutable'
 import { NextFunc, NydusServer, RouteHandler } from 'nydus'
 import { container, singleton } from 'tsyringe'
+import { assertUnreachable } from '../../../common/assert-unreachable'
 import { isValidChannelName } from '../../../common/constants'
 import { MULTI_CHANNEL } from '../../../common/flags'
-import filterChatMessage from '../messaging/filter-chat-message'
-import {
-  addMessageToChannel,
-  addUserToChannel,
-  findChannel,
-  getChannelsForUser,
-  getMessagesForChannel,
-  getUsersForChannel,
-  leaveChannel,
-} from '../models/chat-channels'
+import ChatService, { ChatServiceError, ChatServiceErrorCode } from '../chat/chat-service'
 import createThrottle from '../throttle/create-throttle'
 import throttleMiddleware from '../throttle/websocket-middleware'
 import { Api, Mount, registerApiRoutes } from '../websockets/api-decorators'
-import { UserSocketsGroup, UserSocketsManager } from '../websockets/socket-groups'
+import { UserSocketsManager } from '../websockets/socket-groups'
 import validateBody from '../websockets/validate-body'
-
-class ChatState extends Record({
-  /** Maps channel name -> Set of users in that channel (as names). */
-  channels: Map<string, Set<string>>(),
-  /** Maps username -> Set of channels they're in (as names). */
-  users: Map<string, Set<string>>(),
-}) {}
 
 const joinThrottle = createThrottle('chatjoin', {
   rate: 3,
@@ -43,6 +28,31 @@ const sendThrottle = createThrottle('chatsend', {
   window: 60000,
 })
 
+function isChatServiceError(error: Error): error is ChatServiceError {
+  return error.hasOwnProperty('code')
+}
+
+function convertChatServiceError(err: Error) {
+  if (!isChatServiceError(err)) {
+    throw err
+  }
+
+  switch (err.code) {
+    case ChatServiceErrorCode.UserOffline:
+      throw new errors.NotFound(err.message)
+    case ChatServiceErrorCode.InvalidJoinAction:
+    case ChatServiceErrorCode.InvalidLeaveAction:
+    case ChatServiceErrorCode.InvalidSendAction:
+    case ChatServiceErrorCode.InvalidGetHistoryAction:
+    case ChatServiceErrorCode.InvalidGetUsersAction:
+      throw new errors.BadRequest(err.message)
+    case ChatServiceErrorCode.LeaveShieldBattery:
+      throw new errors.Forbidden(err.message)
+    default:
+      assertUnreachable(err.code)
+  }
+}
+
 const featureEnabled: RouteHandler = async (data, next) => {
   if (!MULTI_CHANNEL) throw new errors.NotFound()
   return next(data)
@@ -55,19 +65,11 @@ const beforeTime = (val: unknown) =>
 
 const MOUNT_BASE = '/chat'
 
-function getPath(channel: string) {
-  return `${MOUNT_BASE}/${encodeURIComponent(channel)}`
-}
-
 @singleton()
 @Mount(MOUNT_BASE)
 export class ChatApi {
-  private state = new ChatState()
-
-  constructor(private nydus: NydusServer, private userSockets: UserSocketsManager) {
-    this.userSockets
-      .on('newUser', user => this.handleNewUser(user))
-      .on('userQuit', name => this.handleUserQuit(name))
+  constructor(private userSockets: UserSocketsManager) {
+    container.resolve(ChatService)
   }
 
   @Api(
@@ -78,22 +80,17 @@ export class ChatApi {
     }),
     'getUser',
     throttleMiddleware(joinThrottle, (data: Map<string, any>) => data.get('user')),
-    'getChannel',
   )
   async join(data: Map<string, any>) {
     const user = data.get('user')
-    const channel = data.get('channel')
-    if (this.state.users.has(user.name) && this.state.users.get(user.name)!.has(channel)) {
-      throw new errors.BadRequest('already in this channel')
+    const { channel } = data.get('body')
+
+    try {
+      const chatService = container.resolve(ChatService)
+      chatService.joinChannel(channel, user.name)
+    } catch (err) {
+      convertChatServiceError(err)
     }
-
-    await addUserToChannel(user.session.userId, channel)
-
-    this.state = this.state
-      .updateIn(['channels', channel], (s = Set()) => s.add(user.name))
-      .updateIn(['users', user.name], (s = Set()) => s.add(channel))
-    this._publishTo(channel, { action: 'join', user: user.name })
-    this._subscribeUserToChannel(user, channel)
   }
 
   @Api(
@@ -103,26 +100,17 @@ export class ChatApi {
       channel: isValidChannelName,
     }),
     'getUser',
-    'getChannel',
   )
   async leave(data: Map<string, any>) {
     const user = data.get('user')
-    const channel = data.get('channel')
-    if (channel === 'ShieldBattery') {
-      throw new errors.Forbidden("can't leave ShieldBattery channel")
-    }
-    if (!this.state.users.has(user.name) || !this.state.users.get(user.name)!.has(channel)) {
-      throw new errors.NotFound('not in this channel')
-    }
+    const { channel } = data.get('body')
 
-    const result = await leaveChannel(user.session.userId, channel)
-    const updated = this.state.channels.get(channel)!.delete(user.name)
-    this.state = updated.size
-      ? this.state.setIn(['channels', channel], updated)
-      : this.state.deleteIn(['channels', channel])
-    this.state = this.state.updateIn(['users', user.name], u => u.delete(channel))
-    this._publishTo(channel, { action: 'leave', user: user.name, newOwner: result.newOwner })
-    this._unsubscribeUserFromChannel(user, channel)
+    try {
+      const chatService = container.resolve(ChatService)
+      chatService.leaveChannel(channel, user.name)
+    } catch (err) {
+      convertChatServiceError(err)
+    }
   }
 
   @Api(
@@ -133,30 +121,17 @@ export class ChatApi {
     }),
     'getUser',
     throttleMiddleware(sendThrottle, (data: Map<string, any>) => data.get('user')),
-    'getChannel',
   )
   async send(data: Map<string, any>) {
-    const { message } = data.get('body')
     const user = data.get('user')
-    const channel = data.get('channel')
-    // TODO(tec27): lookup channel keys case insensitively?
-    if (!this.state.users.has(user.name) || !this.state.users.get(user.name)!.has(channel)) {
-      throw new errors.Forbidden('must be in a channel to send a message to it')
+    const { channel, message } = data.get('body')
+
+    try {
+      const chatService = container.resolve(ChatService)
+      chatService.sendChatMessage(channel, user.name, message)
+    } catch (err) {
+      convertChatServiceError(err)
     }
-
-    const text = filterChatMessage(message)
-    const result = await addMessageToChannel(user.session.userId, channel, {
-      type: 'message',
-      text,
-    })
-
-    this._publishTo(channel, {
-      id: result.msgId,
-      action: 'message',
-      user: result.userName,
-      sent: +result.sent,
-      data: result.data,
-    })
   }
 
   @Api(
@@ -168,29 +143,17 @@ export class ChatApi {
     }),
     'getUser',
     throttleMiddleware(retrievalThrottle, (data: Map<string, any>) => data.get('user')),
-    'getChannel',
   )
   async getHistory(data: Map<string, any>) {
-    const { limit, beforeTime } = data.get('body')
     const user = data.get('user')
-    const channel = data.get('channel')
-    // TODO(tec27): lookup channel keys case insensitively?
-    if (!this.state.users.has(user.name) || !this.state.users.get(user.name)!.has(channel)) {
-      throw new errors.Forbidden('must be in a channel to retrieve message history')
-    }
+    const { channel, limit, beforeTime } = data.get('body')
 
-    const messages = await getMessagesForChannel(
-      channel,
-      user.session.userId,
-      limit,
-      beforeTime && beforeTime > -1 ? new Date(beforeTime) : undefined,
-    )
-    return messages.map(m => ({
-      id: m.msgId,
-      user: m.userName,
-      sent: +m.sent,
-      data: m.data,
-    }))
+    try {
+      const chatService = container.resolve(ChatService)
+      return chatService.getChannelHistory(channel, user.name, limit, beforeTime)
+    } catch (err) {
+      return convertChatServiceError(err)
+    }
   }
 
   @Api(
@@ -200,17 +163,17 @@ export class ChatApi {
     }),
     'getUser',
     throttleMiddleware(retrievalThrottle, (data: Map<string, any>) => data.get('user')),
-    'getChannel',
   )
   async getUsers(data: Map<string, any>) {
     const user = data.get('user')
-    const channel = data.get('channel')
-    if (!this.state.users.has(user.name) || !this.state.users.get(user.name)!.has(channel)) {
-      throw new errors.Forbidden('must be in a channel to retrieve user list')
-    }
+    const { channel } = data.get('body')
 
-    const users = await getUsersForChannel(channel)
-    return users.map(u => u.userName)
+    try {
+      const chatService = container.resolve(ChatService)
+      return chatService.getChannelUsers(channel, user.name)
+    } catch (err) {
+      return convertChatServiceError(err)
+    }
   }
 
   async getUser(data: Map<string, any>, next: NextFunc) {
@@ -219,74 +182,6 @@ export class ChatApi {
     const newData = data.set('user', user)
 
     return next(newData)
-  }
-
-  async getChannel(data: Map<string, any>, next: NextFunc) {
-    const { channel } = data.get('body')
-    const foundChannel = await findChannel(channel)
-
-    // If the channel already exists in database, return its name with original casing; otherwise
-    // return it as is
-    const newData = data.set('channel', foundChannel ? foundChannel.name : channel)
-    return next(newData)
-  }
-
-  // TODO(tec27): type event properly
-  _publishTo(channel: string, event: any) {
-    this.nydus.publish(getPath(channel), event)
-  }
-
-  _subscribeUserToChannel(user: UserSocketsGroup, channel: string) {
-    user.subscribe(getPath(channel), () => {
-      return {
-        action: 'init',
-        activeUsers: this.state.channels.get(channel),
-      }
-    })
-  }
-
-  _unsubscribeUserFromChannel(user: UserSocketsGroup, channel: string) {
-    user.unsubscribe(getPath(channel))
-  }
-
-  private async handleNewUser(user: UserSocketsGroup) {
-    const channelsForUser = await getChannelsForUser(user.session.userId)
-    if (!user.sockets.size) {
-      // The user disconnected while we were waiting for their channel list
-      return
-    }
-
-    const channelSet = Set(channelsForUser.map(c => c.channelName))
-    const userSet = Set([user.name])
-    const inChannels = Map(channelsForUser.map(c => [c.channelName, userSet]))
-
-    this.state = this.state
-      .mergeDeepIn(['channels'], inChannels)
-      .setIn(['users', user.name], channelSet)
-    for (const { channelName: chan } of channelsForUser) {
-      this._publishTo(chan, { action: 'userActive', user: user.name })
-      this._subscribeUserToChannel(user, chan)
-    }
-    user.subscribe(`${user.getPath()}/chat`, () => ({ type: 'chatReady' }))
-  }
-
-  private async handleUserQuit(userName: string) {
-    if (!this.state.users.has(userName)) {
-      // This can happen if a user disconnects before we get their channel list back from the DB
-      return
-    }
-    const channels = this.state.users.get(userName)!
-    for (const channel of channels.values()) {
-      const updated = this.state.channels.get(channel)?.delete(userName)
-      this.state = updated?.size
-        ? this.state.setIn(['channels', channel], updated)
-        : this.state.deleteIn(['channels', channel])
-    }
-    this.state = this.state.deleteIn(['users', userName])
-
-    for (const c of channels.values()) {
-      this._publishTo(c, { action: 'userOffline', user: userName })
-    }
   }
 }
 
