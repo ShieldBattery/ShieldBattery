70af0bd2502169229bb00ce574c59e5e04b8dda1
2Pacalypse- <marko@kastela.com>
2021-05-28 18:37:07 +0200

    Add a HTTP-based chat API (client is still using the WS API; will be moved to HTTP in the next PR).

--- server/http-apis.ts
+++ server/http-apis.ts
@@ -1,5 +1,6 @@
 // This file should import every `HttpApi` that needs to be included in the app. It doesn't have to
 // do anything with them, they just need to be imported so they will be registered with tsyringe
+import './lib/chat/chat-api'
 import './lib/logging/logs-api'
 import './lib/matchmaking/matchmaking-debug-api'
 import './lib/notifications/notification-api'
