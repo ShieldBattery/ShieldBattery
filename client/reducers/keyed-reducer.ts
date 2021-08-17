4b0632fb63fa2b644e317a41e6277a378945a3ce
Travis Collins <travis@tec27.com>
2021-08-17 12:57:00 -0700

    Allow for easy state resets in immer reducers.

--- client/reducers/keyed-reducer.ts
+++ client/reducers/keyed-reducer.ts
@@ -1,4 +1,4 @@
-import { Draft, Immutable, produce } from 'immer'
+import { castDraft, Draft, Immutable, produce } from 'immer'
 import { ReduxAction } from '../action-types'
 
 type ReducerFunc<ActionType extends ReduxAction, S> = (state: S, action: ActionType) => S
@@ -38,11 +38,24 @@ export function keyedReducer<S>(defaultState: S, reducerObject: ReducerMap<S>) {
 
 // TODO(tec27): If immer ends up being our choice, delete the non-immer stuff and rename these to
 // remove the prefix
+/**
+ * A reducer function that uses Immer to ensure immutability.
+ *
+ * @param state A draft state that can be mutated into the new, resulting state
+ * @param action The actin that triggered this reducer run
+ * @param originalState A copy of the state as it existed before this reducer was run. This is
+ *   immutable and is mainly useful as a reference.
+ *
+ * @returns In most cases, no value will be returned, and Immer will use the mutations to the draft
+ *   state to update things. If you need to reset to the default state, returning the default state
+ *   will work. You may also return the draft state, or a completely new object to replace the
+ *   state.
+ */
 type ImmerReducerFunc<ActionType extends ReduxAction, S> = (
   state: Draft<S>,
   action: ActionType,
   originalState: Immutable<S>,
-) => Draft<S> | void | undefined
+) => S | Immutable<S> | Draft<S> | void
 
 type ImmerReducerMap<S> = {
   [A in AllActionTypes]?: ImmerReducerFunc<Extract<ReduxAction, { type: A }>, S>
@@ -67,8 +80,10 @@ export function immerKeyedReducer<S>(defaultState: S, reducerObject: ImmerReduce
   return (state = defaultState, action: { type: string }) => {
     if (reducerObject.hasOwnProperty(action.type)) {
       const mapping = reducerObject as Record<string, ImmerReducerFunc<ReduxAction, S>>
+
       return produce(state, draft =>
-        mapping[action.type](draft, action as any, state as Immutable<S>),
+        // NOTE(tec27): The castDraft here is necessary to allow `defaultState` to be a valid return
+        castDraft(mapping[action.type](draft, action as any, state as Immutable<S>)),
       )
     } else {
       return state
