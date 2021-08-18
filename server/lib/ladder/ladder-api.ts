426618ff2aa11572ffb9ef3afa1581c263c106cb
Travis Collins <travis@tec27.com>
2021-08-17 21:20:32 -0700

    Move ladder-api to decorators.

--- server/lib/ladder/ladder-api.ts
+++ server/lib/ladder/ladder-api.ts
@@ -1,17 +1,19 @@
-import Router from '@koa/router'
+import Router, { RouterContext } from '@koa/router'
 import Joi from 'joi'
 import { GetRankingsPayload, LadderPlayer } from '../../../common/ladder'
 import { ALL_MATCHMAKING_TYPES, MatchmakingType } from '../../../common/matchmaking'
 import { User } from '../../../common/users/user-info'
-import { HttpApi, httpApi } from '../http/http-api'
-import { apiEndpoint } from '../http/http-api-endpoint'
+import { HttpApi, httpApi, httpBeforeAll } from '../http/http-api'
+import { httpGet } from '../http/route-decorators'
 import { JobScheduler } from '../jobs/job-scheduler'
 import { getRankings, refreshRankings } from '../matchmaking/models'
 import ensureLoggedIn from '../session/ensure-logged-in'
+import { validateRequest } from '../validation/joi-validator'
 
 const UPDATE_RANKS_MINUTES = 5
 
 @httpApi('/ladder')
+@httpBeforeAll(ensureLoggedIn)
 export class LadderApi implements HttpApi {
   constructor(private jobScheduler: JobScheduler) {
     const startTime = new Date()
@@ -27,41 +29,39 @@ export class LadderApi implements HttpApi {
     )
   }
 
-  applyRoutes(router: Router): void {
-    router.use(ensureLoggedIn).get('/:matchmakingType', this.getRankings)
-  }
+  applyRoutes(router: Router): void {}
 
-  getRankings = apiEndpoint(
-    {
+  @httpGet('/:matchmakingType')
+  async getRankings(ctx: RouterContext): Promise<GetRankingsPayload> {
+    const { params } = validateRequest(ctx, {
       params: Joi.object<{ matchmakingType: MatchmakingType }>({
         matchmakingType: Joi.valid(...ALL_MATCHMAKING_TYPES).required(),
       }),
-    },
-    async (ctx, { params }): Promise<GetRankingsPayload> => {
-      const rankings = await getRankings(params.matchmakingType)
+    })
 
-      const players: LadderPlayer[] = []
-      const users: User[] = []
-      for (const r of rankings) {
-        players.push({
-          rank: r.rank,
-          userId: r.userId,
-          rating: r.rating,
-          wins: r.wins,
-          losses: r.losses,
-          lastPlayedDate: Number(r.lastPlayedDate),
-        })
-        users.push({
-          id: r.userId,
-          name: r.username,
-        })
-      }
+    const rankings = await getRankings(params.matchmakingType)
 
-      return {
-        totalCount: rankings.length,
-        players,
-        users,
-      }
-    },
-  )
+    const players: LadderPlayer[] = []
+    const users: User[] = []
+    for (const r of rankings) {
+      players.push({
+        rank: r.rank,
+        userId: r.userId,
+        rating: r.rating,
+        wins: r.wins,
+        losses: r.losses,
+        lastPlayedDate: Number(r.lastPlayedDate),
+      })
+      users.push({
+        id: r.userId,
+        name: r.username,
+      })
+    }
+
+    return {
+      totalCount: rankings.length,
+      players,
+      users,
+    }
+  }
 }
