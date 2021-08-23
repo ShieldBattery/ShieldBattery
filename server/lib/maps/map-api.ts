bba0d0b4dfcd3b7a7011c09cacf41b6a4b3a22c5
Travis Collins <travis@tec27.com>
2021-08-23 00:52:39 -0700

    Move maps API to the maps folder. No attempt was made to convert it to better patterns of validation, and the return types are all way too permissive. This'll have to be cleaned up later.

---
+++ server/lib/maps/map-api.ts
@@ -0,0 +1,306 @@
+import { RouterContext } from '@koa/router'
+import { File } from 'formidable'
+import { writeFile as fsWriteFile } from 'fs/promises'
+import httpErrors from 'http-errors'
+import { withFile as withTmpFile } from 'tmp-promise'
+import {
+  ALL_MAP_EXTENSIONS,
+  ALL_MAP_SORT_TYPES,
+  ALL_MAP_VISIBILITIES,
+  MapVisibility,
+} from '../../../common/maps'
+import { deleteFiles, readFile } from '../file-upload'
+import handleMultipartFiles from '../file-upload/handle-multipart-files'
+import { httpApi, httpBeforeAll } from '../http/http-api'
+import { httpBefore, httpDelete, httpGet, httpPatch, httpPost } from '../http/route-decorators'
+import {
+  addMapToFavorites,
+  getFavoritedMaps,
+  getMapInfo,
+  getMaps,
+  removeMap,
+  removeMapFromFavorites,
+  updateMap,
+  veryDangerousDeleteAllMaps,
+} from '../maps/map-models'
+import { mapPath, storeMap, storeRegeneratedImages } from '../maps/store'
+import { checkAllPermissions } from '../permissions/check-permissions'
+import ensureLoggedIn from '../session/ensure-logged-in'
+import createThrottle from '../throttle/create-throttle'
+import throttleMiddleware from '../throttle/middleware'
+
+const mapsListThrottle = createThrottle('mapslist', {
+  rate: 30,
+  burst: 50,
+  window: 60000,
+})
+const mapUploadThrottle = createThrottle('mapupload', {
+  rate: 10,
+  burst: 20,
+  window: 60000,
+})
+const mapUpdateThrottle = createThrottle('mapupdate', {
+  rate: 20,
+  burst: 60,
+  window: 60000,
+})
+const mapFavoriteThrottle = createThrottle('mapfavorite', {
+  rate: 30,
+  burst: 70,
+  window: 60000,
+})
+const mapRemoveThrottle = createThrottle('mapremove', {
+  rate: 20,
+  burst: 40,
+  window: 60000,
+})
+
+@httpApi('/maps')
+@httpBeforeAll(ensureLoggedIn)
+export class MapsApi {
+  @httpGet('/')
+  @httpBefore(throttleMiddleware(mapsListThrottle, ctx => String(ctx.session!.userId)))
+  async list(ctx: RouterContext): Promise<any> {
+    // TODO(tec27): Use validateRequest for this stuff to remove the need for all the casts
+
+    const { q, visibility } = ctx.query
+    const { sort, numPlayers, tileset, limit, page } = ctx.query
+
+    const sortVal = parseInt(sort as string, 10)
+    if (sortVal && !ALL_MAP_SORT_TYPES.includes(sortVal)) {
+      throw new httpErrors.BadRequest('Invalid sort order option: ' + sort)
+    }
+
+    const numPlayersVal = numPlayers && JSON.parse(numPlayers as string)
+    if (
+      numPlayersVal &&
+      (!Array.isArray(numPlayersVal) || numPlayersVal.some(n => n < 2 || n > 8))
+    ) {
+      throw new httpErrors.BadRequest('Invalid filter for number of players: ' + numPlayers)
+    }
+
+    const tilesetVal = tileset && JSON.parse(tileset as string)
+    if (tilesetVal && (!Array.isArray(tilesetVal) || tilesetVal.some(n => n < 0 || n > 7))) {
+      throw new httpErrors.BadRequest('Invalid filter for tileset: ' + tileset)
+    }
+
+    let limitVal = parseInt(limit as string, 10)
+    if (!limitVal || isNaN(limitVal) || limitVal < 0 || limitVal > 100) {
+      limitVal = 60
+    }
+
+    let pageVal = parseInt(page as string, 10)
+    if (!pageVal || isNaN(pageVal) || pageVal < 0) {
+      pageVal = 0
+    }
+
+    if (!ALL_MAP_VISIBILITIES.includes(visibility as MapVisibility)) {
+      throw new httpErrors.BadRequest('Invalid map visibility: ' + visibility)
+    }
+
+    const uploadedBy = visibility === MapVisibility.Private ? ctx.session!.userId : undefined
+
+    const filters = { numPlayers: numPlayersVal, tileset: tilesetVal }
+    const favoritedBy = ctx.session!.userId
+    const [mapsResult, favoritedMaps] = await Promise.all([
+      getMaps(
+        visibility as MapVisibility,
+        sortVal,
+        filters,
+        limitVal,
+        pageVal,
+        favoritedBy,
+        uploadedBy,
+        q as string,
+      ),
+      getFavoritedMaps(favoritedBy, sortVal, filters, q as string),
+    ])
+    const { total, maps } = mapsResult
+    return {
+      maps,
+      favoritedMaps,
+      page,
+      limit,
+      total,
+    }
+  }
+
+  @httpGet('/:mapId')
+  @httpBefore(throttleMiddleware(mapsListThrottle, ctx => String(ctx.session!.userId)))
+  async getDetails(ctx: RouterContext): Promise<any> {
+    const { mapId } = ctx.params
+
+    const map = (await getMapInfo([mapId], ctx.session!.userId))[0]
+    if (!map) {
+      throw new httpErrors.NotFound('Map not found')
+    }
+
+    return {
+      map,
+    }
+  }
+
+  @httpPost('/')
+  @httpBefore(
+    throttleMiddleware(mapUploadThrottle, ctx => String(ctx.session!.userId)),
+    handleMultipartFiles,
+  )
+  async upload(ctx: RouterContext): Promise<any> {
+    // TODO(tec27): This was originally handled by the same method as the official path, and
+    // thus has logic for both. That logic can be stripped out now
+
+    const { path } = ctx.request.files!.file as File
+    const { extension } = ctx.request.body
+
+    if (!path) {
+      throw new httpErrors.BadRequest('map file must be specified')
+    } else if (!extension) {
+      throw new httpErrors.BadRequest('extension must be specified')
+    }
+
+    const lowerCaseExtension = extension.toLowerCase()
+    if (!ALL_MAP_EXTENSIONS.includes(lowerCaseExtension)) {
+      throw new httpErrors.BadRequest('Unsupported extension: ' + lowerCaseExtension)
+    }
+
+    const visibility = ctx.request.path.endsWith('/official')
+      ? MapVisibility.Official
+      : MapVisibility.Private
+    const map = await storeMap(path, lowerCaseExtension, ctx.session!.userId, visibility)
+    return {
+      map,
+    }
+  }
+
+  @httpPatch('/:mapId')
+  @httpBefore(throttleMiddleware(mapUpdateThrottle, ctx => String(ctx.session!.userId)))
+  async update(ctx: RouterContext): Promise<any> {
+    const { mapId } = ctx.params
+    const { name, description } = ctx.request.body
+
+    if (!name) {
+      throw new httpErrors.BadRequest("Map name can't be empty")
+    } else if (!description) {
+      throw new httpErrors.BadRequest("Map description can't be empty")
+    }
+
+    let map = (await getMapInfo([mapId], ctx.session!.userId))[0]
+    if (!map) {
+      throw new httpErrors.NotFound('Map not found')
+    }
+
+    // TODO(tec27): These checks are bad and should be changed before we allow anyone to make maps
+    // public
+    if (
+      [MapVisibility.Official, MapVisibility.Public].includes(map.visibility) &&
+      !ctx.session!.permissions.manageMaps
+    ) {
+      throw new httpErrors.Forbidden('Not enough permissions')
+    }
+    if (map.visibility === MapVisibility.Private && map.uploadedBy.id !== ctx.session!.userId) {
+      throw new httpErrors.Forbidden("Can't update maps of other users")
+    }
+
+    map = await updateMap(mapId, ctx.session!.userId, name, description)
+    return {
+      map,
+    }
+  }
+
+  @httpDelete('/:mapId')
+  @httpBefore(throttleMiddleware(mapRemoveThrottle, ctx => String(ctx.session!.userId)))
+  async remove(ctx: RouterContext): Promise<void> {
+    const { mapId } = ctx.params
+
+    const map = (await getMapInfo([mapId]))[0]
+    if (!map) {
+      throw new httpErrors.NotFound('Map not found')
+    }
+    // TODO(tec27): These checks are bad and should be changed before we allow anyone to make maps
+    // public
+    if (
+      (map.visibility === MapVisibility.Official || map.visibility === MapVisibility.Public) &&
+      !ctx.session!.permissions.manageMaps
+    ) {
+      throw new httpErrors.Forbidden('Not enough permissions')
+    }
+    if (map.visibility === MapVisibility.Private && map.uploadedBy.id !== ctx.session!.userId) {
+      throw new httpErrors.Forbidden("Can't remove maps of other users")
+    }
+
+    await removeMap(mapId)
+    ctx.status = 204
+  }
+
+  // TODO(tec27): Move these URLs, they're backwards
+  @httpPost('/favorites/:mapId')
+  @httpBefore(throttleMiddleware(mapFavoriteThrottle, ctx => String(ctx.session!.userId)))
+  async addToFavorites(ctx: RouterContext): Promise<void> {
+    await addMapToFavorites(ctx.params.mapId, ctx.session!.userId)
+    ctx.status = 204
+  }
+
+  @httpDelete('/favorites/:mapId')
+  @httpBefore(throttleMiddleware(mapFavoriteThrottle, ctx => String(ctx.session!.userId)))
+  async removeFromFavorites(ctx: RouterContext): Promise<void> {
+    await removeMapFromFavorites(ctx.params.mapId, ctx.session!.userId)
+    ctx.status = 204
+  }
+
+  @httpPost('/official')
+  @httpBefore(checkAllPermissions('manageMaps'), handleMultipartFiles)
+  async upload2(ctx: RouterContext): Promise<any> {
+    // TODO(tec27): This was originally handled by the same method as the non-official path, and
+    // thus has logic for both. That logic can be stripped out now
+
+    const { path } = ctx.request.files!.file as File
+    const { extension } = ctx.request.body
+
+    if (!path) {
+      throw new httpErrors.BadRequest('map file must be specified')
+    } else if (!extension) {
+      throw new httpErrors.BadRequest('extension must be specified')
+    }
+
+    const lowerCaseExtension = extension.toLowerCase()
+    if (!ALL_MAP_EXTENSIONS.includes(lowerCaseExtension)) {
+      throw new httpErrors.BadRequest('Unsupported extension: ' + lowerCaseExtension)
+    }
+
+    const visibility = ctx.request.path.endsWith('/official')
+      ? MapVisibility.Official
+      : MapVisibility.Private
+    const map = await storeMap(path, lowerCaseExtension, ctx.session!.userId, visibility)
+    return {
+      map,
+    }
+  }
+
+  @httpPost('/:mapId/regenerate')
+  @httpBefore(checkAllPermissions('manageMaps'))
+  async regenMapImage(ctx: RouterContext): Promise<void> {
+    const { mapId } = ctx.params
+
+    const map = (await getMapInfo([mapId]))[0]
+    if (!map) {
+      throw new httpErrors.NotFound('Map not found')
+    }
+
+    const mapBufferPromise = readFile(mapPath(map.hash, map.mapData.format))
+    await withTmpFile(async ({ path }) => {
+      await fsWriteFile(path, await mapBufferPromise)
+      await storeRegeneratedImages(path, map.mapData.format)
+    })
+
+    ctx.status = 204
+  }
+
+  @httpDelete('/')
+  @httpBefore(checkAllPermissions('massDeleteMaps'))
+  async deleteAllMaps(ctx: RouterContext): Promise<void> {
+    await veryDangerousDeleteAllMaps(async () => {
+      await Promise.all([deleteFiles('maps/'), deleteFiles('map_images/')])
+    })
+    ctx.status = 204
+  }
+}
