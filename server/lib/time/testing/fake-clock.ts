46adf3f414aefae6a99bffb27f28086fea6e8ffb
Travis Collins <travis@tec27.com>
2021-08-27 02:44:45 -0700

    Allow more direct control of running FakeClock setTimeouts, instead of requiring them all to be automatically run. This should make it possible to test things that want precise control over how many timeouts to run.

--- server/lib/time/testing/fake-clock.ts
+++ server/lib/time/testing/fake-clock.ts
@@ -1,6 +1,32 @@
+import { assertUnreachable } from '../../../../common/assert-unreachable'
 import { Clock, TimeoutId } from '../clock'
 
 type ScheduledTimeout = [fn: () => void, timeoutMillis: number]
+
+export enum StopCriteria {
+  EmptyQueue,
+  NumTasks,
+  TimeReached,
+}
+
+export interface EmptyQueueStopCondition {
+  criteria: StopCriteria.EmptyQueue
+}
+
+export interface NumTasksStopCondition {
+  criteria: StopCriteria.NumTasks
+  numTasks: number
+}
+
+export interface TimeReachedStopCondition {
+  criteria: StopCriteria.TimeReached
+  timeMillis: number
+}
+
+export type StopCondition =
+  | EmptyQueueStopCondition
+  | NumTasksStopCondition
+  | TimeReachedStopCondition
 
 export class FakeClock extends Clock {
   private currentTime = 0
@@ -10,6 +36,12 @@ export class FakeClock extends Clock {
   private timeoutsCompletedPromise = Promise.resolve()
   private timeoutsCompletedResolve = () => {}
   private timeoutRunnerScheduled = false
+
+  /**
+   * If true (default), automatically runs any setTimeout's as a microtask immediately after they
+   * are scheduled. (That is, they will run the next time execution returns to the event loop).
+   */
+  autoRunTimeouts = true
 
   setCurrentTime(time: number) {
     this.currentTime = time
@@ -26,45 +58,79 @@ export class FakeClock extends Clock {
   // TODO(tec27): Build a system for only running X timeouts. This auto setup only works if the
   // timeout setting terminates (e.g. a called timeout doesn't schedule an additional timeout).
   override setTimeout(fn: () => void, timeoutMillis: number): TimeoutId {
-    this.timeoutsToRun.push([fn, timeoutMillis])
-    if (!this.timeoutRunnerScheduled) {
+    this.timeoutsToRun.push([fn, this.currentTime + timeoutMillis])
+    if (this.autoRunTimeouts && !this.timeoutRunnerScheduled) {
       this.timeoutRunnerScheduled = true
       this.timeoutsCompletedPromise = new Promise(resolve => {
         this.timeoutsCompletedResolve = resolve
       })
-      queueMicrotask(() => this.runTimeouts())
+      queueMicrotask(() => this.runTimeouts({ criteria: StopCriteria.EmptyQueue }, true))
     }
 
     this.currentTimeoutId = (Number(this.currentTimeoutId) + 1) as unknown as TimeoutId
     return this.currentTimeoutId
   }
 
-  /** Returns a promise that resolves when all the currently scheduled timeouts have been run. */
+  /**
+   * Returns a promise that resolves when all the currently scheduled timeouts have been run. Only
+   * works if timeouts are set to automatically run.
+   */
   async allTimeoutsCompleted() {
+    if (!this.autoRunTimeouts) {
+      throw new Error('This method only works when FakeClock#autoRunTimeouts is true.')
+    }
     return this.timeoutsCompletedPromise
   }
 
-  private async runTimeouts() {
-    try {
-      do {
-        const toRun = this.timeoutsToRun
-        this.timeoutsToRun = []
+  /**
+   * Runs scheduled timeouts (previous and newly-scheduled) until the specified stop condition is
+   * met.
+   */
+  async runTimeoutsUntil(stopCondition: StopCondition) {
+    if (this.autoRunTimeouts) {
+      throw new Error(
+        'Timeouts are being run automatically. If you would like to run timeouts ' +
+          'manually, set FakeClock#autoRunTimeouts to false first.',
+      )
+    }
 
-        toRun.sort((a, b) => a[1] - b[1])
-        let lastTimeout = 0
-        for (const [fn, timeoutMillis] of toRun) {
-          this.currentTime += timeoutMillis - lastTimeout
-          lastTimeout = timeoutMillis
-          fn()
+    await this.runTimeouts(stopCondition, false)
+  }
+
+  private async runTimeouts(stopCondition: StopCondition, isAutoRun: boolean) {
+    try {
+      let tasksRun = 0
+      const stopConditionMet = (): boolean => {
+        switch (stopCondition.criteria) {
+          case StopCriteria.EmptyQueue:
+            return this.timeoutsToRun.length === 0
+          case StopCriteria.NumTasks:
+            return tasksRun >= stopCondition.numTasks
+          case StopCriteria.TimeReached:
+            return this.currentTime >= stopCondition.timeMillis
+          default:
+            return assertUnreachable(stopCondition)
         }
+      }
+
+      while (!stopConditionMet() && this.timeoutsToRun.length) {
+        // TODO(tec27): We could probably insert these in a sorted manner instead :)
+        this.timeoutsToRun.sort((a, b) => a[1] - b[1])
+
+        const [fn, timeoutMillis] = this.timeoutsToRun.shift()!
+        this.currentTime = Math.max(this.currentTime, timeoutMillis)
+        fn()
+        tasksRun++
 
         // In case the timer functions themselves use promises, we wait for the event queue to come
         // back around
         await new Promise<void>(resolve => setTimeout(resolve, 0))
-      } while (this.timeoutsToRun.length)
+      }
     } finally {
-      this.timeoutRunnerScheduled = false
-      this.timeoutsCompletedResolve()
+      if (isAutoRun) {
+        this.timeoutRunnerScheduled = false
+        this.timeoutsCompletedResolve()
+      }
     }
   }
 }
