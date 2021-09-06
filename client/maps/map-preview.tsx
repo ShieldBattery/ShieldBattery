c09732b332191b37962dac398c46e24464b47c50
Travis Collins <travis@tec27.com>
2021-09-06 12:30:45 -0700

    Remove background color on map preview dialog to fix artifacts due to rounding in rare cases.

--- client/maps/map-preview.tsx
+++ client/maps/map-preview.tsx
@@ -3,18 +3,27 @@ import styled from 'styled-components'
 import { CommonDialogProps } from '../dialogs/common-dialog-props'
 import { Dialog } from '../material/dialog'
 import { useAppDispatch, useAppSelector } from '../redux-hooks'
+import { background400 } from '../styles/colors'
 import { batchGetMapInfo } from './action-creators'
 import MapImage from './map-image'
 
 const StyledDialog = styled(Dialog)`
   --sb-map-aspect-ratio: calc(var(--sb-map-width, 1) / var(--sb-map-height, 1));
 
+  /**
+    We remove the background here and put it on the image instead, because sometimes rounding leads
+    to them being different heights. This does cause an artifact with the shadow (it appears to
+    start several pixels after the image), but this is less noticeable than the background color.
+  */
+  background-color: transparent;
   aspect-ratio: var(--sb-map-aspect-ratio);
   max-width: min(calc((100vh - 160px) * var(--sb-map-aspect-ratio)), calc(100% - 160px));
 `
 
 const StyledMapImage = styled(MapImage)`
   width: 100%;
+  background-color: ${background400};
+  border-radius: 2px;
 `
 
 export interface MapPreviewDialogProps extends CommonDialogProps {
