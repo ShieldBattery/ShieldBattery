ab57a2d71a108692c5a3f438091d8ea244abdc9a
Travis Collins <travis@tec27.com>
2021-10-20 19:16:14 -0700

    Add policies (acceptable use, privacy, terms of service). This is just the policy content, displaying these and notifying users of updates will come in a later commit.

---
+++ common/policies/README.md
@@ -0,0 +1,12 @@
+# policies
+
+These policies are only valid for the official version of ShieldBattery. Forks
+or other hosts should provide their own replacements to match their own policies
+and usage.
+
+## Update guide
+
+Any updates to these policies should add an entry to the changelog at the
+beginning (or add a changelog if one is not yet there) and update the effective
+date. \*_Make sure to increment the version number in `versions.ts` as well_, as
+this is how users get notified of the changes.
---
+++ common/policies/versions.ts
@@ -0,0 +1,15 @@
+/**
+ * The current version of our Acceptable Use Policy. This should be incremented every time the
+ * policy file changes.
+ */
+export const ACCEPTABLE_USE_VERSION = 1
+/**
+ * The current version of our Privacy Policy. This should be incremented every time the
+ * policy file changes.
+ */
+export const PRIVACY_POLICY_VERSION = 1
+/**
+ * The current version of our Terms of Service. This should be incremented every time the
+ * policy file changes.
+ */
+export const TERMS_OF_SERVICE_VERSION = 1
