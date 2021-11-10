ShieldBattery/ShieldBattery

commit b30e014a69b4626771893094d98695281142c613

Refactor the matchmaking preferences to support multiple matchmaking types. (#734)
This PR also refactors the terminology of "preferred maps" to a more
general "map selections". This means that "map selections" may be used
for any kind of map system we might have (e.g. in 1v1/2v2 matchmaking it
might mean "vetoed maps", while in 3v3 matchmaking it might mean "maps
selected to play on".

We're also adding 2v2 matchmaking type in here to make things easier
with typescript. Actual implementation of the 2v2 matchmaking will come
in subsequent PRs.

Finally, we've stopped rendering the tabs in find-match overlay for
types we don't support yet, e.g. 3v3. Keeping the tab showing while not
having the support for it is more trouble than it's worth, and besides,
hopefully we'll add 3v3 matchmaking soon after the 2v2 one ^^.

server/lib/maps/map-models.ts
@@ -570,7 +570,7 @@ export async function veryDangerousDeleteAllMaps(deleteFromStoreFn: () => Promis
     const query = sql`
       BEGIN;
         UPDATE lobby_preferences SET recent_maps = NULL, selected_map = NULL;
-        UPDATE matchmaking_preferences SET preferred_maps = NULL;
+        UPDATE matchmaking_preferences SET map_selections = NULL;
         TRUNCATE matchmaking_map_pools, favorited_maps, uploaded_maps, maps;
       COMMIT;
     `

client/activities/activity-overlay.jsx
@@ -14,15 +14,15 @@ import { closeOverlay } from './action-creators'
 
 const {
   FindMatch,
-  BrowsePreferredMaps,
+  BrowseMapSelections,
   CreateLobby,
   WatchReplay,
   BrowseLocalMaps,
   BrowseServerMaps,
 } = IS_ELECTRON
   ? {
-      FindMatch: require('../matchmaking/find-match').default,
-      BrowsePreferredMaps: require('../matchmaking/browse-preferred-maps').default,
+      FindMatch: require('../matchmaking/find-match').FindMatch,
+      BrowseMapSelections: require('../matchmaking/browse-map-selections').default,
       CreateLobby: require('../lobbies/create-lobby').default,
       WatchReplay: require('../replays/watch-replay').default,
       BrowseLocalMaps: require('../maps/browse-local-maps').default,
@@ -126,8 +126,8 @@ export default class ActivityOverlay extends React.Component {
     switch (activityOverlay.current.overlayType) {
       case 'findMatch':
         return FindMatch
-      case 'browsePreferredMaps':
-        return BrowsePreferredMaps
+      case 'browseMapSelections':
+        return BrowseMapSelections
       case 'createLobby':
         return CreateLobby
       case 'joinLobby':

client/admin/map-pools.jsx
@@ -491,26 +491,15 @@ class MapPoolHistory extends React.PureComponent {
   }
 }
 
-const TAB_1V1 = 0
-
-function tabToType(tab) {
-  switch (tab) {
-    case TAB_1V1:
-      return MatchmakingType.Match1v1
-    default:
-      throw new Error('Invalid tab value')
-  }
-}
-
 @connect(state => ({ mapPools: state.mapPools }))
 export default class MapPools extends React.Component {
   state = {
-    activeTab: TAB_1V1,
+    activeTab: MatchmakingType.Match1v1,
     initialMaps: new List(),
   }
 
   componentDidMount() {
-    this.props.dispatch(getMapPoolHistory(tabToType(this.state.activeTab), MAP_POOLS_LIMIT, 0))
+    this.props.dispatch(getMapPoolHistory(this.state.activeTab, MAP_POOLS_LIMIT, 0))
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -518,19 +507,20 @@ export default class MapPools extends React.Component {
     const { activeTab: newTab } = this.state
 
     if (oldTab !== newTab) {
-      this.props.dispatch(getMapPoolHistory(tabToType(newTab), MAP_POOLS_LIMIT, 0))
+      this.props.dispatch(getMapPoolHistory(newTab, MAP_POOLS_LIMIT, 0))
     }
   }
 
   render() {
     const { mapPools } = this.props
     const { activeTab, initialMaps } = this.state
-    const mapPoolHistory = mapPools.types.get(tabToType(activeTab))
+    const mapPoolHistory = mapPools.types.get(activeTab)
 
     return (
       <Container>
         <StyledTabs activeTab={activeTab} onChange={this.onTabChange}>
-          <TabItem text='1v1' value={TAB_1V1} />
+          <TabItem text='1v1' value={MatchmakingType.Match1v1} />
+          <TabItem text='2v2' value={MatchmakingType.Match2v2} />
         </StyledTabs>
         <h3>Create a new map pool</h3>
         <MapPoolEditor
@@ -566,11 +556,11 @@ export default class MapPools extends React.Component {
   }
 
   onCreateNewMapPool = (maps, startDate) => {
-    this.props.dispatch(createMapPool(tabToType(this.state.activeTab), maps, startDate))
+    this.props.dispatch(createMapPool(this.state.activeTab, maps, startDate))
   }
 
   onUseAsTemplate = id => {
-    const mapPoolHistory = this.props.mapPools.types.get(tabToType(this.state.activeTab))
+    const mapPoolHistory = this.props.mapPools.types.get(this.state.activeTab)
     const mapPool = mapPoolHistory.byId.get(id)
 
     if (!mapPool) return
@@ -579,6 +569,6 @@ export default class MapPools extends React.Component {
   }
 
   onDeleteMapPool = id => {
-    this.props.dispatch(deleteMapPool(tabToType(this.state.activeTab), id))
+    this.props.dispatch(deleteMapPool(this.state.activeTab, id))
   }
 }

client/admin/matchmaking-times.jsx
@@ -205,17 +205,6 @@ class MatchmakingTimesHistory extends React.PureComponent {
   }
 }
 
-const TAB_1V1 = 0
-
-function tabToType(tab) {
-  switch (tab) {
-    case TAB_1V1:
-      return MatchmakingType.Match1v1
-    default:
-      throw new Error('Invalid tab value')
-  }
-}
-
 const Container = styled.div`
   max-width: 800px;
   padding: 0 16px;
@@ -247,7 +236,7 @@ const AddNewButton = styled(RaisedButton)`
 @connect(state => ({ matchmakingTimes: state.matchmakingTimes }))
 export default class MatchmakingTimes extends React.Component {
   state = {
-    activeTab: TAB_1V1,
+    activeTab: MatchmakingType.Match1v1,
     startDate: '',
     invalidDate: false,
     enabled: false,
@@ -256,7 +245,7 @@ export default class MatchmakingTimes extends React.Component {
   }
 
   componentDidMount() {
-    this.props.dispatch(getMatchmakingTimesHistory(tabToType(this.state.activeTab)))
+    this.props.dispatch(getMatchmakingTimesHistory(this.state.activeTab))
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -264,7 +253,7 @@ export default class MatchmakingTimes extends React.Component {
     const { activeTab: newTab } = this.state
 
     if (oldTab !== newTab) {
-      this.props.dispatch(getMatchmakingTimesHistory(tabToType(this.state.activeTab)))
+      this.props.dispatch(getMatchmakingTimesHistory(this.state.activeTab))
     }
   }
 
@@ -272,7 +261,7 @@ export default class MatchmakingTimes extends React.Component {
     const { matchmakingTimes } = this.props
     const { activeTab, startDate, invalidDate, enabled, futureTimesPage, pastTimesPage } =
       this.state
-    const matchmakingTimesHistory = matchmakingTimes.types.get(tabToType(activeTab))
+    const matchmakingTimesHistory = matchmakingTimes.types.get(activeTab)
 
     let dateValidationContents
     if (invalidDate) {
@@ -287,7 +276,8 @@ export default class MatchmakingTimes extends React.Component {
       <ScrollableContent>
         <Container>
           <Tabs activeTab={activeTab} onChange={this.onTabChange}>
-            <TabItem text='1v1' value={TAB_1V1} />
+            <TabItem text='1v1' value={MatchmakingType.Match1v1} />
+            <TabItem text='2v2' value={MatchmakingType.Match2v2} />
           </Tabs>
           <h3>Add a new matchmaking time</h3>
           <p>Choose a date and time (in your local timezone) when the matchmaking will start</p>
@@ -345,14 +335,14 @@ export default class MatchmakingTimes extends React.Component {
     const { activeTab, startDate, enabled } = this.state
 
     this.setState({ startDate: '', enabled: false })
-    this.props.dispatch(addMatchmakingTime(tabToType(activeTab), Date.parse(startDate), enabled))
+    this.props.dispatch(addMatchmakingTime(activeTab, Date.parse(startDate), enabled))
   }
 
   onLoadMoreFutureTimes = () => {
     const { activeTab, futureTimesPage } = this.state
 
     this.props.dispatch(
-      getMatchmakingTimesFuture(tabToType(activeTab), MATCHMAKING_TIMES_LIMIT, futureTimesPage),
+      getMatchmakingTimesFuture(activeTab, MATCHMAKING_TIMES_LIMIT, futureTimesPage),
     )
     this.setState({ futureTimesPage: futureTimesPage + 1 })
   }
@@ -360,13 +350,11 @@ export default class MatchmakingTimes extends React.Component {
   onLoadMorePastTimes = () => {
     const { activeTab, pastTimesPage } = this.state
 
-    this.props.dispatch(
-      getMatchmakingTimesPast(tabToType(activeTab), MATCHMAKING_TIMES_LIMIT, pastTimesPage),
-    )
+    this.props.dispatch(getMatchmakingTimesPast(activeTab, MATCHMAKING_TIMES_LIMIT, pastTimesPage))
     this.setState({ pastTimesPage: pastTimesPage + 1 })
   }
 
   onDeleteMatchmakingTime = id => {
-    this.props.dispatch(deleteMatchmakingTime(tabToType(this.state.activeTab), id))
+    this.props.dispatch(deleteMatchmakingTime(this.state.activeTab, id))
   }
 }

client/main-layout.jsx
@@ -231,17 +231,18 @@ class MainLayout extends React.Component {
   renderSearchingMatchOverlay() {
     const {
       matchmaking: { isFinding, searchStartTime },
-      matchmakingPreferences: { matchmakingType, race },
+      matchmakingPreferences: { lastQueuedMatchmakingType, typeToPreferences },
     } = this.props
-    if (!isFinding || !IS_ELECTRON) return null
+    const preferences = typeToPreferences.get(lastQueuedMatchmakingType)
+    if (!isFinding || !preferences || !IS_ELECTRON) return null
 
     return (
       <MatchmakingSearchingOverlay
         open={this.state.searchingMatchOverlayOpen}
         anchor={this._searchingMatchButtonRef.current}
         startTime={searchStartTime}
-        matchmakingType={matchmakingType}
-        selectedRace={race}
+        matchmakingType={preferences.matchmakingType}
+        selectedRace={preferences.race}
         onCancelSearch={() => {
           this.onCancelFindMatchClick()
           this.onSearchingMatchOverlayClose()

client/matchmaking/action-creators.ts
@@ -1,37 +1,83 @@
+import { List } from 'immutable'
+import { assertUnreachable } from '../../common/assert-unreachable'
 import { TypedIpcRenderer } from '../../common/ipc'
+import { MapInfoJson } from '../../common/maps'
 import {
   GetPreferencesPayload,
   MatchmakingMapPool,
   MatchmakingPreferences,
+  MatchmakingPreferences1v1,
+  MatchmakingPreferences2v2,
   MatchmakingType,
 } from '../../common/matchmaking'
-import { AssignedRaceChar, RaceChar } from '../../common/races'
 import { ThunkAction } from '../dispatch-registry'
 import { clientId } from '../network/client-id'
 import fetch from '../network/fetch'
 import { apiUrl } from '../network/urls'
+import { UpdateLastQueuedMatchmakingType } from './actions'
+import { MatchmakingPreferencesRecord } from './matchmaking-preferences-reducer'
 
 const ipcRenderer = new TypedIpcRenderer()
 
-export function findMatch(
-  type: MatchmakingType,
-  race: RaceChar,
-  useAlternateRace: boolean,
-  alternateRace: AssignedRaceChar,
-  preferredMaps: string[],
-): ThunkAction {
-  ipcRenderer.send('rallyPointRefreshPings')
+function format1v1Preferences(
+  prefs: MatchmakingPreferencesRecord,
+  mapSelections: List<MapInfoJson>,
+  userId: number,
+): MatchmakingPreferences1v1 {
+  const mapSelectionIds = mapSelections.map(m => m.id).toArray()
 
-  const params = {
-    clientId,
-    type,
-    race,
-    useAlternateRace,
-    alternateRace,
-    preferredMaps,
+  return {
+    userId,
+    matchmakingType: MatchmakingType.Match1v1,
+    race: prefs.race,
+    mapPoolId: prefs.mapPoolId,
+    mapSelections: mapSelectionIds,
+    data: {
+      useAlternateRace: prefs.race !== 'r' ? prefs.data.useAlternateRace : false,
+      alternateRace: prefs.data.alternateRace,
+    },
   }
+}
 
+function format2v2Preferences(
+  prefs: MatchmakingPreferencesRecord,
+  mapSelections: List<MapInfoJson>,
+  userId: number,
+): MatchmakingPreferences2v2 {
+  const mapSelectionIds = mapSelections.map(m => m.id).toArray()
+
+  return {
+    userId,
+    matchmakingType: MatchmakingType.Match2v2,
+    race: prefs.race,
+    mapPoolId: prefs.mapPoolId,
+    mapSelections: mapSelectionIds,
+    data: {},
+  }
+}
+
+export function findMatch(
+  matchmakingType: MatchmakingType,
+  prefs: MatchmakingPreferencesRecord,
+  mapSelections: List<MapInfoJson>,
+  userId: number,
+): ThunkAction {
   return dispatch => {
+    ipcRenderer.send('rallyPointRefreshPings')
+
+    let preferences: MatchmakingPreferences
+    switch (matchmakingType) {
+      case MatchmakingType.Match1v1:
+        preferences = format1v1Preferences(prefs, mapSelections, userId)
+        break
+      case MatchmakingType.Match2v2:
+        preferences = format2v2Preferences(prefs, mapSelections, userId)
+        break
+      default:
+        assertUnreachable(matchmakingType)
+    }
+
+    const params = { clientId, preferences }
     dispatch({
       type: '@matchmaking/findMatchBegin',
       payload: params,
@@ -42,7 +88,18 @@ export function findMatch(
       payload: fetch<void>(apiUrl`matchmaking/find`, {
         method: 'POST',
         body: JSON.stringify(params),
-      }).then<{ startTime: number }>(() => ({ startTime: window.performance.now() })),
+      }).then<{ startTime: number }>(() => {
+        const { matchmakingType: type } = preferences
+
+        dispatch(updateLastQueuedMatchmakingType(type))
+        // Load the current map pool in the store so we can download all of the maps in it as soon
+        // as the player queues.
+        dispatch(getCurrentMapPool(type))
+
+        return {
+          startTime: window.performance.now(),
+        }
+      }),
       meta: params,
     })
   }
@@ -79,38 +136,68 @@ export function getCurrentMapPool(type: MatchmakingType): ThunkAction {
     })
     dispatch({
       type: '@matchmaking/getCurrentMapPool',
-      payload: fetch<MatchmakingMapPool>(apiUrl`matchmakingMapPools/${type}/current`),
+      payload: fetch<MatchmakingMapPool>(
+        apiUrl`matchmakingMapPools/${type}/current`,
+      ).then<MatchmakingMapPool>(mapPool => {
+        // As a slight optimization, we download the whole map pool as soon as we get it. This
+        // shouldn't be a prohibitively expensive operation, since our map store checks if a map
+        // already exists before attempting to download it.
+        mapPool.maps.forEach((map: MapInfoJson) =>
+          ipcRenderer
+            .invoke('mapStoreDownloadMap', map.hash, map.mapData.format, map.mapUrl!)
+            ?.catch(err => {
+              // This is already logged to our file by the map store, so we just log it to the
+              // console for easy visibility during development
+              console.error('Error downloading map: ' + err + '\n' + err.stack)
+            }),
+        )
+
+        return mapPool
+      }),
       meta: { type },
     })
   }
 }
 
-export function getMatchmakingPreferences(matchmakingType: MatchmakingType): ThunkAction {
+export function updateMatchmakingPreferences(
+  matchmakingType: MatchmakingType,
+  prefs: MatchmakingPreferencesRecord,
+  mapSelections: List<MapInfoJson>,
+  userId: number,
+): ThunkAction {
   return dispatch => {
-    dispatch({ type: '@matchmaking/getPreferencesBegin', payload: { type: matchmakingType } })
-    dispatch({
-      type: '@matchmaking/getPreferences',
-      payload: fetch<GetPreferencesPayload>(apiUrl`matchmakingPreferences/${matchmakingType}`),
-      meta: { type: matchmakingType },
-    })
-  }
-}
+    let preferences: MatchmakingPreferences
+    switch (matchmakingType) {
+      case MatchmakingType.Match1v1:
+        preferences = format1v1Preferences(prefs, mapSelections, userId)
+        break
+      case MatchmakingType.Match2v2:
+        preferences = format2v2Preferences(prefs, mapSelections, userId)
+        break
+      default:
+        assertUnreachable(matchmakingType)
+    }
 
-export function updateMatchmakingPreferences(preferences: MatchmakingPreferences): ThunkAction {
-  return dispatch => {
-    const { matchmakingType } = preferences
     dispatch({
       type: '@matchmaking/updatePreferencesBegin',
-      payload: preferences,
-      meta: { type: matchmakingType },
+      payload: matchmakingType,
     })
     dispatch({
       type: '@matchmaking/updatePreferences',
       payload: fetch<GetPreferencesPayload>(apiUrl`matchmakingPreferences/${matchmakingType}`, {
-        method: 'post',
+        method: 'POST',
         body: JSON.stringify(preferences),
       }),
       meta: { type: preferences.matchmakingType },
     })
+  }
+}
+
+export function updateLastQueuedMatchmakingType(
+  type: MatchmakingType,
+): UpdateLastQueuedMatchmakingType {
+  return {
+    type: '@matchmaking/updateLastQueuedMatchmakingType',
+    payload: type,
   }
 }

client/matchmaking/actions.ts
@@ -7,7 +7,6 @@ import {
   MatchmakingPreferences,
   MatchmakingType,
 } from '../../common/matchmaking'
-import { AssignedRaceChar, RaceChar } from '../../common/races'
 import { BaseFetchFailure } from '../network/fetch-action-types'
 import { MatchmakingMatchRecord } from './matchmaking-reducer'
 
@@ -15,12 +14,11 @@ export type MatchmakingActions =
   | GetCurrentMapPoolBegin
   | GetCurrentMapPoolSuccess
   | GetCurrentMapPoolFailure
-  | GetPreferencesBegin
-  | GetPreferencesSuccess
-  | GetPreferencesFailure
+  | InitPreferences
   | UpdatePreferencesBegin
   | UpdatePreferencesSuccess
   | UpdatePreferencesFailure
+  | UpdateLastQueuedMatchmakingType
   | FindMatchBegin
   | FindMatchSuccess
   | FindMatchFailure
@@ -65,25 +63,19 @@ export interface GetCurrentMapPoolFailure
   }
 }
 
-export interface GetPreferencesBegin {
-  type: '@matchmaking/getPreferencesBegin'
-  payload: { type: MatchmakingType }
-}
-
-export interface GetPreferencesSuccess {
-  type: '@matchmaking/getPreferences'
-  payload: GetPreferencesPayload
-  error?: false
-  meta: { type: MatchmakingType }
-}
-
-export interface GetPreferencesFailure extends BaseFetchFailure<'@matchmaking/getPreferences'> {
+/**
+ * Initialize the user's matchmaking preferences when they connect to the application. If they don't
+ * have any preferences saved yet, the default values will be used.
+ */
+export interface InitPreferences {
+  type: '@matchmaking/initPreferences'
+  payload: GetPreferencesPayload | Record<string, undefined>
   meta: { type: MatchmakingType }
 }
 
 export interface UpdatePreferencesBegin {
   type: '@matchmaking/updatePreferencesBegin'
-  payload: MatchmakingPreferences
+  payload: MatchmakingType
 }
 
 export interface UpdatePreferencesSuccess {
@@ -98,15 +90,21 @@ export interface UpdatePreferencesFailure
   meta: { type: MatchmakingType }
 }
 
+/**
+ * Update the user's last queued matchmaking type. This matchmaking type will be used when user
+ * opens the find-match overlay again. This only does the update on the client; the server will do
+ * its own thing where it saves this value on the user's session.
+ */
+export interface UpdateLastQueuedMatchmakingType {
+  type: '@matchmaking/updateLastQueuedMatchmakingType'
+  payload: MatchmakingType
+}
+
 export interface FindMatchBegin {
   type: '@matchmaking/findMatchBegin'
   payload: {
     clientId: string
-    type: MatchmakingType
-    race: RaceChar
-    useAlternateRace: boolean
-    alternateRace: AssignedRaceChar
-    preferredMaps: string[]
+    preferences: MatchmakingPreferences
   }
 }
 
@@ -117,11 +115,7 @@ export interface FindMatchSuccess {
   }
   meta?: {
     clientId: string
-    type: MatchmakingType
-    race: RaceChar
-    useAlternateRace: boolean
-    alternateRace: AssignedRaceChar
-    preferredMaps: string[]
+    preferences: MatchmakingPreferences
   }
   error?: false
 }
@@ -129,11 +123,7 @@ export interface FindMatchSuccess {
 export interface FindMatchFailure extends BaseFetchFailure<'@matchmaking/findMatch'> {
   meta?: {
     clientId: string
-    type: MatchmakingType
-    race: RaceChar
-    useAlternateRace: boolean
-    alternateRace: AssignedRaceChar
-    preferredMaps: string[]
+    preferences: MatchmakingPreferences
   }
 }
 
@@ -193,7 +183,7 @@ export interface MatchReady {
     slots: Slot[]
     players: MatchmakingPlayer[]
     mapsByPlayer: { [key: number]: MapInfoJson }
-    preferredMaps: MapInfoJson[]
+    mapSelections: MapInfoJson[]
     randomMaps: MapInfoJson[]
     chosenMap: MapInfoJson
   }

client/matchmaking/browse-map-selections.jsx
@@ -18,7 +18,7 @@ import { ScrollableContent } from '../material/scroll-bar'
 import LoadingIndicator from '../progress/dots'
 import { colorDividers, colorError, colorTextSecondary } from '../styles/colors'
 import { Body1Old, HeadlineOld, SubheadingOld } from '../styles/typography'
-import { getCurrentMapPool } from './action-creators'
+import { getCurrentMapPool, updateMatchmakingPreferences } from './action-creators'
 
 const ENTER = 'Enter'
 const ENTER_NUMPAD = 'NumpadEnter'
@@ -96,7 +96,7 @@ const SelectionsText = styled(SubheadingOld)`
 `
 
 @form()
-class PreferredMapsForm extends React.Component {
+class MapSelectionsForm extends React.Component {
   static propTypes = {
     mapPool: PropTypes.object.isRequired,
     favoriteStatusRequests: PropTypes.instanceOf(Set),
@@ -123,7 +123,7 @@ class PreferredMapsForm extends React.Component {
           random map choice for each match.
         </InfoText>
         <MapSelect
-          {...bindCustom('preferredMaps')}
+          {...bindCustom('mapSelections')}
           list={mapPool.maps}
           byId={mapPool.byId}
           maxSelections={2}
@@ -139,15 +139,20 @@ class PreferredMapsForm extends React.Component {
   }
 }
 
-@connect(state => ({ maps: state.maps, matchmaking: state.matchmaking }))
-export default class PreferredMaps extends React.Component {
+@connect(state => ({
+  auth: state.auth,
+  maps: state.maps,
+  matchmaking: state.matchmaking,
+  matchmakingPreferences: state.matchmakingPreferences,
+}))
+export default class MapSelections extends React.Component {
   static propTypes = {
     type: PropTypes.oneOf(Object.values(MatchmakingType)).isRequired,
-    preferredMaps: PropTypes.instanceOf(List),
+    mapSelections: PropTypes.instanceOf(List),
   }
 
   static defaultProps = {
-    preferredMaps: new List(),
+    mapSelections: new List(),
   }
 
   state = {
@@ -161,7 +166,7 @@ export default class PreferredMaps extends React.Component {
   }
 
   renderContents() {
-    const { maps, matchmaking, type, preferredMaps } = this.props
+    const { maps, matchmaking, type, mapSelections } = this.props
     const mapPool = matchmaking.mapPoolTypes.get(type)
 
     if (!mapPool) return null
@@ -179,11 +184,11 @@ export default class PreferredMaps extends React.Component {
     }
 
     const model = {
-      preferredMaps,
+      mapSelections,
     }
 
     return (
-      <PreferredMapsForm
+      <MapSelectionsForm
         ref={this._form}
         model={model}
         mapPool={mapPool}
@@ -197,10 +202,10 @@ export default class PreferredMaps extends React.Component {
   }
 
   render() {
-    const { preferredMaps } = this.props
+    const { mapSelections } = this.props
     const { selectionCount } = this.state
 
-    const selectCount = selectionCount === -1 ? preferredMaps.size : selectionCount
+    const selectCount = selectionCount === -1 ? mapSelections.size : selectionCount
     return (
       <Container>
         <KeyListener onKeyDown={this.onKeyDown} />
@@ -239,19 +244,21 @@ export default class PreferredMaps extends React.Component {
   }
 
   onChange = () => {
-    const { preferredMaps } = this._form.current.getModel()
+    const { mapSelections } = this._form.current.getModel()
 
-    this.setState({ selectionCount: preferredMaps.size })
+    this.setState({ selectionCount: mapSelections.size })
   }
 
   onSubmit = () => {
-    const { preferredMaps } = this._form.current.getModel()
-    const { matchmaking, type } = this.props
+    const { mapSelections } = this._form.current.getModel()
+    const { auth, matchmaking, matchmakingPreferences, type } = this.props
 
     const mapPool = matchmaking.mapPoolTypes.get(type)
-    const preferred = preferredMaps.map(mapId => mapPool.byId.get(mapId))
+    const prefs = matchmakingPreferences.typeToPreferences.get(type)
+    const selections = mapSelections.map(mapId => mapPool.byId.get(mapId))
 
-    this.props.dispatch(openOverlay('findMatch', { preferredMaps: preferred }))
+    this.props.dispatch(updateMatchmakingPreferences(type, prefs, selections, auth.user.id))
+    this.props.dispatch(openOverlay('findMatch', { type, mapSelections: selections }))
   }
 
   onKeyDown = event => {

commit 570379d7cb977a03d7e49540ef66e6b032b81805

Fix deleting all maps
It was broken from DB changes in b30e014a69b4626771893094d98695281142c613.

server/lib/maps/map-models.ts
@@ -571,7 +571,7 @@ export async function veryDangerousDeleteAllMaps(deleteFromStoreFn: () => Promis
     const query = sql`
       BEGIN;
         UPDATE lobby_preferences SET recent_maps = NULL, selected_map = NULL;
-        UPDATE matchmaking_preferences SET map_selections = NULL;
+        UPDATE matchmaking_preferences SET map_selections = '{}';
         TRUNCATE matchmaking_map_pools, favorited_maps, uploaded_maps, maps;
       COMMIT;
     `