6bd5a0fb43c595ff937b97849bb04a7b4922252a
T1mL3arn <T1mL3arn@users.noreply.github.com>
2021-11-11 10:53:39 +1000

    address PR comments

--- client/messaging/actions.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export type MessagingActions = MaybeOpenExternalLinkDialog
-
-export interface MaybeOpenExternalLinkDialog {
-  type: '@messaging/maybeOpenExternalLinkDialog'
-  payload: React.MouseEvent<HTMLAnchorElement>
-}
