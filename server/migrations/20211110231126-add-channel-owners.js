fe34574b2c2886b6194235b774031ddf68b6a5ad
2Pacalypse- <marko@kastela.com>
2021-11-13 01:40:07 +0100

    Give the channel ownership (and all the other permissions) to the earliest joined user in the migration.

--- server/migrations/20211110231126-add-channel-owners.js
+++ server/migrations/20211110231126-add-channel-owners.js
@@ -10,6 +10,20 @@ exports.up = async function (db) {
     ON joined_channels (channel_name, owner)
     WHERE owner = true;
   `)
+
+  // Give the channel ownership (and all the other permissions) to the earliest joined user.
+  await db.runSql(`
+    WITH o AS (
+      SELECT DISTINCT ON (channel_name) *
+      FROM joined_channels
+      ORDER BY channel_name, join_date
+    )
+    UPDATE joined_channels AS jc
+    SET kick = true, ban = true, change_topic = true, toggle_private = true,
+      edit_permissions = true, owner = true
+    FROM o
+    WHERE jc.user_id = o.user_id AND jc.channel_name = o.channel_name;
+  `)
 }
 
 exports.down = async function (db) {
