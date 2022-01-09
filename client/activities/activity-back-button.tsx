bb1bff2a8ddd677851548ede1546e8cf89051418
Travis Collins <travis@tec27.com>
2022-01-08 23:03:18 -0800

    Move a few easy things to TS.

---
+++ client/activities/activity-back-button.tsx
@@ -0,0 +1,25 @@
+import React, { useCallback, useState } from 'react'
+import styled from 'styled-components'
+import BackIcon from '../icons/material/baseline-arrow_back-24px.svg'
+import { IconButton } from '../material/button'
+import { useAppDispatch, useAppSelector } from '../redux-hooks'
+import { goBack } from './action-creators'
+
+const BackButton = styled(IconButton)`
+  margin-right: 16px;
+`
+
+export function ActivityBackButton() {
+  const dispatch = useAppDispatch()
+  const historySize = useAppSelector(s => s.activityOverlay.history.size)
+  // We only check to see if the back button should display on the first render, and keep it from
+  // then on. This assumes that the back stack cannot change without the ActivityOverlay content
+  // changing, which seems to be an accurate assumption. By doing it this way, we prevent the
+  // back button from disappearing during transitions (e.g. if you click off the overlay)
+  const [shouldShow] = useState(historySize >= 2)
+  const onBackClick = useCallback(() => {
+    dispatch(goBack() as any)
+  }, [dispatch])
+
+  return shouldShow ? <BackButton icon={<BackIcon />} title='Back' onClick={onBackClick} /> : null
+}
--- client/avatars/computer-avatar.tsx
+++ client/avatars/computer-avatar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ComputerIcon from '../icons/material/ic_memory_black_24px.svg'
 import { IconAvatar, IconContainer } from './avatar'
 
-export default props => (
+export default (props: { className?: string }) => (
   <IconContainer {...props}>
     <IconAvatar as={ComputerIcon} />
   </IconContainer>
