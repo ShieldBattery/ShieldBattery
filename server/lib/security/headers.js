a92cacfa7cd0ca2a1e40c1dea7a527201149a090
Travis Collins <travis@tec27.com>
2022-01-09 16:16:27 -0800

    Ensure secure headers get set even on error responses.

--- server/lib/security/headers.js
+++ server/lib/security/headers.js
@@ -1,12 +1,15 @@
 // middleware to add some more secure headers to our responses
 export default function () {
   return async function secureHeaders(ctx, next) {
-    await next()
-    // prevent framing of our page off our domain
-    ctx.response.set('X-Frame-Options', 'SAMEORIGIN')
-    // prevent content type sniffing
-    ctx.response.set('X-Content-Type-Options', 'nosniff')
-    // turn on xss protection in IE
-    ctx.response.set('X-XSS-Protection', '1; mode=block')
+    try {
+      await next()
+    } finally {
+      // prevent framing of our page off our domain
+      ctx.response.set('X-Frame-Options', 'SAMEORIGIN')
+      // prevent content type sniffing
+      ctx.response.set('X-Content-Type-Options', 'nosniff')
+      // turn on xss protection in IE
+      ctx.response.set('X-XSS-Protection', '1; mode=block')
+    }
   }
 }
