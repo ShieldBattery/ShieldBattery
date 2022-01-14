cea1d5f8c536f02c2d7b4100838fd20df85dca38
Travis Collins <travis@tec27.com>
2022-01-14 02:49:57 -0800

    Add a test for logging in with the wrong on an existing account.

--- integration/tests/login.spec.ts
+++ integration/tests/login.spec.ts
@@ -20,3 +20,13 @@ test('logging in with a non-existent account', async ({ page }) => {
   const errorMessage = await page.innerText('[data-test=errors-container]')
   expect(errorMessage).toBe('Incorrect username or password')
 })
+
+test('logging in with the wrong password', async ({ page }) => {
+  await page.goto('/login')
+  await page.fill('input[name="username"]', 'admin')
+  await page.fill('input[name="password"]', 'NotMyPassword')
+  await page.click('[data-test=submit-button]')
+
+  const errorMessage = await page.innerText('[data-test=errors-container]')
+  expect(errorMessage).toBe('Incorrect username or password')
+})
