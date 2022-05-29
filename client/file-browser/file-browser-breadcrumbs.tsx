2e09829ba933a33b9b1efa6b8ed1db76608718b3
Marko Žarković <marko@kastela.com>
2022-05-29 17:19:19 +0200

    Refactor file-browser to not use redux. (#841)
    It's unlikely we would've ever used the file-browser state outside the
    file-browser itself, so it was not worth the complexity and boilerplate
    of redux. This simplifies the file-browser quite a bit, which is always
    a good thing. Now it's actually pleasurable to work on this component,
    hooray!
    
    A few additional changes in this PR:
      - Separate the file-browser breadcrumbs and entries into their own
        files
      - Remove a leftover "externalError" message that was not used anymore
      - Add props to allow any file-browser type (e.g. maps, replays) to
        sort the folders and files in any way they want, defaulting to
        sorting both by name. Note that this is different than what we had
        so far, where we sorted the replay files by date. I feel like that
        caused more confusion than it helped and I'm planning on adding a
        way for users to specify how they want their files sorted anyway
        soon(tm).

---
+++ client/file-browser/file-browser-breadcrumbs.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import styled from 'styled-components'
+import ChevronRight from '../icons/material/ic_chevron_right_black_24px.svg'
+import { colorTextFaint, colorTextPrimary, colorTextSecondary } from '../styles/colors'
+import { headline6 } from '../styles/typography'
+
+const BreadcrumbPiece = styled.span<{ $active: boolean }>`
+  ${headline6};
+  padding: 8px;
+  flex-grow: 0;
+  flex-shrink: 0;
+
+  font-weight: normal;
+  color: ${props => (props.$active ? colorTextPrimary : colorTextSecondary)};
+  cursor: ${props => (props.$active ? 'auto' : 'pointer')};
+`
+
+const BreadcrumbSeparator = styled(ChevronRight)`
+  display: inline-block;
+  flex-grow: 0;
+  flex-shrink: 0;
+  color: ${colorTextFaint};
+`
+
+interface PathBreadcrumbsProps {
+  className?: string
+  path: string
+  onNavigate: (navPath: string) => void
+}
+
+export const PathBreadcrumbs = React.memo<PathBreadcrumbsProps>(
+  ({ className, onNavigate, path }) => {
+    const pieces = path.split(/[\\\/]/g)
+    if (pieces[pieces.length - 1] === '') {
+      // Remove the last entry if it's empty (due to a trailing slash)
+      pieces.pop()
+    }
+    const { elems } = pieces.reduce<{ elems: React.ReactNode[]; curPath: string }>(
+      (r, piece, i) => {
+        if (piece.length === 0) {
+          return r
+        }
+        const isLast = i === pieces.length - 1
+        r.curPath += (i === 0 ? '' : '\\') + piece
+        // Save the value at the current time so the function doesn't always use the last value
+        const navPath = r.curPath
+        r.elems.push(
+          <BreadcrumbPiece
+            key={i}
+            $active={isLast}
+            onClick={isLast ? undefined : () => onNavigate(navPath)}>
+            {piece}
+          </BreadcrumbPiece>,
+        )
+        r.elems.push(<BreadcrumbSeparator key={i + '|'} />)
+
+        return r
+      },
+      { elems: [], curPath: '' },
+    )
+
+    return <div className={className}>{elems}</div>
+  },
+  (prevProps, nextProps) => prevProps.path === nextProps.path,
+)
