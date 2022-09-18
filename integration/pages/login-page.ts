68e5504334cddb17c00a4d7dd2409e074a194792
Viktor Kalinin <kalinin.viktor@gmail.com>
2022-09-18 04:13:05 +0300

    Split loginWith method into smaller bits
    There is a test that clicks "Remember me" checkbox during filling login form

--- integration/pages/login-page.ts
+++ integration/pages/login-page.ts
@@ -6,6 +6,7 @@ export class LoginPage {
 
   private readonly inputUsername: Locator
   private readonly inputPassword: Locator
+  private readonly inputRememberMe: Locator
 
   private readonly buttonLogIn: Locator
 
@@ -14,14 +15,27 @@ export class LoginPage {
 
     this.inputUsername = page.locator('input[name="username"]')
     this.inputPassword = page.locator('input[name="password"]')
+    this.inputRememberMe = page.locator('input[name="remember"]')
 
     this.buttonLogIn = page.locator('button[data-test="submit-button"]')
   }
 
   async loginWith(username: string, password: string): Promise<void> {
+    await this.fillLoginForm(username, password)
+
+    await this.clickLogInButton()
+  }
+
+  async fillLoginForm(username: string, password: string): Promise<void> {
     await this.inputUsername.fill(username)
     await this.inputPassword.fill(password)
+  }
 
+  async checkRememberMe(): Promise<void> {
+    await this.inputRememberMe.check()
+  }
+
+  async clickLogInButton(): Promise<void> {
     await this.buttonLogIn.click()
   }
 }
