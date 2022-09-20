d1f322919ec03689b33621c3619676a1d5aa63aa
Travis Collins <travis@tec27.com>
2022-09-19 17:03:17 -0700

    Fix using context menus inside popovers causing both the popover + context menu to close.
    This adds a PortalContext that manages the layering for Portals, such
    that only the root Portal attaches click handlers and delegates those
    events to the nested Portals. By doing so, we can check if any of the
    nested Portals contain the click event (which is necessary because each
    Portal is a sibling at the root document level, rather than being nested
    like the React component tree).
    
    I think a cleaner way to handle this in the future (without
    reimplementing event delegation like this) might be to use the
    `<dialog>` element (which allows for this top-level kind of UI without
    requiring a portal). Instead of handling click events, we could utilize
    focus events (I *think*) to determine when clicks outside the top-level
    UI occur without requiring a document handler, and allow events to
    follow the normal document order. This change would be a lot more
    significant and have much more potential to cause bugs, so I didn't
    pursue it here.
    
    As of this change there's still some potentially undesirable behavior
    with nested Portals, namely that clicking outside of all of them will
    dismiss them all at once. I *think* a more desirable behavior would be
    to only dismiss the top one. This could be handled with a change to the
    capture handlers (non-top-level Portals could always be told that the
    event was contained?) but I left that out for now to see how it feels
    for a day or two.

--- client/material/portal.tsx
+++ client/material/portal.tsx
@@ -1,12 +1,15 @@
-import React, { useCallback, useEffect, useRef } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
 import { useExternalElementRef } from '../dom/use-external-element-ref'
+import { useStableCallback } from '../state-hooks'
 import { markEventAsHandledDismissal } from './dismissal-events'
 
 function useDismissalClickHandler(
-  portalRef: React.MutableRefObject<HTMLDivElement>,
   onDismiss?: (event?: MouseEvent) => void,
-) {
+): [
+  onCapture: (event: MouseEvent, isContained: boolean) => void,
+  onBubble: (event: MouseEvent) => void,
+] {
   const timeoutRef = useRef<ReturnType<typeof setTimeout>>()
   const capturedRef = useRef(false)
   // We capture the event and check if it will dismiss there, so that we can mark it as a
@@ -14,40 +17,34 @@ function useDismissalClickHandler(
   // is closed. We don't actually perform the dismissal until the event bubbles back up, however,
   // so that it is easier to make things that trigger portals/popovers act more like toggles (that
   // is, close if you click them again while open).
-  const onCapture = useCallback(
-    (event: MouseEvent) => {
-      if (onDismiss) {
-        if (!portalRef.current?.contains(event.target as Node)) {
-          markEventAsHandledDismissal(event)
-          capturedRef.current = true
+  const onCapture = useStableCallback((event: MouseEvent, isContained: boolean) => {
+    if (onDismiss) {
+      if (!isContained) {
+        markEventAsHandledDismissal(event)
+        capturedRef.current = true
 
-          // NOTE(tec27): We use a timeout here because queueMicrotask seems to happen too quickly
-          // (between the capture and the bubble :( )
-          if (timeoutRef.current) {
-            clearTimeout(timeoutRef.current)
-          }
-          timeoutRef.current = setTimeout(() => {
-            capturedRef.current = false
-          }, 10)
+        // NOTE(tec27): We use a timeout here because queueMicrotask seems to happen too quickly
+        // (between the capture and the bubble :( )
+        if (timeoutRef.current) {
+          clearTimeout(timeoutRef.current)
         }
+        timeoutRef.current = setTimeout(() => {
+          capturedRef.current = false
+        }, 10)
       }
-    },
-    [onDismiss, portalRef],
-  )
-  const onBubble = useCallback(
-    (event: MouseEvent) => {
-      if (onDismiss && capturedRef.current) {
-        onDismiss(event)
-      }
+    }
+  })
+  const onBubble = useStableCallback((event: MouseEvent) => {
+    if (onDismiss && capturedRef.current) {
+      onDismiss(event)
+    }
 
-      capturedRef.current = false
-      if (timeoutRef.current) {
-        clearTimeout(timeoutRef.current)
-        timeoutRef.current = undefined
-      }
-    },
-    [onDismiss],
-  )
+    capturedRef.current = false
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current)
+      timeoutRef.current = undefined
+    }
+  })
 
   useEffect(() => {
     return () => {
@@ -59,6 +56,20 @@ function useDismissalClickHandler(
 
   return [onCapture, onBubble]
 }
+
+interface PortalContextValue {
+  registerDescendant(descendant: PortalContextValue): void
+  unregisterDescendant(descendant: PortalContextValue): void
+
+  isEventContained(event: MouseEvent): boolean
+
+  onCaptureClick: (event: MouseEvent) => void
+  onBubbleClick: (event: MouseEvent) => void
+  onCaptureContextMenu: (event: MouseEvent) => void
+  onBubbleContextMenu: (event: MouseEvent) => void
+}
+
+const PortalContext = React.createContext<PortalContextValue | undefined>(undefined)
 
 export interface PortalProps {
   className?: string
@@ -81,29 +92,118 @@ export interface PortalProps {
 export function Portal(props: PortalProps) {
   const { onDismiss, open, children } = props
 
+  const parentPortal = useContext(PortalContext)
+
   const portalRef = useExternalElementRef()
-  const [onCaptureClick, onBubbleClick] = useDismissalClickHandler(portalRef, onDismiss)
-  const [onCaptureContextMenu, onBubbleContextMenu] = useDismissalClickHandler(portalRef, onDismiss)
+  const [onCaptureClick, onBubbleClick] = useDismissalClickHandler(onDismiss)
+  const [onCaptureContextMenu, onBubbleContextMenu] = useDismissalClickHandler(onDismiss)
 
   portalRef.current.className = props.className ?? ''
 
+  const descendantsRef = useRef<PortalContextValue[]>([])
+  const containedEventsRef = useRef<WeakMap<MouseEvent, boolean>>(new WeakMap())
+
+  const contextValue = React.useMemo<PortalContextValue>(
+    () => ({
+      registerDescendant(descendant) {
+        descendantsRef.current.push(descendant)
+      },
+
+      unregisterDescendant(descendant) {
+        descendantsRef.current.splice(descendantsRef.current.indexOf(descendant), 1)
+      },
+
+      isEventContained(event) {
+        return (
+          portalRef.current?.contains(event.target as Node) ||
+          descendantsRef.current.some(d => d.isEventContained(event))
+        )
+      },
+
+      onCaptureClick(event) {
+        const isContained = this.isEventContained(event)
+        if (isContained) {
+          containedEventsRef.current.set(event, true)
+        }
+
+        onCaptureClick(event, isContained)
+        for (const descendent of descendantsRef.current) {
+          descendent.onCaptureClick(event)
+        }
+      },
+
+      onCaptureContextMenu(event) {
+        const isContained = this.isEventContained(event)
+        if (isContained) {
+          containedEventsRef.current.set(event, true)
+        }
+
+        onCaptureContextMenu(event, isContained)
+        for (const descendent of descendantsRef.current) {
+          descendent.onCaptureContextMenu(event)
+        }
+      },
+
+      onBubbleClick(event) {
+        for (let i = descendantsRef.current.length - 1; i >= 0; i--) {
+          descendantsRef.current[i].onBubbleClick(event)
+        }
+        onBubbleClick(event)
+      },
+
+      onBubbleContextMenu(event) {
+        for (let i = descendantsRef.current.length - 1; i >= 0; i--) {
+          descendantsRef.current[i].onBubbleContextMenu(event)
+        }
+        onBubbleContextMenu(event)
+      },
+    }),
+    [onBubbleClick, onBubbleContextMenu, onCaptureClick, onCaptureContextMenu, portalRef],
+  )
+
   useEffect(() => {
     if (open) {
-      document.addEventListener('click', onCaptureClick, true /* useCapture */)
-      document.addEventListener('click', onBubbleClick)
-      document.addEventListener('contextmenu', onCaptureContextMenu, true /* useCapture */)
-      document.addEventListener('contextmenu', onBubbleContextMenu)
-      return () => {
-        document.removeEventListener('click', onCaptureClick, true /* useCapture */)
-        document.removeEventListener('click', onBubbleClick)
-        document.removeEventListener('contextmenu', onCaptureContextMenu, true /* useCapture */)
-        document.removeEventListener('contextmenu', onBubbleContextMenu)
+      if (!parentPortal) {
+        // If this portal has no portal ancestors, it registers document handlers to deal with
+        // clicks
+        const captureClick = contextValue.onCaptureClick.bind(contextValue)
+        document.addEventListener('click', captureClick, true /* useCapture */)
+        const bubbleClick = contextValue.onBubbleClick.bind(contextValue)
+        document.addEventListener('click', bubbleClick)
+        const captureContext = contextValue.onCaptureContextMenu.bind(contextValue)
+        document.addEventListener('contextmenu', captureContext, true /* useCapture */)
+        const bubbleContext = contextValue.onBubbleContextMenu.bind(contextValue)
+        document.addEventListener('contextmenu', bubbleContext)
+        return () => {
+          document.removeEventListener('click', captureClick, true /* useCapture */)
+          document.removeEventListener('click', bubbleClick)
+          document.removeEventListener('contextmenu', captureContext, true /* useCapture */)
+          document.removeEventListener('contextmenu', bubbleContext)
+        }
+      } else {
+        // If this portal does have portal ancestors, one of those ancestors will handle the clicks
+        // and pass them down to us, so we just register ourselves as a descendant to receive them
+        parentPortal.registerDescendant(contextValue)
+        return () => {
+          parentPortal.unregisterDescendant(contextValue)
+        }
       }
     }
 
     // makes the linter happy :)
     return undefined
-  }, [onCaptureClick, onBubbleClick, onCaptureContextMenu, onBubbleContextMenu, open])
+  }, [
+    onCaptureClick,
+    onBubbleClick,
+    onCaptureContextMenu,
+    onBubbleContextMenu,
+    open,
+    parentPortal,
+    contextValue,
+  ])
 
-  return ReactDOM.createPortal(children, portalRef.current)
+  return ReactDOM.createPortal(
+    <PortalContext.Provider value={contextValue}>{children}</PortalContext.Provider>,
+    portalRef.current,
+  )
 }
