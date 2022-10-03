a001204978d87d4c75428c6775d35bcc2b977d6e
Travis Collins <travis@tec27.com>
2022-10-03 16:04:52 -0700

    Log unknown notification types instead of throwing (for when we add new notification types in the future, avoids breaking older clients until they update).

--- client/notifications/notification-to-ui.tsx
+++ client/notifications/notification-to-ui.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { assertUnreachable } from '../../common/assert-unreachable'
 import { NotificationType, SbNotification } from '../../common/notifications'
 import { EmailVerificationNotificationUi } from '../auth/email-verification-notification-ui'
+import { logger } from '../logging/logger'
 import { PartyInviteNotificationUi } from '../parties/party-notification-ui'
 import { PolicyUpdateNotificationUi } from '../policies/policy-update-notification-ui'
 import {
@@ -19,60 +20,65 @@ export function notificationToUi(
   showDivider: boolean,
   ref?: React.Ref<HTMLDivElement>,
 ) {
-  switch (notification.type) {
-    case NotificationType.EmailVerification:
-      return (
-        <EmailVerificationNotificationUi
-          ref={ref}
-          key={key}
-          showDivider={showDivider}
-          read={notification.read}
-        />
-      )
-    case NotificationType.PartyInvite:
-      return (
-        <PartyInviteNotificationUi
-          ref={ref}
-          key={key}
-          from={notification.from}
-          partyId={notification.partyId}
-          notificationId={notification.id}
-          showDivider={showDivider}
-          read={notification.read}
-        />
-      )
-    case NotificationType.PolicyUpdated:
-      return (
-        <PolicyUpdateNotificationUi
-          ref={ref}
-          key={key}
-          showDivider={showDivider}
-          read={notification.read}
-          policyType={notification.policyType}
-        />
-      )
-    case NotificationType.FriendRequest:
-      return (
-        <FriendRequestNotificationUi
-          ref={ref}
-          key={key}
-          notificationId={notification.id}
-          showDivider={showDivider}
-          read={notification.read}
-          from={notification.from}
-        />
-      )
-    case NotificationType.FriendStart:
-      return (
-        <FriendStartNotificationUi
-          ref={ref}
-          key={key}
-          showDivider={showDivider}
-          read={notification.read}
-          otherUser={notification.with}
-        />
-      )
-    default:
-      return assertUnreachable(notification)
+  try {
+    switch (notification.type) {
+      case NotificationType.EmailVerification:
+        return (
+          <EmailVerificationNotificationUi
+            ref={ref}
+            key={key}
+            showDivider={showDivider}
+            read={notification.read}
+          />
+        )
+      case NotificationType.PartyInvite:
+        return (
+          <PartyInviteNotificationUi
+            ref={ref}
+            key={key}
+            from={notification.from}
+            partyId={notification.partyId}
+            notificationId={notification.id}
+            showDivider={showDivider}
+            read={notification.read}
+          />
+        )
+      case NotificationType.PolicyUpdated:
+        return (
+          <PolicyUpdateNotificationUi
+            ref={ref}
+            key={key}
+            showDivider={showDivider}
+            read={notification.read}
+            policyType={notification.policyType}
+          />
+        )
+      case NotificationType.FriendRequest:
+        return (
+          <FriendRequestNotificationUi
+            ref={ref}
+            key={key}
+            notificationId={notification.id}
+            showDivider={showDivider}
+            read={notification.read}
+            from={notification.from}
+          />
+        )
+      case NotificationType.FriendStart:
+        return (
+          <FriendStartNotificationUi
+            ref={ref}
+            key={key}
+            showDivider={showDivider}
+            read={notification.read}
+            otherUser={notification.with}
+          />
+        )
+      default:
+        return assertUnreachable(notification)
+    }
+  } catch (err: any) {
+    logger.error(`Error converting notification to UI: ${err?.stack ?? err}`)
+    return null
   }
 }
