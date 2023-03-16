df84c6021c2a818a9b49296d78ad25c646961604
Marko Žarković <marko@kastela.com>
2023-03-16 10:05:14 +0100

    Add unit tests for chat service. (#900)

--- common/users/permissions.ts
+++ common/users/permissions.ts
@@ -13,3 +13,17 @@ export interface SbPermissions {
 }
 
 export type PermissionName = keyof SbPermissions
+
+export const DEFAULT_PERMISSIONS: Readonly<SbPermissions> = {
+  editPermissions: false,
+  debug: false,
+  banUsers: false,
+  manageLeagues: false,
+  manageMaps: false,
+  manageMapPools: false,
+  manageMatchmakingSeasons: false,
+  manageMatchmakingTimes: false,
+  manageRallyPointServers: false,
+  massDeleteMaps: false,
+  moderateChatChannels: false,
+}
