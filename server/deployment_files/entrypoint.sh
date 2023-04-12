27dc9b01133c38c8d84e305d914ab5808be215db
Travis Collins <travis@tec27.com>
2023-04-12 01:15:10 -0700

    Remove pino-pg util (no longer exists) from Docker container entrypoint.

--- server/deployment_files/entrypoint.sh
+++ server/deployment_files/entrypoint.sh
@@ -4,6 +4,4 @@ crond -l 3 -f > /dev/stdout 2> /dev/stderr &
 
 node ./server/index.js | \
   ./node_modules/.bin/pino-tee warn ./server/logs/errors.log | \
-  node ./server/utils/pino-pg | \
   tee ./server/logs/server.log
-
