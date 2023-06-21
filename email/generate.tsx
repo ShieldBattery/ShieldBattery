67f1d7847f22fb60ddb7933856264b4e2f36e336
Travis Collins <travis@tec27.com>
2023-06-20 17:43:13 -0700

    Add email template check to the CI.

--- email/generate.tsx
+++ email/generate.tsx
@@ -9,8 +9,13 @@ const OUTPUT_DIR = path.resolve(__dirname, '..', 'server', 'email')
 async function generateTemplates() {
   console.log('Generating email templates...\n')
 
-  const templateGlob = path.resolve(__dirname, 'templates', '**', '*.tsx')
+  const templateGlob = path.resolve(__dirname, 'templates', '*.tsx')
   const templateFiles = await glob(templateGlob, { windowsPathsNoEscape: true })
+
+  const oldTemplatesGlob = path.resolve(OUTPUT_DIR, '*.html')
+  const oldTemplateFiles = await glob(oldTemplatesGlob, { windowsPathsNoEscape: true })
+
+  await Promise.all(oldTemplateFiles.map(f => fs.unlink(f)))
 
   for (const file of templateFiles) {
     const fileName = path.basename(file, path.extname(file))
