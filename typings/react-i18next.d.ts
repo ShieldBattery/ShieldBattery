6798118237c609b3964ad21d38e4027e2172ef2d
Travis Collins <travis@tec27.com>
2023-06-27 21:16:55 -0700

    Fix typings, remove now-unused mail template.

--- typings/react-i18next.d.ts
+++ typings/react-i18next.d.ts
@@ -1,8 +1,8 @@
-import 'react-i18next'
+import { Trans } from 'react-i18next'
 
 declare module 'react-i18next' {
   // The types for react-i18next aren't completely accurate to their actual exports, as they say
   // that the `Trans` export is `TransWithoutContext` but don't also export `TransWithoutContext`.
   // We need `TransWithoutContext` to use `Trans` in emails, so we have to add it here.
-  export { Trans as TransWithoutContext } from 'react-i18next'
+  const TransWithoutContext: typeof Trans
 }
