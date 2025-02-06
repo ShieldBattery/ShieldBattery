a81c0fd4686d1d7fee3f8c9932ce0111b68ba5cf
Travis Collins <travis@tec27.com>
2025-02-05 18:13:43 -0800

    Remove as much of the loading stuff as possible since we don't really utilize it and it is kind of an anti-pattern.

--- client/loading/actions.ts
+++ client/loading/actions.ts
@@ -1,12 +1,7 @@
 import { ChatReadyEvent } from '../../common/chat'
-import { SubscribedClientEvent, SubscribedUserEvent } from '../../common/websockets'
 import { WhispersReadyEvent } from '../../common/whispers'
 
-export type LoadingActions =
-  | ChatLoadingComplete
-  | SubscriptionsClientLoadingComplete
-  | SubscriptionsUserLoadingComplete
-  | WhispersLoadingComplete
+export type LoadingActions = ChatLoadingComplete | WhispersLoadingComplete
 
 /**
  * The server has finished giving us our initial chat data (e.g what channels we are in) on connect.
@@ -14,22 +9,6 @@ export type LoadingActions =
 export interface ChatLoadingComplete {
   type: '@loading/chatReady'
   payload: ChatReadyEvent
-}
-
-/**
- * The server has finished subscribing this particular client to the things it needs.
- */
-export interface SubscriptionsClientLoadingComplete {
-  type: '@loading/subscribedClient'
-  payload: SubscribedClientEvent
-}
-
-/**
- * The server has finished subscribing this user (across clients) to the things it needs.
- */
-export interface SubscriptionsUserLoadingComplete {
-  type: '@loading/subscribedUser'
-  payload: SubscribedUserEvent
 }
 
 /**
