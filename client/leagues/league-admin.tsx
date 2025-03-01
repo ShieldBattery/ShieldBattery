232719364801ce46971fe1f24e8b3b7c165c4788
Travis Collins <travis@tec27.com>
2025-02-28 16:06:08 -0800

    Fix issue with league start/end dates not using the correct timezone when editing leagues.

--- client/leagues/league-admin.tsx
+++ client/leagues/league-admin.tsx
@@ -541,7 +541,18 @@ function EditLeague({ params: { id: routeId } }: RouteComponentProps<{ id: strin
 }
 
 function asDatetimeLocalValue(unixTime: number): string {
-  return new Date(unixTime + new Date().getTimezoneOffset() * -60 * 1000).toISOString().slice(0, 19)
+  const date = new Date(unixTime)
+  return (
+    String(date.getFullYear()).padStart(4, '0') +
+    '-' +
+    String(date.getMonth() + 1).padStart(2, '0') +
+    '-' +
+    String(date.getDate()).padStart(2, '0') +
+    'T' +
+    String(date.getHours()).padStart(2, '0') +
+    ':' +
+    String(date.getMinutes()).padStart(2, '0')
+  )
 }
 
 interface EditLeagueModel extends LeagueModel {
