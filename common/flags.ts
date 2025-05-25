413db82ed2d79fd43a927d024ec18878877cd379
Travis Collins <travis@tec27.com>
2025-05-24 21:25:25 -0700

    Turn on CAN_LEAVE_SHIELDBATTERY_CHANNEL feature.

--- common/flags.ts
+++ common/flags.ts
@@ -3,6 +3,7 @@
 // and On (on everywhere). To add a new flag, just declare it as an export here, and set its value
 // to the return value of the right level (e.g. `DEV()`), then check it in any relevant code paths.
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const OFF = () => false
 const DEV = () => process.env.NODE_ENV !== 'production'
 const ON = () => true
