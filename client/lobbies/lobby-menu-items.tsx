a04838d0406916c59ae4fa3a5079b0f21c45d745
2Pacalypse- <marko@kastela.com>
2025-07-14 17:26:47 +0200

    Remove the lobby context since it's not needed.

--- client/lobbies/lobby-menu-items.tsx
+++ client/lobbies/lobby-menu-items.tsx
@@ -1,4 +1,3 @@
-import { useContext } from 'react'
 import { useTranslation } from 'react-i18next'
 import { appendToMultimap } from '../../common/data-structures/maps'
 import { findSlotByUserId } from '../../common/lobbies'
@@ -6,21 +5,21 @@ import { DestructiveMenuItem } from '../material/menu/item'
 import { useAppDispatch, useAppSelector } from '../redux-hooks'
 import { MenuItemCategory, UserMenuProps } from '../users/user-context-menu'
 import { banPlayer, kickPlayer } from './action-creators'
-import { LobbyContext } from './lobby-context'
 
 export function LobbyUserMenu({ userId, items, onMenuClose, MenuComponent }: UserMenuProps) {
   const { t } = useTranslation()
   const dispatch = useAppDispatch()
   const selfUserId = useAppSelector(s => s.auth.self!.user.id)
   const user = useAppSelector(s => s.users.byId.get(userId))
-  const { lobby } = useContext(LobbyContext)
+  const inLobby = useAppSelector(s => s.lobby.inLobby)
+  const lobby = useAppSelector(s => s.lobby.info)
   const [, , slot] = findSlotByUserId(lobby, userId)
 
   const isHost = lobby.host.userId === selfUserId
   const isSelf = user?.id === selfUserId
 
   const menuItems = new Map(items)
-  if (user && slot && !isSelf && isHost) {
+  if (inLobby && user && slot && !isSelf && isHost) {
     appendToMultimap(
       menuItems,
       MenuItemCategory.Destructive,
