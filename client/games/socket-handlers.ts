db9b45f49f34ef304264b83675e84f7ef42d31e7
Travis Collins <travis@tec27.com>
2025-07-25 15:17:04 -0700

    Attempt to resend results from the app for longer.
    We now try 20 times, once every 30 seconds (so for 10 minutes).
    
    Fixes #542.

--- client/games/socket-handlers.ts
+++ client/games/socket-handlers.ts
@@ -1,11 +1,15 @@
 import { NydusClient, RouteInfo } from 'nydus-client'
 import { assertUnreachable } from '../../common/assert-unreachable'
+import swallowNonBuiltins from '../../common/async/swallow-non-builtins'
+import { getErrorStack } from '../../common/errors'
 import { GameSubscriptionEvent } from '../../common/games/games'
+import { GameResultErrorCode } from '../../common/games/results'
 import { TypedIpcRenderer } from '../../common/ipc'
 import { apiUrl } from '../../common/urls'
 import { dispatch } from '../dispatch-registry'
 import logger from '../logging/logger'
 import { fetchJson } from '../network/fetch'
+import { isFetchError } from '../network/fetch-errors'
 
 export default function ({
   ipcRenderer,
@@ -26,19 +30,40 @@ export default function ({
       })
     })
     .on('activeGameResendResults', (_, gameId, requestBody) => {
-      // TODO(#542): Retry submission of these results more times/for longer to try and ensure
-      // complete resutls on the server
-      logger.verbose('Game failed to send result, retrying once from the app')
-      fetchJson<void>(apiUrl`games/${gameId}/results2`, {
-        method: 'POST',
-        body: JSON.stringify(requestBody),
-      })
-        .then(() => {
-          logger.verbose('Game result submitted successfully')
+      const ATTEMPTS = 20
+      const DELAY_MILLIS = 30 * 1000 // 30 seconds
+
+      logger.verbose('Game failed to send result, retrying from the app')
+      Promise.resolve()
+        .then(async () => {
+          for (let i = 0; i < ATTEMPTS; i++) {
+            try {
+              await fetchJson<void>(apiUrl`games/${gameId}/results2`, {
+                method: 'POST',
+                body: JSON.stringify(requestBody),
+              })
+              logger.verbose('Game result resent successfully')
+              return
+            } catch (err) {
+              if (isFetchError(err)) {
+                if (err.code == GameResultErrorCode.AlreadyReported) {
+                  logger.verbose(`Game result already reported, not retrying further`)
+                  return
+                } else if (err.code == GameResultErrorCode.NotFound) {
+                  logger.warning(`Game result resend failed, game not found!`)
+                  return
+                }
+              }
+
+              logger.error(
+                `Game result resend ${i + 1} / ${ATTEMPTS} failed: ${getErrorStack(err)}`,
+              )
+            }
+
+            await new Promise(resolve => setTimeout(resolve, DELAY_MILLIS))
+          }
         })
-        .catch(err => {
-          logger.error(`Game result submission failed: ${(err as any)?.stack ?? err}`)
-        })
+        .catch(swallowNonBuiltins)
     })
 
   siteSocket.registerRoute('/games/:gameId', (route: RouteInfo, event: GameSubscriptionEvent) => {
