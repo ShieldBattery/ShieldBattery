8422deddf5fb5bbf6fe3b26887a408de8550ba6e
Travis Collins <travis@tec27.com>
2025-08-13 23:06:10 -0700

    Fix useRedirectAfterLogin to work properly in strict mode.
    Fixes #1236.

--- client/auth/auth-utils.ts
+++ client/auth/auth-utils.ts
@@ -6,14 +6,24 @@ import { SelfUser } from '../../common/users/sb-user'
 import { makePathString, replace } from '../navigation/routing'
 import { useAppSelector } from '../redux-hooks'
 
+// Tracks if we're in the process of redirecting after login so that having multiple
+// `useRedirectAfterLogin` hooks rendered (or being in strict mode in dev) doesn't cause issues
+const IS_REDIRECTING = { value: false }
+
 /** Redirects to the current `nextPath` query string parameter once the client has logged in. */
 export function useRedirectAfterLogin() {
   const isLoggedIn = useIsLoggedIn()
   useEffect(() => {
-    if (isLoggedIn) {
+    if (isLoggedIn && !IS_REDIRECTING.value) {
+      IS_REDIRECTING.value = true
+
       const nextPath =
         location && location.search ? (queryString.parse(location.search).nextPath ?? '/') : '/'
       replace(Array.isArray(nextPath) ? (nextPath[0] ?? '/') : nextPath)
+
+      queueMicrotask(() => {
+        IS_REDIRECTING.value = false
+      })
     }
   }, [isLoggedIn])
 }
