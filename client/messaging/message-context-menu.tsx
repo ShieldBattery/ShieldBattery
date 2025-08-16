ShieldBattery/ShieldBattery

commit 616923056d429ff1c00e71c71ad6b9f52b933152

Don't render the context menu contents if it doesn't have any items. (#1234)
Can be reproduced by trying to right-click a chat message in the
whispers session; or for users who don't have the "Delete message"
action, by right-clicking any message in the chat.

client/messaging/message-context-menu.tsx
@@ -154,14 +154,14 @@ function MessageContextMenuContents({
     )
   }
 
-  return (
+  return items.size > 0 ? (
     <MessageMenu
       messageId={messageId}
       items={items}
       onMenuClose={onDismiss}
       MenuComponent={MessageContextMenuList}
     />
-  )
+  ) : null
 }
 
 function MessageContextMenuList({

client/users/user-context-menu.tsx
@@ -464,14 +464,14 @@ function ConnectedUserContextMenuContents({
   }
   const mergedItems = mergeMultimaps(items, baseItems)
 
-  return (
+  return mergedItems.size > 0 ? (
     <UserMenu
       MenuComponent={UserContextMenuList}
       items={mergedItems}
       userId={userId}
       onMenuClose={onDismiss}
     />
-  )
+  ) : null
 }
 
 function UserContextMenuList({

commit c5bad4b2e19ba92d646e6226aa9b64df4fb5a9b0

Fix message menus not showing up without selecting text.
Broken in 616923056d429ff1c00e71c71ad6b9f52b933152. The MessageMenu is
passed in via props and can be customized/add its own items, so checking
the size there can't work. Those are given `MenuComponent` (which we
pass in `MessageContextMenuList` to actually render their menu, so we
just check the length in there instead.

client/messaging/message-context-menu.tsx
@@ -154,14 +154,14 @@ function MessageContextMenuContents({
     )
   }
 
-  return items.size > 0 ? (
+  return (
     <MessageMenu
       messageId={messageId}
       items={items}
       onMenuClose={onDismiss}
       MenuComponent={MessageContextMenuList}
     />
-  ) : null
+  )
 }
 
 function MessageContextMenuList({
@@ -184,9 +184,9 @@ function MessageContextMenuList({
     return elems
   }, [])
 
-  return (
+  return orderedMenuItems.length > 0 ? (
     <MessageMenuContext.Provider value={{ messageId, onMenuClose }}>
       <MenuList dense={true}>{orderedMenuItems}</MenuList>
     </MessageMenuContext.Provider>
-  )
+  ) : null
 }