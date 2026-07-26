3e9c5dad9ba7212ed6df23cf97c01aa80113d916
Travis Collins <travis@tec27.com>
2026-07-25 20:31:37 -0700

    Re-gen graphql files.

--- client/gql/graphql.ts
+++ client/gql/graphql.ts
@@ -3,8 +3,7 @@
 type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] }
 /** Internal type. DO NOT USE DIRECTLY. */
 export type Incremental<T> =
-  | T
-  | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never }
+  T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never }
 import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core'
 import * as Types from './types'
 export type CreateSignupCodeInput = {
